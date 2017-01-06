zulip/zulip

Manage allowed domains from admin settings.
Fixes: #1867.

zerver/tests/test_realm_aliases.py
@@ -1,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from zerver.lib.test_classes import ZulipTestCase
+from zerver.models import get_realm, get_realm_by_email_domain, \
+    GetRealmByDomainException, RealmAlias
+import ujson
+
+
+class RealmAliasTest(ZulipTestCase):
+
+    def test_list(self):
+        # type: () -> None
+        self.login("iago@zulip.com")
+        realm = get_realm('zulip')
+        alias = RealmAlias(realm=realm, domain='zulip.org')
+        alias.save()
+        result = self.client_get("/json/realm/domains")
+        self.assert_json_success(result)
+        self.assertEqual(200, result.status_code)
+        content = ujson.loads(result.content)
+        self.assertEqual(len(content['aliases']), 2)
+
+    def test_not_realm_admin(self):
+        # type: () -> None
+        self.login("hamlet@zulip.com")
+        result = self.client_post("/json/realm/domains")
+        self.assert_json_error(result, 'Must be a realm administrator')
+        result = self.client_delete("/json/realm/domains/15")
+        self.assert_json_error(result, 'Must be a realm administrator')
+
+    def test_create(self):
+        # type: () -> None
+        self.login("iago@zulip.com")
+        data = {"domain": ""}
+        result = self.client_post("/json/realm/domains", info=data)
+        self.assert_json_error(result, 'Domain can\'t be empty.')
+
+        data['domain'] = 'zulip.org'
+        result = self.client_post("/json/realm/domains", info=data)
+        self.assert_json_success(result)
+
+        result = self.client_post("/json/realm/domains", info=data)
+        self.assert_json_error(result, 'A Realm for this domain already exists.')
+
+    def test_delete(self):
+        # type: () -> None
+        self.login("iago@zulip.com")
+        realm = get_realm('zulip')
+        alias_id = RealmAlias.objects.create(realm=realm, domain='zulip.org').id
+        aliases_count = RealmAlias.objects.count()
+        result = self.client_delete("/json/realm/domains/{0}".format(alias_id + 1))
+        self.assert_json_error(result, 'No such entry found.')
+
+        result = self.client_delete("/json/realm/domains/{0}".format(alias_id))
+        self.assert_json_success(result)
+        self.assertEqual(RealmAlias.objects.count(), aliases_count - 1)
+
+    def test_get_realm_by_email_domain(self):
+        # type: () -> None
+        self.assertEqual(get_realm_by_email_domain('user@zulip.com').string_id, 'zulip')
+        self.assertEqual(get_realm_by_email_domain('user@fakedomain.com'), None)
+        with self.settings(REALMS_HAVE_SUBDOMAINS = True), (
+             self.assertRaises(GetRealmByDomainException)):
+            get_realm_by_email_domain('user@zulip.com')

zerver/views/realm_aliases.py
@@ -1,0 +1,42 @@
+from __future__ import absolute_import
+
+from django.core.exceptions import ValidationError
+from django.http import HttpRequest, HttpResponse
+from django.utils.translation import ugettext as _
+
+from zerver.decorator import has_request_variables, REQ, require_realm_admin
+from zerver.lib.actions import do_add_realm_alias, do_get_realm_aliases, \
+    do_remove_realm_alias
+from zerver.lib.response import json_error, json_success
+from zerver.models import can_add_alias, RealmAlias, UserProfile
+
+from typing import Text
+
+def list_aliases(request, user_profile):
+    # type: (HttpRequest, UserProfile) -> (HttpResponse)
+    aliases = do_get_realm_aliases(user_profile.realm)
+    return json_success({'aliases': aliases})
+
+@require_realm_admin
+@has_request_variables
+def create_alias(request, user_profile, domain=REQ()):
+    # type: (HttpRequest, UserProfile, Text) -> (HttpResponse)
+    if can_add_alias(domain):
+        try:
+            alias = do_add_realm_alias(user_profile.realm, domain)
+        except ValidationError:
+            return json_error(_('Domain can\'t be empty.'))
+    else:
+        return json_error(_('A Realm for this domain already exists.'))
+    return json_success({'new_alias': [alias.id, alias.domain]})
+
+@require_realm_admin
+@has_request_variables
+def delete_alias(request, user_profile, alias_id):
+    # type: (HttpRequest, UserProfile, int) -> (HttpResponse)
+    try:
+        # Ensure alias_id is an integer. Django passes captured url parameters as strings.
+        do_remove_realm_alias(user_profile.realm, int(alias_id))
+    except RealmAlias.DoesNotExist:
+        return json_error(_('No such entry found.'))
+    return json_success()

afedotov89/flexar-zulip

zerver/tests/test_realm_aliases.py
@@ -1,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from zerver.lib.test_classes import ZulipTestCase
+from zerver.models import get_realm, get_realm_by_email_domain, \
+    GetRealmByDomainException, RealmAlias
+import ujson
+
+
+class RealmAliasTest(ZulipTestCase):
+
+    def test_list(self):
+        # type: () -> None
+        self.login("iago@zulip.com")
+        realm = get_realm('zulip')
+        alias = RealmAlias(realm=realm, domain='zulip.org')
+        alias.save()
+        result = self.client_get("/json/realm/domains")
+        self.assert_json_success(result)
+        self.assertEqual(200, result.status_code)
+        content = ujson.loads(result.content)
+        self.assertEqual(len(content['domains']), 2)
+
+    def test_not_realm_admin(self):
+        # type: () -> None
+        self.login("hamlet@zulip.com")
+        result = self.client_post("/json/realm/domains")
+        self.assert_json_error(result, 'Must be a realm administrator')
+        result = self.client_delete("/json/realm/domains/15")
+        self.assert_json_error(result, 'Must be a realm administrator')
+
+    def test_create(self):
+        # type: () -> None
+        self.login("iago@zulip.com")
+        data = {"domain": ""}
+        result = self.client_post("/json/realm/domains", info=data)
+        self.assert_json_error(result, 'Domain can\'t be empty.')
+
+        data['domain'] = 'zulip.org'
+        result = self.client_post("/json/realm/domains", info=data)
+        self.assert_json_success(result)
+
+        result = self.client_post("/json/realm/domains", info=data)
+        self.assert_json_error(result, 'A Realm for this domain already exists.')
+
+    def test_delete(self):
+        # type: () -> None
+        self.login("iago@zulip.com")
+        realm = get_realm('zulip')
+        alias_id = RealmAlias.objects.create(realm=realm, domain='zulip.org').id
+        aliases_count = RealmAlias.objects.count()
+        result = self.client_delete("/json/realm/domains/{0}".format(alias_id + 1))
+        self.assert_json_error(result, 'No such entry found.')
+
+        result = self.client_delete("/json/realm/domains/{0}".format(alias_id))
+        self.assert_json_success(result)
+        self.assertEqual(RealmAlias.objects.count(), aliases_count - 1)
+
+    def test_get_realm_by_email_domain(self):
+        # type: () -> None
+        self.assertEqual(get_realm_by_email_domain('user@zulip.com').string_id, 'zulip')
+        self.assertEqual(get_realm_by_email_domain('user@fakedomain.com'), None)
+        with self.settings(REALMS_HAVE_SUBDOMAINS = True), (
+             self.assertRaises(GetRealmByDomainException)):
+            get_realm_by_email_domain('user@zulip.com')

zerver/views/realm_aliases.py
@@ -1,0 +1,42 @@
+from __future__ import absolute_import
+
+from django.core.exceptions import ValidationError
+from django.http import HttpRequest, HttpResponse
+from django.utils.translation import ugettext as _
+
+from zerver.decorator import has_request_variables, REQ, require_realm_admin
+from zerver.lib.actions import do_add_realm_alias, do_get_realm_aliases, \
+    do_remove_realm_alias
+from zerver.lib.response import json_error, json_success
+from zerver.models import can_add_alias, RealmAlias, UserProfile
+
+from typing import Text
+
+def list_aliases(request, user_profile):
+    # type: (HttpRequest, UserProfile) -> (HttpResponse)
+    aliases = do_get_realm_aliases(user_profile.realm)
+    return json_success({'domains': aliases})
+
+@require_realm_admin
+@has_request_variables
+def create_alias(request, user_profile, domain=REQ()):
+    # type: (HttpRequest, UserProfile, Text) -> (HttpResponse)
+    if can_add_alias(domain):
+        try:
+            alias = do_add_realm_alias(user_profile.realm, domain)
+        except ValidationError:
+            return json_error(_('Domain can\'t be empty.'))
+    else:
+        return json_error(_('A Realm for this domain already exists.'))
+    return json_success({'new_domain': [alias.id, alias.domain]})
+
+@require_realm_admin
+@has_request_variables
+def delete_alias(request, user_profile, alias_id):
+    # type: (HttpRequest, UserProfile, int) -> (HttpResponse)
+    try:
+        # Ensure alias_id is an integer. Django passes captured url parameters as strings.
+        do_remove_realm_alias(user_profile.realm, int(alias_id))
+    except RealmAlias.DoesNotExist:
+        return json_error(_('No such entry found.'))
+    return json_success()