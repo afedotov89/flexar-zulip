afedotov89/flexar-zulip

commit 70922d398568b68e54601ab20174780b4fac6af0

stats.js: Change how we set the precision for percentages close to 100%.

static/js/stats/stats.js
@@ -303,13 +303,8 @@ function round_percentages(values) {
     var total = values.reduce(function (a, b) { return a + b; }, 0);
     var percents = values.map(function (x) {
         var unrounded = x/total*100;
-        var rounded;
-        if (unrounded > 99.4) {
-            rounded = unrounded.toPrecision(3);
-        } else {
-            rounded = unrounded.toPrecision(2);
-        }
-        return rounded + '%';
+        var precision = Math.max(2, Math.floor(-Math.log(100-unrounded)/Math.log(10)) + 3);
+        return unrounded.toPrecision(precision) + '%';
     });
     return [total, percents];
 }

commit 74aad20982a6cfe4497ed189521cd5e2b032a6c3

stats.js: Fix round_to_percentages when percentage is 100.
Fixes bug introduced in 70922d3.

static/js/stats/stats.js
@@ -314,8 +314,12 @@ function word_wrap(text, width) {
 
 function round_to_percentages(values, total) {
     return values.map(function (x) {
+        if (x === total) {
+            return '100%';
+        }
         var unrounded = x/total*100;
-        var precision = Math.max(2, Math.floor(-Math.log(100-unrounded)/Math.log(10)) + 3);
+        var precision = Math.min(6, Math.max(2, Math.floor(
+                -Math.log(100-unrounded)/Math.log(10)) + 3));
         return unrounded.toPrecision(precision) + '%';
     });
 }