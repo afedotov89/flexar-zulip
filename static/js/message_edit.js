afedotov89/flexar-zulip

commit f2a6115d77a44735b34d219b38e69eadc197aecd

eslint: change no-plusplus from warning to 2 and fix violations.

static/js/message_edit.js
@@ -239,7 +239,7 @@ function edit_message (row, raw_content) {
         // since otherwise there is a noticeable lag
         message_edit_countdown_timer.text(timer_text(seconds_left));
         var countdown_timer = setInterval(function () {
-            if (--seconds_left <= 0) {
+            if (seconds_left - 1 <= 0) {
                 clearInterval(countdown_timer);
                 message_edit_content.prop("readonly", "readonly");
                 if (message.type === 'stream') {

frontend_tests/casper_lib/common.js
@@ -56,7 +56,7 @@ exports.initialize_casper = function (viewport) {
     casper.test.on('fail', function failure() {
         if (casper_failure_count <= 10) {
             casper.capture("var/casper/casper-failure" + casper_failure_count + ".png");
-            casper_failure_count++;
+            casper_failure_count += 1;
         }
     });
 

frontend_tests/node_tests/unread.js
@@ -191,7 +191,7 @@ var zero_counts = {
 
     var num_msgs = 10000;
     var i;
-    for (i = 0; i < num_msgs; ++i) {
+    for (i = 0; i < num_msgs; i += 1) {
         message.id = i+1;
         unread.process_loaded_messages([message]);
     }
@@ -199,7 +199,7 @@ var zero_counts = {
     count = unread.num_unread_for_subject('social', 'lunch');
     assert.equal(count, num_msgs);
 
-    for (i = 0; i < num_msgs; ++i) {
+    for (i = 0; i < num_msgs; i += 1) {
         message.id = i+1;
         unread.process_read_message(message);
     }

frontend_tests/node_tests/util.js
@@ -188,19 +188,19 @@ var _ = global._;
       'some_email@**everyone**.com'
     ];
     var i;
-    for (i=0; i<messages_with_all_mentions.length; i++) {
+    for (i=0; i<messages_with_all_mentions.length; i += 1) {
         assert(util.is_all_or_everyone_mentioned(messages_with_all_mentions[i]));
     }
 
-    for (i=0; i<messages_with_everyone_mentions.length; i++) {
+    for (i=0; i<messages_with_everyone_mentions.length; i += 1) {
         assert(util.is_all_or_everyone_mentioned(messages_with_everyone_mentions[i]));
     }
 
-    for (i=0; i<messages_without_all_mentions.length; i++) {
+    for (i=0; i<messages_without_all_mentions.length; i += 1) {
         assert(!util.is_all_or_everyone_mentioned(messages_without_everyone_mentions[i]));
     }
 
-    for (i=0; i<messages_without_everyone_mentions.length; i++) {
+    for (i=0; i<messages_without_everyone_mentions.length; i += 1) {
         assert(!util.is_all_or_everyone_mentioned(messages_without_everyone_mentions[i]));
     }
 }());

static/js/activity.js
@@ -176,7 +176,7 @@ exports.huddle_fraction_present = function (huddle, presence_info) {
         if (presence_info[user_id]) {
             var status = presence_info[user_id].status;
             if (status && (status !== 'offline')) {
-                ++num_present;
+                num_present += 1;
             }
         }
     });

static/js/admin.js
@@ -264,7 +264,7 @@ function _setup_page() {
     var domains_string = stringify_list_with_conjunction(page_params.domains, "or");
     var atdomains = page_params.domains.slice();
     var i;
-    for (i = 0; i < atdomains.length; i++) {
+    for (i = 0; i < atdomains.length; i += 1) {
         atdomains[i] = '@' + atdomains[i];
     }
     var atdomains_string = stringify_list_with_conjunction(atdomains, "or");

static/js/compose_fade.js
@@ -61,7 +61,7 @@ function _fade_messages() {
     normal_display = false;
 
     // Update the visible messages first, before the compose box opens
-    for (i = 0; i < visible_groups.length; i++) {
+    for (i = 0; i < visible_groups.length; i += 1) {
         first_row = rows.first_message_in_group(visible_groups[i]);
         first_message = current_msg_list.get(rows.id(first_row));
         should_fade_group = !fade_heuristic(focused_recipient, first_message);
@@ -83,7 +83,7 @@ function _fade_messages() {
 
         // Note: The below algorithm relies on the fact that all_elts is
         // sorted as it would be displayed in the message view
-        for (i = 0; i < all_groups.length; i++) {
+        for (i = 0; i < all_groups.length; i += 1) {
             var group_elt = $(all_groups[i]);
             should_fade_group = !fade_heuristic(focused_recipient, rows.recipient_from_group(group_elt));
             change_fade_state(group_elt, should_fade_group);

static/js/copy_and_paste.js
@@ -19,7 +19,7 @@ function find_boundary_tr(initial_tr, iterate_row) {
     // To ensure we can't enter an infinite loop, bail out (and let the
     // browser handle the copy-paste on its own) if we don't hit what we
     // are looking for within 10 rows.
-    for (j = 0; (!tr.is('.message_row')) && j < 10; j++) {
+    for (j = 0; (!tr.is('.message_row')) && j < 10; j += 1) {
         tr = iterate_row(tr);
     }
     if (j === 10) {
@@ -41,7 +41,7 @@ function copy_handler(e) {
     var start_data, end_data;
     var skip_same_td_check = false;
     var div = $('<div>'), content;
-    for (i = 0; i < selection.rangeCount; i++) {
+    for (i = 0; i < selection.rangeCount; i += 1) {
         range = selection.getRangeAt(i);
         ranges.push(range);
 

commit 97060f3d8b32b793b055d843e82cb72fc5fbe47e

message_edit: Fix timer text not decrementing.
This fixes a regression introduced in f2a6115, where we simply lost the `--` part.

Fixes #3720.

static/js/message_edit.js
@@ -244,7 +244,8 @@ function edit_message(row, raw_content) {
         // since otherwise there is a noticeable lag
         message_edit_countdown_timer.text(timer_text(seconds_left));
         var countdown_timer = setInterval(function () {
-            if (seconds_left - 1 <= 0) {
+            seconds_left -= 1;
+            if (seconds_left <= 0) {
                 clearInterval(countdown_timer);
                 message_edit_content.prop("readonly", "readonly");
                 if (message.type === 'stream') {