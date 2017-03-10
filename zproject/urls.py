zulip/zulip

Migrate legacy url json/bulk_invite_users to invite/bulk_invite.

zproject/urls.py
@@ -234,6 +234,10 @@ v1_api_and_json_patterns = [
     url(r'^user_uploads$', rest_dispatch,
         {'POST': 'zerver.views.upload.upload_file_backend'}),
 
+    # invite -> zerver.views.invite
+    url(r'^invite/bulk_invite$', rest_dispatch,
+        {'POST': 'zerver.views.invite.bulk_invite_users'}),
+
     # users/me -> zerver.views
     url(r'^users/me$', rest_dispatch,
         {'GET': 'zerver.views.users.get_profile_backend',

afedotov89/flexar-zulip

zproject/urls.py
@@ -263,6 +263,10 @@ v1_api_and_json_patterns = [
     url(r'^user_uploads$', rest_dispatch,
         {'POST': 'zerver.views.upload.upload_file_backend'}),
 
+    # invite -> zerver.views.invite
+    url(r'^invite/bulk$', rest_dispatch,
+        {'POST': 'zerver.views.invite.bulk_invite_users'}),
+
     # users/me -> zerver.views
     url(r'^users/me$', rest_dispatch,
         {'GET': 'zerver.views.users.get_profile_backend',