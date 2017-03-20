afedotov89/flexar-zulip

commit 9a6c98284757f9b2cbc9f63c55b5322269593626

refactor: Extract activity.get_user_list_item().

static/js/activity.js
@@ -73,9 +73,13 @@ function update_count_in_dom(count_span, value_span, count) {
     value_span.text(count);
 }
 
+function get_user_list_item(user_id) {
+    return $("li.user_sidebar_entry[data-user-id='" + user_id + "']");
+}
+
 function get_filter_li(user_ids_string) {
     if (name.indexOf(",") < 0) {
-        return $("li.user_sidebar_entry[data-user-id='" + user_ids_string + "']");
+        get_user_list_item(user_ids_string);
     }
     return $("li.group-pms-sidebar-entry[data-user-ids='" + user_ids_string + "']");
 }

commit c1f3a075d2a7cf5a4ed7a159af4c842a5088ad76

bug fix: Fix missing return statement in get_filter_li().
This was regressed in 9a6c98284757f9b2cbc9f63c55b5322269593626.

At least one of the symptoms was that we weren't updating the
activity list properly.  This could also cause tracebacks in
compose fade logic.

static/js/activity.js
@@ -79,7 +79,7 @@ function get_user_list_item(user_id) {
 
 function get_filter_li(user_ids_string) {
     if (name.indexOf(",") < 0) {
-        get_user_list_item(user_ids_string);
+        return  get_user_list_item(user_ids_string);
     }
     return $("li.group-pms-sidebar-entry[data-user-ids='" + user_ids_string + "']");
 }