zulip/zulip

alerts: Change sidebar alerts to be at top of the screen.
This changes the alerts to be individual boxes that slide down from
the top of the screen for a better UI experience.

frontend_tests/node_tests/server_events.js
@@ -31,6 +31,10 @@ set_global('echo', {
     },
     set_realm_filters: noop,
 });
+set_global('ui', {
+    hide_error: function () { return false; },
+    show_error: function () { return false; },
+});
 
 var server_events = require('js/server_events.js');
 

static/js/server_events.js
@@ -465,7 +465,7 @@ function get_events(options) {
             try {
                 get_events_xhr = undefined;
                 get_events_failures = 0;
-                $('#connection-error').hide();
+                ui.hide_error($("#connection-error"));
 
                 get_events_success(data.events);
             } catch (ex) {
@@ -496,15 +496,15 @@ function get_events(options) {
                 } else if (error_type === 'timeout') {
                     // Retry indefinitely on timeout.
                     get_events_failures = 0;
-                    $('#connection-error').hide();
+                    ui.hide_error($("#connection-error"));
                 } else {
                     get_events_failures += 1;
                 }
 
                 if (get_events_failures >= 5) {
-                    $('#connection-error').show();
+                    ui.show_error($("#connection-error"));
                 } else {
-                    $('#connection-error').hide();
+                    ui.hide_error($("#connection-error"));
                 }
             } catch (ex) {
                 blueslip.error('Failed to handle get_events error\n' +

afedotov89/flexar-zulip

frontend_tests/node_tests/server_events.js
@@ -31,6 +31,10 @@ set_global('echo', {
     },
     set_realm_filters: noop,
 });
+set_global('ui_report', {
+    hide_error: function () { return false; },
+    show_error: function () { return false; },
+});
 
 var server_events = require('js/server_events.js');
 

static/js/server_events.js
@@ -465,7 +465,7 @@ function get_events(options) {
             try {
                 get_events_xhr = undefined;
                 get_events_failures = 0;
-                $('#connection-error').hide();
+                ui_report.hide_error($("#connection-error"));
 
                 get_events_success(data.events);
             } catch (ex) {
@@ -496,15 +496,15 @@ function get_events(options) {
                 } else if (error_type === 'timeout') {
                     // Retry indefinitely on timeout.
                     get_events_failures = 0;
-                    $('#connection-error').hide();
+                    ui_report.hide_error($("#connection-error"));
                 } else {
                     get_events_failures += 1;
                 }
 
                 if (get_events_failures >= 5) {
-                    $('#connection-error').show();
+                    ui_report.show_error($("#connection-error"));
                 } else {
-                    $('#connection-error').hide();
+                    ui_report.hide_error($("#connection-error"));
                 }
             } catch (ex) {
                 blueslip.error('Failed to handle get_events error\n' +