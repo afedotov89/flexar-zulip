afedotov89/flexar-zulip

commit 6a13ee819ce0e6fe3532381e3487d11eb625f491

git integrations: Expand details in commit push notifications.
We now show a few new things:
(1) The number of commits pushed.
(2) Who authored the commits (just counts, not which specific ones, for brevity).

Add tests for case of multiple committers.

Part of #3968.

zerver/lib/webhooks/git.py
@@ -1,5 +1,5 @@
-from typing import Optional, Any, Dict, List, Text
-
+from typing import Optional, Any, Dict, List, Text, Tuple
+from collections import defaultdict
 SUBJECT_WITH_BRANCH_TEMPLATE = u'{repo} / {branch}'
 SUBJECT_WITH_PR_OR_ISSUE_INFO_TEMPLATE = u'{repo} / {type} #{id} {title}'
 
@@ -8,13 +8,16 @@ EMPTY_SHA = '0000000000000000000000000000000000000000'
 COMMITS_LIMIT = 20
 COMMIT_ROW_TEMPLATE = u'* {commit_msg} ([{commit_short_sha}]({commit_url}))\n'
 COMMITS_MORE_THAN_LIMIT_TEMPLATE = u"[and {commits_number} more commit(s)]"
+COMMIT_OR_COMMITS = u"commit{}"
 
-PUSH_PUSHED_TEXT_WITH_URL = u"[pushed]({compare_url})"
-PUSH_PUSHED_TEXT_WITHOUT_URL = u"pushed"
-PUSH_COMMITS_MESSAGE_TEMPLATE = u"""{user_name} {pushed_text} to branch {branch_name}
+PUSH_PUSHED_TEXT_WITH_URL = u"[pushed]({compare_url}) {number_of_commits} {commit_or_commits}"
+PUSH_PUSHED_TEXT_WITHOUT_URL = u"pushed {number_of_commits} {commit_or_commits}"
+PUSH_COMMITS_MESSAGE_TEMPLATE = u"""{user_name} {pushed_text} to branch {branch_name}. {committers_details}
 
 {commits_data}
 """
+PUSH_COMMITS_MESSAGE_EXTENSION = u"Commits by {}"
+PUSH_COMMITTERS_LIMIT_INFO = 3
 
 FORCE_PUSH_COMMITS_MESSAGE_TEMPLATE = u"{user_name} [force pushed]({url}) to branch {branch_name}. Head is now {head}"
 CREATE_BRANCH_MESSAGE_TEMPLATE = u"{user_name} created [{branch_name}]({url}) branch"
@@ -35,17 +38,30 @@ PUSH_TAGS_MESSAGE_TEMPLATE = u"""{user_name} {action} tag {tag}"""
 TAG_WITH_URL_TEMPLATE = u"[{tag_name}]({tag_url})"
 TAG_WITHOUT_URL_TEMPLATE = u"{tag_name}"
 
+
 def get_push_commits_event_message(user_name, compare_url, branch_name, commits_data, is_truncated=False):
     # type: (Text, Optional[Text], Text, List[Dict[str, Any]], Optional[bool]) -> Text
-    if compare_url:
-        pushed_text_message = PUSH_PUSHED_TEXT_WITH_URL.format(compare_url=compare_url)
-    else:
-        pushed_text_message = PUSH_PUSHED_TEXT_WITHOUT_URL
+    committers_items = get_all_committers(commits_data)  # type: List[Tuple[str, int]]
+    committers_details = "{}({})".format(*committers_items[0])
+
+    for name, number_of_commits in committers_items[1:-1]:
+        committers_details = "{}, {}({})".format(committers_details, name, number_of_commits)
+
+    if len(committers_items) > 1:
+        committers_details = "{} and {}({})".format(committers_details, *committers_items[-1])
+
+    pushed_message_template = PUSH_PUSHED_TEXT_WITH_URL if compare_url else PUSH_PUSHED_TEXT_WITHOUT_URL
+
+    pushed_text_message = pushed_message_template.format(
+        compare_url=compare_url,
+        number_of_commits=len(commits_data),
+        commit_or_commits=COMMIT_OR_COMMITS.format(u's' if len(commits_data) > 1 else u''))
 
     return PUSH_COMMITS_MESSAGE_TEMPLATE.format(
         user_name=user_name,
         pushed_text=pushed_text_message,
         branch_name=branch_name,
+        committers_details=PUSH_COMMITS_MESSAGE_EXTENSION.format(committers_details),
         commits_data=get_commits_content(commits_data, is_truncated),
     ).rstrip()
 
@@ -166,3 +182,20 @@ def get_commits_content(commits_data, is_truncated=False):
 def get_short_sha(sha):
     # type: (Text) -> Text
     return sha[:7]
+
+def get_all_committers(commits_data):
+    # type: (List[Dict[str, Any]]) -> List[Tuple[str, int]]
+    committers = defaultdict(int)  # type: Dict[str, int]
+
+    for commit in commits_data:
+        committers[commit['name']] += 1
+
+    committers_items = sorted(list(committers.items()), key=lambda item: item[1], reverse=True)  # type: List[Tuple[str, int]]
+    committers_values = [c_i[1] for c_i in committers_items]  # type: List[int]
+
+    if len(committers) > PUSH_COMMITTERS_LIMIT_INFO:
+        others_number_of_commits = sum(committers_values[PUSH_COMMITTERS_LIMIT_INFO:])
+        committers_items = committers_items[:PUSH_COMMITTERS_LIMIT_INFO]
+        committers_items.append(('others', others_number_of_commits))
+
+    return committers_items

zerver/webhooks/gitlab/tests.py
@@ -21,21 +21,34 @@ class GitlabHookTests(WebhookTestCase):
     def test_push_event_message(self):
         # type: () -> None
         expected_subject = u"my-awesome-project / tomek"
-        expected_message = u"Tomasz Kolek [pushed](https://gitlab.com/tomaszkolek0/my-awesome-project/compare/5fcdd5551fc3085df79bece2c32b1400802ac407...eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9) to branch tomek\n\n* b ([66abd2d](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/66abd2da28809ffa128ed0447965cf11d7f863a7))\n* c ([eb6ae1e](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9))"
+        expected_message = u"Tomasz Kolek [pushed](https://gitlab.com/tomaszkolek0/my-awesome-project/compare/5fcdd5551fc3085df79bece2c32b1400802ac407...eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9) 2 commits to branch tomek. Commits by Tomasz Kolek(2)\n\n* b ([66abd2d](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/66abd2da28809ffa128ed0447965cf11d7f863a7))\n* c ([eb6ae1e](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9))"
         self.send_and_test_stream_message('push', expected_subject, expected_message, HTTP_X_GITLAB_EVENT="Push Hook")
 
     def test_push_event_message_filtered_by_branches(self):
         # type: () -> None
         self.url = self.build_webhook_url('master,tomek')
         expected_subject = u"my-awesome-project / tomek"
-        expected_message = u"Tomasz Kolek [pushed](https://gitlab.com/tomaszkolek0/my-awesome-project/compare/5fcdd5551fc3085df79bece2c32b1400802ac407...eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9) to branch tomek\n\n* b ([66abd2d](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/66abd2da28809ffa128ed0447965cf11d7f863a7))\n* c ([eb6ae1e](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9))"
+        expected_message = u"Tomasz Kolek [pushed](https://gitlab.com/tomaszkolek0/my-awesome-project/compare/5fcdd5551fc3085df79bece2c32b1400802ac407...eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9) 2 commits to branch tomek. Commits by Tomasz Kolek(2)\n\n* b ([66abd2d](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/66abd2da28809ffa128ed0447965cf11d7f863a7))\n* c ([eb6ae1e](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9))"
         self.send_and_test_stream_message('push', expected_subject, expected_message, HTTP_X_GITLAB_EVENT="Push Hook")
+
+    def test_push_multiple_committers(self):
+        # type: () -> None
+        expected_subject = u"my-awesome-project / tomek"
+        expected_message = u"Tomasz Kolek [pushed](https://gitlab.com/tomaszkolek0/my-awesome-project/compare/5fcdd5551fc3085df79bece2c32b1400802ac407...eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9) 2 commits to branch tomek. Commits by Ben(1) and Tomasz Kolek(1)\n\n* b ([66abd2d](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/66abd2da28809ffa128ed0447965cf11d7f863a7))\n* c ([eb6ae1e](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9))"
+        self.send_and_test_stream_message('push_multiple_committers', expected_subject, expected_message, HTTP_X_GITLAB_EVENT="Push Hook")
+
+    def test_push_multiple_committers_with_others(self):
+        # type: () -> None
+        expected_subject = u"my-awesome-project / tomek"
+        commit_info = u"* b ([eb6ae1e](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9))\n"
+        expected_message = u"Tomasz Kolek [pushed](https://gitlab.com/tomaszkolek0/my-awesome-project/compare/5fcdd5551fc3085df79bece2c32b1400802ac407...eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9) 7 commits to branch tomek. Commits by Ben(3), baxterthehacker(2), Tomasz Kolek(1) and others(1)\n\n{}* b ([eb6ae1e](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9))".format(commit_info * 6)
+        self.send_and_test_stream_message('push_multiple_committers_with_others', expected_subject, expected_message, HTTP_X_GITLAB_EVENT="Push Hook")
 
     def test_push_commits_more_than_limit_event_message(self):
         # type: () -> None
         expected_subject = u"my-awesome-project / tomek"
         commits_info = u'* b ([66abd2d](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/66abd2da28809ffa128ed0447965cf11d7f863a7))\n'
-        expected_message = u"Tomasz Kolek [pushed](https://gitlab.com/tomaszkolek0/my-awesome-project/compare/5fcdd5551fc3085df79bece2c32b1400802ac407...eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9) to branch tomek\n\n{}[and {} more commit(s)]".format(
+        expected_message = u"Tomasz Kolek [pushed](https://gitlab.com/tomaszkolek0/my-awesome-project/compare/5fcdd5551fc3085df79bece2c32b1400802ac407...eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9) 50 commits to branch tomek. Commits by Tomasz Kolek(50)\n\n{}[and {} more commit(s)]".format(
             commits_info * COMMITS_LIMIT,
             50 - COMMITS_LIMIT,
         )
@@ -46,7 +59,7 @@ class GitlabHookTests(WebhookTestCase):
         self.url = self.build_webhook_url('master,tomek')
         expected_subject = u"my-awesome-project / tomek"
         commits_info = u'* b ([66abd2d](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/66abd2da28809ffa128ed0447965cf11d7f863a7))\n'
-        expected_message = u"Tomasz Kolek [pushed](https://gitlab.com/tomaszkolek0/my-awesome-project/compare/5fcdd5551fc3085df79bece2c32b1400802ac407...eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9) to branch tomek\n\n{}[and {} more commit(s)]".format(
+        expected_message = u"Tomasz Kolek [pushed](https://gitlab.com/tomaszkolek0/my-awesome-project/compare/5fcdd5551fc3085df79bece2c32b1400802ac407...eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9) 50 commits to branch tomek. Commits by Tomasz Kolek(50)\n\n{}[and {} more commit(s)]".format(
             commits_info * COMMITS_LIMIT,
             50 - COMMITS_LIMIT,
         )

zerver/webhooks/beanstalk/tests.py
@@ -10,17 +10,29 @@ class BeanstalkHookTests(WebhookTestCase):
     def test_git_single(self):
         # type: () -> None
         expected_subject = "work-test / master"
-        expected_message = """Leo Franchi [pushed](http://lfranchi-svn.beanstalkapp.com/work-test) to branch master
+        expected_message = """Leo Franchi [pushed](http://lfranchi-svn.beanstalkapp.com/work-test) 1 commit to branch master. Commits by Leo Franchi(1)
 
 * add some stuff ([e50508d](http://lfranchi-svn.beanstalkapp.com/work-test/changesets/e50508df))"""
         self.send_and_test_stream_message('git_singlecommit', expected_subject, expected_message,
                                           content_type=None,
                                           **self.api_auth(self.TEST_USER_EMAIL))
 
+    def test_git_multiple_committers(self):
+        # type: () -> None
+        expected_subject = "work-test / master"
+        expected_message = """Leo Franchi [pushed](http://lfranchi-svn.beanstalkapp.com/work-test) 3 commits to branch master. Commits by Leo Franchi(2) and Tomasz Kolek(1)
+
+* Added new file ([edf529c](http://lfranchi-svn.beanstalkapp.com/work-test/changesets/edf529c7))
+* Filled in new file with some stuff ([c2a191b](http://lfranchi-svn.beanstalkapp.com/work-test/changesets/c2a191b9))
+* More work to fix some bugs ([2009815](http://lfranchi-svn.beanstalkapp.com/work-test/changesets/20098158))"""
+        self.send_and_test_stream_message('git_multiple_committers', expected_subject, expected_message,
+                                          content_type=None,
+                                          **self.api_auth(self.TEST_USER_EMAIL))
+
     def test_git_multiple(self):
         # type: () -> None
         expected_subject = "work-test / master"
-        expected_message = """Leo Franchi [pushed](http://lfranchi-svn.beanstalkapp.com/work-test) to branch master
+        expected_message = """Leo Franchi [pushed](http://lfranchi-svn.beanstalkapp.com/work-test) 3 commits to branch master. Commits by Leo Franchi(3)
 
 * Added new file ([edf529c](http://lfranchi-svn.beanstalkapp.com/work-test/changesets/edf529c7))
 * Filled in new file with some stuff ([c2a191b](http://lfranchi-svn.beanstalkapp.com/work-test/changesets/c2a191b9))
@@ -33,7 +45,7 @@ class BeanstalkHookTests(WebhookTestCase):
         # type: () -> None
         commits_info = "* add some stuff ([e50508d](http://lfranchi-svn.beanstalkapp.com/work-test/changesets/e50508df))\n"
         expected_subject = "work-test / master"
-        expected_message = """Leo Franchi [pushed](http://lfranchi-svn.beanstalkapp.com/work-test) to branch master
+        expected_message = """Leo Franchi [pushed](http://lfranchi-svn.beanstalkapp.com/work-test) 50 commits to branch master. Commits by Leo Franchi(50)
 
 {}[and {} more commit(s)]""".format((commits_info * COMMITS_LIMIT), 50 - COMMITS_LIMIT)
         self.send_and_test_stream_message('git_morethanlimitcommits', expected_subject, expected_message,

zerver/webhooks/bitbucket/tests.py
@@ -15,7 +15,7 @@ class BitbucketHookTests(WebhookTestCase):
         fixture_name = 'push'
         self.url = self.build_url(fixture_name)
         commit_info = u'* c ([25f93d2](https://bitbucket.org/kolaszek/repository-name/commits/25f93d22b719e2d678a7ad5ee0ef0d1fcdf39c12))'
-        expected_message = u"kolaszek pushed to branch master\n\n{}".format(commit_info)
+        expected_message = u"kolaszek pushed 1 commit to branch master. Commits by kolaszek(1)\n\n{}".format(commit_info)
         self.send_and_test_stream_message(fixture_name, self.EXPECTED_SUBJECT_BRANCH_EVENTS, expected_message, **self.api_auth(self.TEST_USER_EMAIL))
 
     def test_bitbucket_on_push_commits_above_limit_event(self):
@@ -23,7 +23,7 @@ class BitbucketHookTests(WebhookTestCase):
         fixture_name = 'push_commits_above_limit'
         self.url = self.build_url(fixture_name)
         commit_info = u'* c ([25f93d2](https://bitbucket.org/kolaszek/repository-name/commits/25f93d22b719e2d678a7ad5ee0ef0d1fcdf39c12))\n'
-        expected_message = u"kolaszek pushed to branch master\n\n{}[and 30 more commit(s)]".format(commit_info * 20)
+        expected_message = u"kolaszek pushed 50 commits to branch master. Commits by kolaszek(50)\n\n{}[and 30 more commit(s)]".format(commit_info * 20)
         self.send_and_test_stream_message(fixture_name, self.EXPECTED_SUBJECT_BRANCH_EVENTS, expected_message, **self.api_auth(self.TEST_USER_EMAIL))
 
     def test_bitbucket_on_force_push_event(self):

zerver/webhooks/bitbucket2/tests.py
@@ -13,13 +13,25 @@ class Bitbucket2HookTests(WebhookTestCase):
     def test_bitbucket2_on_push_event(self):
         # type: () -> None
         commit_info = u'* first commit ([84b96ad](https://bitbucket.org/kolaszek/repository-name/commits/84b96adc644a30fd6465b3d196369d880762afed))'
-        expected_message = u"kolaszek [pushed](https://bitbucket.org/kolaszek/repository-name/branch/master) to branch master\n\n{}".format(commit_info)
+        expected_message = u"kolaszek [pushed](https://bitbucket.org/kolaszek/repository-name/branch/master) 1 commit to branch master. Commits by Tomasz(1)\n\n{}".format(commit_info)
         self.send_and_test_stream_message('v2_push', self.EXPECTED_SUBJECT_BRANCH_EVENTS, expected_message)
+
+    def test_bitbucket2_on_push_commits_multiple_committers(self):
+        # type: () -> None
+        commit_info = u'* first commit ([84b96ad](https://bitbucket.org/kolaszek/repository-name/commits/84b96adc644a30fd6465b3d196369d880762afed))\n'
+        expected_message = u"""kolaszek [pushed](https://bitbucket.org/kolaszek/repository-name/branch/master) 3 commits to branch master. Commits by Benjamin(2) and Tomasz(1)\n\n{}* first commit ([84b96ad](https://bitbucket.org/kolaszek/repository-name/commits/84b96adc644a30fd6465b3d196369d880762afed))""".format(commit_info*2)
+        self.send_and_test_stream_message('v2_push_multiple_committers', self.EXPECTED_SUBJECT_BRANCH_EVENTS, expected_message)
+
+    def test_bitbucket2_on_push_commits_multiple_committers_with_others(self):
+        # type: () -> None
+        commit_info = u'* first commit ([84b96ad](https://bitbucket.org/kolaszek/repository-name/commits/84b96adc644a30fd6465b3d196369d880762afed))\n'
+        expected_message = u"""kolaszek [pushed](https://bitbucket.org/kolaszek/repository-name/branch/master) 10 commits to branch master. Commits by Tomasz(4), James(3), Brendon(2) and others(1)\n\n{}* first commit ([84b96ad](https://bitbucket.org/kolaszek/repository-name/commits/84b96adc644a30fd6465b3d196369d880762afed))""".format(commit_info*9)
+        self.send_and_test_stream_message('v2_push_multiple_committers_with_others', self.EXPECTED_SUBJECT_BRANCH_EVENTS, expected_message)
 
     def test_bitbucket2_on_push_commits_above_limit_event(self):
         # type: () -> None
         commit_info = '* a ([6f161a7](https://bitbucket.org/kolaszek/repository-name/commits/6f161a7bced94430ac8947d87dbf45c6deee3fb0))\n'
-        expected_message = u"kolaszek [pushed](https://bitbucket.org/kolaszek/repository-name/branches/compare/6f161a7bced94430ac8947d87dbf45c6deee3fb0..1221f2fda6f1e3654b09f1f3a08390e4cb25bb48) to branch master\n\n{}[and more commit(s)]".format(
+        expected_message = u"kolaszek [pushed](https://bitbucket.org/kolaszek/repository-name/branches/compare/6f161a7bced94430ac8947d87dbf45c6deee3fb0..1221f2fda6f1e3654b09f1f3a08390e4cb25bb48) 5 commits to branch master. Commits by Tomasz(5)\n\n{}[and more commit(s)]".format(
             (commit_info * 5),
         )
         self.send_and_test_stream_message('v2_push_commits_above_limit', self.EXPECTED_SUBJECT_BRANCH_EVENTS, expected_message)
@@ -173,7 +185,7 @@ class Bitbucket2HookTests(WebhookTestCase):
         }
         self.send_and_test_stream_message('v2_more_than_one_push_event', **kwargs)
         msg = self.get_second_to_last_message()
-        self.do_test_message(msg, 'kolaszek [pushed](https://bitbucket.org/kolaszek/repository-name/branch/master) to branch master\n\n* first commit ([84b96ad](https://bitbucket.org/kolaszek/repository-name/commits/84b96adc644a30fd6465b3d196369d880762afed))')
+        self.do_test_message(msg, 'kolaszek [pushed](https://bitbucket.org/kolaszek/repository-name/branch/master) 1 commit to branch master. Commits by Tomasz(1)\n\n* first commit ([84b96ad](https://bitbucket.org/kolaszek/repository-name/commits/84b96adc644a30fd6465b3d196369d880762afed))')
         self.do_test_subject(msg, self.EXPECTED_SUBJECT_BRANCH_EVENTS)
         msg = self.get_last_message()
         self.do_test_message(msg, 'kolaszek pushed tag [a](https://bitbucket.org/kolaszek/repository-name/commits/tag/a)')

zerver/webhooks/github/tests.py
@@ -12,7 +12,7 @@ class GithubV1HookTests(WebhookTestCase):
     SEND_STREAM = False
     BRANCHES = None # type: Optional[Text]
 
-    push_content = u"""zbenjamin [pushed](https://github.com/zbenjamin/zulip-test/compare/4f9adc4777d5...b95449196980) to branch master
+    push_content = u"""zbenjamin [pushed](https://github.com/zbenjamin/zulip-test/compare/4f9adc4777d5...b95449196980) 3 commits to branch master. Commits by zbenjamin(3)
 
 * Add baz ([48c329a](https://github.com/zbenjamin/zulip-test/commit/48c329a0b68a9a379ff195ee3f1c1f4ab0b2a89e))
 * Baz needs to be longer ([06ebe5f](https://github.com/zbenjamin/zulip-test/commit/06ebe5f472a32f6f31fd2a665f0c7442b69cce72))
@@ -78,7 +78,7 @@ class GithubV1HookTests(WebhookTestCase):
     def test_push_multiple_commits(self):
         # type: () -> None
         commit_info = "* Add baz ([48c329a](https://github.com/zbenjamin/zulip-test/commit/48c329a0b68a9a379ff195ee3f1c1f4ab0b2a89e))\n"
-        expected_subject = "zbenjamin [pushed](https://github.com/zbenjamin/zulip-test/compare/4f9adc4777d5...b95449196980) to branch master\n\n{}[and {} more commit(s)]".format(
+        expected_subject = "zbenjamin [pushed](https://github.com/zbenjamin/zulip-test/compare/4f9adc4777d5...b95449196980) 50 commits to branch master. Commits by zbenjamin(50)\n\n{}[and {} more commit(s)]".format(
             commit_info * COMMITS_LIMIT,
             50 - COMMITS_LIMIT,
         )
@@ -152,7 +152,7 @@ class GithubV2HookTests(WebhookTestCase):
     SEND_STREAM = False
     BRANCHES = None # type: Optional[Text]
 
-    push_content = """zbenjamin [pushed](https://github.com/zbenjamin/zulip-test/compare/4f9adc4777d5...b95449196980) to branch master
+    push_content = """zbenjamin [pushed](https://github.com/zbenjamin/zulip-test/compare/4f9adc4777d5...b95449196980) 3 commits to branch master. Commits by zbenjamin(3)
 
 * Add baz ([48c329a](https://github.com/zbenjamin/zulip-test/commit/48c329a0b68a9a379ff195ee3f1c1f4ab0b2a89e))
 * Baz needs to be longer ([06ebe5f](https://github.com/zbenjamin/zulip-test/commit/06ebe5f472a32f6f31fd2a665f0c7442b69cce72))
@@ -214,11 +214,23 @@ class GithubV2HookTests(WebhookTestCase):
     def test_push_multiple_commits(self):
         # type: () -> None
         commit_info = "* Add baz ([48c329a](https://github.com/zbenjamin/zulip-test/commit/48c329a0b68a9a379ff195ee3f1c1f4ab0b2a89e))\n"
-        expected_subject = "zbenjamin [pushed](https://github.com/zbenjamin/zulip-test/compare/4f9adc4777d5...b95449196980) to branch master\n\n{}[and {} more commit(s)]".format(
+        expected_subject = "zbenjamin [pushed](https://github.com/zbenjamin/zulip-test/compare/4f9adc4777d5...b95449196980) 50 commits to branch master. Commits by zbenjamin(50)\n\n{}[and {} more commit(s)]".format(
             commit_info * COMMITS_LIMIT,
             50 - COMMITS_LIMIT,
         )
         self.basic_test('push_commits_more_than_limit', 'commits', 'zulip-test / master', expected_subject)
+
+    def test_push_multiple_committers(self):
+        # type: () -> None
+        commit_info = "* Add baz ([48c329a](https://github.com/zbenjamin/zulip-test/commit/48c329a0b68a9a379ff195ee3f1c1f4ab0b2a89e))\n"
+        expected_subject = "zbenjamin [pushed](https://github.com/zbenjamin/zulip-test/compare/4f9adc4777d5...b95449196980) 6 commits to branch master. Commits by tomasz(3), baxthehacker(2) and zbenjamin(1)\n\n{}* Add baz ([48c329a](https://github.com/zbenjamin/zulip-test/commit/48c329a0b68a9a379ff195ee3f1c1f4ab0b2a89e))".format(commit_info * 5)
+        self.basic_test('push_multiple_committers', 'commits', 'zulip-test / master', expected_subject)
+
+    def test_push_multiple_committers_with_others(self):
+        # type: () -> None
+        commit_info = "* Final edit to baz, I swear ([b954491](https://github.com/zbenjamin/zulip-test/commit/b95449196980507f08209bdfdc4f1d611689b7a8))\n"
+        expected_subject = "zbenjamin [pushed](https://github.com/zbenjamin/zulip-test/compare/4f9adc4777d5...b95449196980) 10 commits to branch master. Commits by baxthehacker(4), James(3), Tomasz(2) and others(1)\n\n{}* Final edit to baz, I swear ([b954491](https://github.com/zbenjamin/zulip-test/commit/b95449196980507f08209bdfdc4f1d611689b7a8))".format(commit_info * 9)
+        self.basic_test('push_multiple_committers_with_others', 'commits', 'zulip-test / master', expected_subject)
 
     def test_legacy_hook(self):
         # type: () -> None

commit b7d7b79ca02e36d6ddc6a75bc9a7ec5199d6496d

git: Fix nondeterministic ordering of commit authors.
This should fix the nondeterministic test failures introduced by
6a13ee819ce0e6fe3532381e3487d11eb625f491.

zerver/lib/webhooks/git.py
@@ -190,7 +190,9 @@ def get_all_committers(commits_data):
     for commit in commits_data:
         committers[commit['name']] += 1
 
-    committers_items = sorted(list(committers.items()), key=lambda item: item[1], reverse=True)  # type: List[Tuple[str, int]]
+    # Sort by commit count, breaking ties alphabetically.
+    committers_items = sorted(list(committers.items()),
+                              key=lambda item: (-item[1], item[0]))  # type: List[Tuple[str, int]]
     committers_values = [c_i[1] for c_i in committers_items]  # type: List[int]
 
     if len(committers) > PUSH_COMMITTERS_LIMIT_INFO:

zerver/webhooks/gitlab/tests.py
@@ -41,7 +41,7 @@ class GitlabHookTests(WebhookTestCase):
         # type: () -> None
         expected_subject = u"my-awesome-project / tomek"
         commit_info = u"* b ([eb6ae1e](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9))\n"
-        expected_message = u"Tomasz Kolek [pushed](https://gitlab.com/tomaszkolek0/my-awesome-project/compare/5fcdd5551fc3085df79bece2c32b1400802ac407...eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9) 7 commits to branch tomek. Commits by Ben(3), baxterthehacker(2), Tomasz Kolek(1) and others(1)\n\n{}* b ([eb6ae1e](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9))".format(commit_info * 6)
+        expected_message = u"Tomasz Kolek [pushed](https://gitlab.com/tomaszkolek0/my-awesome-project/compare/5fcdd5551fc3085df79bece2c32b1400802ac407...eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9) 7 commits to branch tomek. Commits by Ben(3), baxterthehacker(2), James(1) and others(1)\n\n{}* b ([eb6ae1e](https://gitlab.com/tomaszkolek0/my-awesome-project/commit/eb6ae1e591e0819dc5bf187c6bfe18ec065a80e9))".format(commit_info * 6)
         self.send_and_test_stream_message('push_multiple_committers_with_others', expected_subject, expected_message, HTTP_X_GITLAB_EVENT="Push Hook")
 
     def test_push_commits_more_than_limit_event_message(self):