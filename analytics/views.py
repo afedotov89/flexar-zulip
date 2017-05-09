afedotov89/flexar-zulip

commit 9b21b2925ff97853882a8cc11547fff22efef91e

analytics: Fix nondeterministic ordering of labels.

analytics/views.py
@@ -121,7 +121,7 @@ def sort_by_totals(value_arrays):
     totals = []
     for label, values in value_arrays.items():
         totals.append((label, sum(values)))
-    totals.sort(key=lambda label_total: label_total[1], reverse=True)
+    totals.sort(key=lambda label_total: "%s:%s" % (label_total[1], label_total[0]), reverse=True)
     return [label for label, total in totals]
 
 # For any given user, we want to show a fixed set of clients in the chart,

commit b3e05a75f6d14ad369f02f936204f7eea4c14c2b

analytics: Fix sort function in views.sort_client_labels.
sort_client_labels sorts first by total, and then to ensure deterministic
outcomes, sorts (reverse) alphabetically by label.

Fixes regression introduced in 9b21b29.

analytics/views.py
@@ -117,11 +117,9 @@ def get_chart_data(request, user_profile, chart_name=REQ(),
 
 def sort_by_totals(value_arrays):
     # type: (Dict[str, List[int]]) -> List[str]
-    totals = []
-    for label, values in value_arrays.items():
-        totals.append((label, sum(values)))
-    totals.sort(key=lambda label_total: "%s:%s" % (label_total[1], label_total[0]), reverse=True)
-    return [label for label, total in totals]
+    totals = [(sum(values), label) for label, values in value_arrays.items()]
+    totals.sort(reverse=True)
+    return [label for total, label in totals]
 
 # For any given user, we want to show a fixed set of clients in the chart,
 # regardless of the time aggregation or whether we're looking at realm or