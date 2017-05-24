vabs22/zulip

Outgoing Webhook System: Add support for personal message triggers.

zerver/tests/test_outgoing_webhook_system.py
@@ -3,16 +3,17 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import mock
-from typing import Any
+from typing import Any, Union, Text, List, Mapping, Callable
 
 from zerver.lib.outgoing_webhook import (
     do_rest_call,
     get_outgoing_webhook_service_handler,
     ServiceMessageActions,
 )
+from zerver.lib.actions import check_send_message
 from zerver.lib.test_helpers import get_user_profile_by_email
 from zerver.lib.test_classes import ZulipTestCase
-from zerver.models import Services
+from zerver.models import Services, Recipient, Client, UserProfile, get_realm, Subscription
 from zerver.outgoing_webhooks import (
     GENERIC_INTERFACE,
     AVAILABLE_OUTGOING_WEBHOOK_INTERFACES,
@@ -20,6 +21,7 @@ from zerver.outgoing_webhooks import (
 )
 
 import requests
+import six
 
 service = Services.objects.create(service_name="test",
                                   base_url="http://requestb.in/18av3gx1",
@@ -117,3 +119,74 @@ class OutgoingWebhookServiceHandlerTests(ZulipTestCase):
         self.assertEqual(service_handler.service_api_key, generic_interface.service_api_key)
         self.assertEqual(service_handler.bot_email, generic_interface.bot_email)
         self.assertEqual(service_handler.service_name, generic_interface.service_name)
+
+
+class PersonalMessageTriggersTest(ZulipTestCase):
+
+    @classmethod
+    def setUpClass(self):
+        # type: () -> None
+        self.bot_user = UserProfile.objects.create(full_name='The Test Bot',
+                                                   short_name='bot',
+                                                   email="testvabs-bot@zulip.com",
+                                                   is_bot=True,
+                                                   bot_type=UserProfile.OUTGOING_WEBHOOK_BOT,
+                                                   is_active=True,
+                                                   pointer=1,
+                                                   realm=get_realm("zulip"))
+
+        self.recipient = Recipient.objects.create(type=Recipient.PERSONAL,
+                                                  type_id=self.bot_user.id)
+
+        self.subscription = Subscription.objects.create(recipient=self.recipient,
+                                                        user_profile=self.bot_user,
+                                                        active=True)
+
+    @classmethod
+    def tearDownClass(self):
+        # type: () -> None
+        self.bot_user.delete()
+        self.recipient.delete()
+        self.subscription.delete()
+
+    def test_trigger_on_stream_message(self):
+        # type: () -> None
+        sender_email = "othello@zulip.com"
+        recipients = "Denmark"
+        message_type = Recipient.STREAM
+        with mock.patch('zerver.lib.actions.queue_json_publish') as queue_json_publish:
+            self.send_message(sender_email, recipients, message_type)
+            self.assertFalse(queue_json_publish.called)
+
+    def test_trigger_on_private_message_by_bot(self):
+        # type: () -> None
+        sender_email = "testvabs-bot@zulip.com"
+        recipients = "othello@zulip.com"
+        message_type = Recipient.PERSONAL
+
+        with mock.patch('zerver.lib.actions.queue_json_publish') as queue_json_publish:
+            self.send_message(sender_email, recipients, message_type)
+            self.assertFalse(queue_json_publish.called)
+
+    @mock.patch('zerver.lib.actions.queue_json_publish')
+    def test_trigger_on_private_message_by_user(self, mock_queue_json_publish):
+        # type: (mock.Mock) -> None
+        sender_email = "othello@zulip.com"
+        recipients = "testvabs-bot@zulip.com"
+        message_type = Recipient.PERSONAL
+
+        def check_values_passed(queue_name, trigger_event, x):
+            # type: (Any, Union[Mapping[Any, Any], Any], Callable[[Any], None]) -> None
+            self.assertEqual(queue_name, "outgoing_webhooks")
+            self.assertEqual(trigger_event['bot_email'], recipients)
+            self.assertEqual(trigger_event["command"], "test")
+            self.assertEqual(trigger_event["retry"], 0)
+            self.assertEqual(trigger_event["service_name"], None)
+            self.assertEqual(trigger_event["message"]["sender_email"], sender_email)
+            self.assertEqual(trigger_event["message"]["type"], self.recipient.type_name())
+            self.assertEqual(trigger_event["message"]["sender_realm_str"], self.bot_user.realm.string_id)
+            self.assertEqual(trigger_event["message"]["recipient"][0]["email"], recipients)
+
+        mock_queue_json_publish.side_effect = check_values_passed
+        self.send_message(sender_email, recipients, message_type, subject='', content='test')
+        self.assertTrue(mock_queue_json_publish.called)

afedotov89/flexar-zulip

zerver/tests/test_outgoing_webhook_system.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import mock
-from typing import Any, Union, Mapping, Callable
+from typing import Any, Union, Mapping, Callable, Text, List
 
 from zerver.lib.test_classes import ZulipTestCase
 
@@ -110,3 +110,87 @@ class TestMentionMessageTrigger(ZulipTestCase):
         self.subscribe_to_stream(self.bot_profile.email, "Denmark")
         self.send_message(self.user_profile.email, "Denmark", Recipient.STREAM, self.content)
         self.assertTrue(mock_queue_json_publish.called)
+
+class PersonalMessageTriggersTest(ZulipTestCase):
+
+    def setUp(self):
+        # type: () -> None
+        user_profile = self.example_user("othello")
+        self.bot_user = do_create_user(email="testvabs-bot@zulip.com",
+                                       password="test",
+                                       realm=get_realm_by_email_domain("zulip.com"),
+                                       full_name="The Test Bot",
+                                       short_name="bot",
+                                       bot_type=UserProfile.OUTGOING_WEBHOOK_BOT,
+                                       bot_owner=user_profile)
+        self.temp_bot = do_create_user(email="temp-bot@zulip.com",
+                                       password="temp",
+                                       realm=get_realm_by_email_domain("zulip.com"),
+                                       full_name="The Temp test Bot",
+                                       short_name="tempbot",
+                                       bot_type=UserProfile.OUTGOING_WEBHOOK_BOT,
+                                       bot_owner=user_profile)
+
+    def test_no_trigger_on_stream_message(self):
+        # type: () -> None
+        sender_email = "othello@zulip.com"
+        recipients = "Denmark"
+        message_type = Recipient.STREAM
+        with mock.patch('zerver.lib.actions.queue_json_publish') as queue_json_publish:
+            self.send_message(sender_email, recipients, message_type)
+            self.assertFalse(queue_json_publish.called)
+
+    def test_no_trigger_on_message_by_bot(self):
+        # type: () -> None
+        sender_email = "testvabs-bot@zulip.com"
+        recipients = "othello@zulip.com"
+        message_type = Recipient.PERSONAL
+
+        with mock.patch('zerver.lib.actions.queue_json_publish') as queue_json_publish:
+            self.send_message(sender_email, recipients, message_type)
+            self.assertFalse(queue_json_publish.called)
+
+    @mock.patch('zerver.lib.actions.queue_json_publish')
+    def test_trigger_on_private_message_by_user(self, mock_queue_json_publish):
+        # type: (mock.Mock) -> None
+        sender_email = "othello@zulip.com"
+        recipients = "testvabs-bot@zulip.com"
+        message_type = Recipient.PERSONAL
+        profile_id = self.bot_user.id
+
+        def check_values_passed(queue_name, trigger_event, x):
+            # type: (Any, Union[Mapping[Any, Any], Any], Callable[[Any], None]) -> None
+            self.assertEqual(queue_name, "outgoing_webhooks")
+            self.assertEqual(trigger_event["user_profile_id"], profile_id)
+            self.assertEqual(trigger_event["trigger"], "private_message")
+            self.assertEqual(trigger_event["failed_tries"], 0)
+            self.assertEqual(trigger_event["message"]["sender_email"], sender_email)
+            self.assertEqual(trigger_event["message"]["display_recipient"][0]["email"], sender_email)
+            self.assertEqual(trigger_event["message"]["display_recipient"][1]["email"], recipients)
+            self.assertEqual(trigger_event["message"]["type"], u'private')
+
+        mock_queue_json_publish.side_effect = check_values_passed
+        self.send_message(sender_email, recipients, message_type, subject='', content='test')
+        self.assertTrue(mock_queue_json_publish.called)
+
+    @mock.patch('zerver.lib.actions.queue_json_publish')
+    def test_trigger_on_huddle_message_by_user(self, mock_queue_json_publish):
+        # type: (mock.Mock) -> None
+        sender_email = "othello@zulip.com"
+        recipients = [u"testvabs-bot@zulip.com", u"temp-bot@zulip.com"]
+        message_type = Recipient.HUDDLE
+        profile_ids = [self.bot_user.id, self.temp_bot.id]
+
+        def check_values_passed(queue_name, trigger_event, x):
+            # type: (Any, Union[Mapping[Any, Any], Any], Callable[[Any], None]) -> None
+            self.assertEqual(queue_name, "outgoing_webhooks")
+            self.assertIn(trigger_event["user_profile_id"], profile_ids)
+            profile_ids.remove(trigger_event["user_profile_id"])
+            self.assertEqual(trigger_event["trigger"], "private_message")
+            self.assertEqual(trigger_event["failed_tries"], 0)
+            self.assertEqual(trigger_event["message"]["sender_email"], sender_email)
+            self.assertEqual(trigger_event["message"]["type"], u'private')
+
+        mock_queue_json_publish.side_effect = check_values_passed
+        self.send_message(sender_email, recipients, message_type, subject='', content='test')
+        self.assertEqual(mock_queue_json_publish.call_count, 2)