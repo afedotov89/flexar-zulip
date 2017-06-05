afedotov89/flexar-zulip

commit dd6ea91a9b7dbd98291d1007e734fcaa5442a28c

Remove date separators from the top of the message feed.
Now that we have the date visible in the recipient rows, we no longer
need a top-of-feed date separator.

Fixes #4581.

static/js/message_list_view.js
@@ -71,8 +71,11 @@ function add_display_time(group, message_container, prev) {
         if (time.toDateString() !== prev_time.toDateString()) {
             // NB: show_date is HTML, inserted into the document without escaping.
             group.show_date = (timerender.render_date(time, prev_time))[0].outerHTML;
+            group.show_date_separator = true;
         }
     } else {
+        // Show the date in the recipient bar, but not a date separator bar.
+        group.show_date_separator = false;
         group.show_date = (timerender.render_date(time))[0].outerHTML;
     }
 
@@ -360,7 +363,8 @@ MessageListView.prototype = {
 
                 if (same_day(last_msg_container, first_msg_container)) {
                     // Clear the date if it is the same as the last group
-                    second_group.show_date = undefined;
+                    delete second_group.show_date;
+                    delete second_group.show_date_separator;
                 }
             }
             message_actions.append_groups = new_message_groups;

frontend_tests/node_tests/templates.js
@@ -649,6 +649,7 @@ function render(template_name, args) {
             message_ids: [1, 2],
             message_containers: messages,
             show_date: '"<span id="timerender82">Jan&nbsp;07</span>"',
+            show_date_separator: true,
             subject: 'two messages',
             match_subject: '<span class="highlight">two</span> messages',
         },

static/templates/message_group.handlebars
@@ -3,7 +3,7 @@
 {{#each message_groups}}
     {{#with this}}
 
-        {{#if show_date}}
+        {{#if show_date_separator}}
         <div class="date_row">{{{show_date}}}</div>
         {{/if}}
 

commit 58919ea40ca7c17a47cd14d0a0f32f2d0cb86684

message_list: Fix missing date boundaries for newly sent messages.
When receiving the first new message of a new day, we were previously
not showing a date separator line before the message.

Fixes a regression introduced
in dd6ea91a9b7dbd98291d1007e734fcaa5442a28c.

static/js/message_list_view.js
@@ -368,6 +368,11 @@ MessageListView.prototype = {
                     // Clear the date if it is the same as the last group
                     delete second_group.show_date;
                     delete second_group.show_date_separator;
+                } else {
+                    // If we just sent the first message on a new day
+                    // in a narrow, make sure we render a date separator.
+                    add_display_time(second_group, first_msg_container,
+                                     last_msg_container);
                 }
             }
             message_actions.append_groups = new_message_groups;