afedotov89/flexar-zulip

commit 2f9c2dc6619e3058366152be0db3d041b513c969

compose.js: Refactor validate_stream_message_address_info().
In this commit we just refactor validate_stream_message_address_info
function to early return in case of stream_data.is_subscribed()
returns true.

static/js/compose.js
@@ -509,29 +509,29 @@ function validate_stream_message_mentions(stream_name) {
 }
 
 exports.validate_stream_message_address_info = function (stream_name) {
-    var response;
-
-    if (!stream_data.is_subscribed(stream_name)) {
-        switch (check_stream_for_send(stream_name, page_params.narrow_stream !== undefined)) {
-        case "does-not-exist":
-            response = "<p>The stream <b>" +
-                Handlebars.Utils.escapeExpression(stream_name) + "</b> does not exist.</p>" +
-                "<p>Manage your subscriptions <a href='#streams/all'>on your Streams page</a>.</p>";
-            compose_error(response, $('#stream'));
-            return false;
-        case "error":
-            compose_error(i18n.t("Error checking subscription"), $("#stream"));
-            return false;
-        case "not-subscribed":
-            response = "<p>You're not subscribed to the stream <b>" +
-                Handlebars.Utils.escapeExpression(stream_name) + "</b>.</p>" +
-                "<p>Manage your subscriptions <a href='#streams/all'>on your Streams page</a>.</p>";
-            compose_error(response, $('#stream'));
-            return false;
-        }
+    if (stream_data.is_subscribed(stream_name)) {
+        return true;
     }
 
-    return true;
+    var response;
+
+    switch (check_stream_for_send(stream_name, page_params.narrow_stream !== undefined)) {
+    case "does-not-exist":
+        response = "<p>The stream <b>" +
+            Handlebars.Utils.escapeExpression(stream_name) + "</b> does not exist.</p>" +
+            "<p>Manage your subscriptions <a href='#streams/all'>on your Streams page</a>.</p>";
+        compose_error(response, $('#stream'));
+        return false;
+    case "error":
+        compose_error(i18n.t("Error checking subscription"), $("#stream"));
+        return false;
+    case "not-subscribed":
+        response = "<p>You're not subscribed to the stream <b>" +
+            Handlebars.Utils.escapeExpression(stream_name) + "</b>.</p>" +
+            "<p>Manage your subscriptions <a href='#streams/all'>on your Streams page</a>.</p>";
+        compose_error(response, $('#stream'));
+        return false;
+    }
 };
 
 function validate_stream_message() {

commit a9e9ca4a1e7ffc40915cf24a944ecb193831f412

compose.js: Add return true for subscribed streams.
This is basically going to fix a regression which was introduced in
2f9c2dc which made code early return in case of subscribed streams.

static/js/compose.js
@@ -530,6 +530,7 @@ exports.validate_stream_message_address_info = function (stream_name) {
         compose_error(response, $('#stream'));
         return false;
     }
+    return true;
 };
 
 function validate_stream_message() {