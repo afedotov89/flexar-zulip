afedotov89/flexar-zulip

commit d3bc791730d2d9e6ce3bc1e67b2f08022d99c63a

queue_processors: Extract LoopQueueProcessingWorker class.
This should help make it easier to add tests coverage for these queue
processors, since they now use a system more similar to the other
queue processors.

zerver/worker/queue_processors.py
@@ -171,6 +171,26 @@ class QueueProcessingWorker:
         # type: () -> None
         self.q.stop_consuming()
 
+class LoopQueueProcessingWorker(QueueProcessingWorker):
+    sleep_delay = 0
+
+    def start(self) -> None:
+        while True:
+            # TODO: Probably it'd be better to share code with consume_wrapper()
+            events = self.q.drain_queue(self.queue_name, json=True)
+            try:
+                self.consume_batch(events)
+            finally:
+                reset_queries()
+            time.sleep(self.sleep_delay)
+
+    def consume_batch(self, event: List[Dict[str, Any]]) -> None:
+        raise NotImplementedError
+
+    def consume(self, event: Dict[str, Any]) -> None:
+        """In LoopQueueProcessingWorker, consume is used just for automated tests"""
+        self.consume_batch([event])
+
 @assign_queue('signups')
 class SignupWorker(QueueProcessingWorker):
     def consume(self, data):
@@ -250,24 +270,21 @@ class UserPresenceWorker(QueueProcessingWorker):
         do_update_user_presence(user_profile, client, log_time, status)
 
 @assign_queue('missedmessage_emails', queue_type="loop")
-class MissedMessageWorker(QueueProcessingWorker):
-    def start(self):
-        # type: () -> None
-        while True:
-            missed_events = self.q.drain_queue("missedmessage_emails", json=True)
-            by_recipient = defaultdict(list)  # type: Dict[int, List[Dict[str, Any]]]
+class MissedMessageWorker(LoopQueueProcessingWorker):
+    # Aggregate all messages received every 2 minutes to let someone finish sending a batch
+    # of messages
+    sleep_delay = 2 * 60
 
-            for event in missed_events:
-                logging.debug("Received missedmessage_emails event: %s" % (event,))
-                by_recipient[event['user_profile_id']].append(event)
+    def consume_batch(self, missed_events: List[Dict[str, Any]]) -> None:
+        missed_events = self.q.drain_queue("missedmessage_emails", json=True)
+        by_recipient = defaultdict(list)  # type: Dict[int, List[Dict[str, Any]]]
 
-            for user_profile_id, events in by_recipient.items():
-                handle_missedmessage_emails(user_profile_id, events)
+        for event in missed_events:
+            logging.debug("Received missedmessage_emails event: %s" % (event,))
+            by_recipient[event['user_profile_id']].append(event)
 
-            reset_queries()
-            # Aggregate all messages received every 2 minutes to let someone finish sending a batch
-            # of messages
-            time.sleep(2 * 60)
+        for user_profile_id, events in by_recipient.items():
+            handle_missedmessage_emails(user_profile_id, events)
 
 @assign_queue('missedmessage_email_senders')
 class MissedMessageSendingWorker(QueueProcessingWorker):
@@ -303,18 +320,12 @@ class ErrorReporter(QueueProcessingWorker):
             do_report_error(event['report']['host'], event['type'], event['report'])
 
 @assign_queue('slow_queries', queue_type="loop")
-class SlowQueryWorker(QueueProcessingWorker):
-    def start(self):
-        # type: () -> None
-        while True:
-            self.process_one_batch()
-            # Aggregate all slow query messages in 1-minute chunks to avoid message spam
-            time.sleep(1 * 60)
+class SlowQueryWorker(LoopQueueProcessingWorker):
+    # Sleep 1 minute between checking the queue
+    sleep_delay = 60 * 1
 
-    def process_one_batch(self):
-        # type: () -> None
-        slow_queries = self.q.drain_queue("slow_queries", json=True)
-
+    def consume_batch(self, slow_queries):
+        # type: (List[Dict[str, Any]]) -> None
         for query in slow_queries:
             logging.info("Slow query: %s" % (query))
 
@@ -331,8 +342,6 @@ class SlowQueryWorker(QueueProcessingWorker):
             error_bot_realm = get_system_bot(settings.ERROR_BOT).realm
             internal_send_message(error_bot_realm, settings.ERROR_BOT,
                                   "stream", "logs", topic, content)
-
-        reset_queries()
 
 @assign_queue("message_sender")
 class MessageSenderWorker(QueueProcessingWorker):

commit d472bb016a51f73e34335f31fa9f79009656ae76

Fix MissedMessageWorker.
This fixes a regression in d3bc791730d2d9e6ce3bc1e67b2f08022d99c63a.

We were draining the queue in both the superclass and the subclass,
so by the time the subclass started processing events, there were
no events to process.  Now the subclass properly uses the events
passed in from the superclass.

zerver/worker/queue_processors.py
@@ -276,7 +276,6 @@ class MissedMessageWorker(LoopQueueProcessingWorker):
     sleep_delay = 2 * 60
 
     def consume_batch(self, missed_events: List[Dict[str, Any]]) -> None:
-        missed_events = self.q.drain_queue("missedmessage_emails", json=True)
         by_recipient = defaultdict(list)  # type: Dict[int, List[Dict[str, Any]]]
 
         for event in missed_events:

zerver/tests/test_queue_worker.py
@@ -18,7 +18,16 @@ from zerver.worker.queue_processors import (
     get_active_worker_queues,
     QueueProcessingWorker,
     LoopQueueProcessingWorker,
+    MissedMessageWorker,
 )
+
+Event = Dict[str, Any]
+
+# This is used for testing LoopQueueProcessingWorker, which
+# would run forever if we don't mock time.sleep to abort the
+# loop.
+class AbortLoop(Exception):
+    pass
 
 class WorkerTest(ZulipTestCase):
     class FakeClient:
@@ -35,6 +44,98 @@ class WorkerTest(ZulipTestCase):
             for queue_name, data in self.queue:
                 callback = self.consumers[queue_name]
                 callback(data)
+
+        def drain_queue(self, queue_name: str, json: bool) -> List[Event]:
+            assert json
+            events = [
+                dct
+                for (queue_name, dct)
+                in self.queue
+            ]
+
+            # IMPORTANT!
+            # This next line prevents us from double draining
+            # queues, which was a bug at one point.
+            self.queue = []
+
+            return events
+
+    def test_missed_message_worker(self) -> None:
+        cordelia = self.example_user('cordelia')
+        hamlet = self.example_user('hamlet')
+        othello = self.example_user('othello')
+
+        hamlet1_msg_id = self.send_personal_message(
+            from_email=cordelia.email,
+            to_email=hamlet.email,
+            content='hi hamlet',
+        )
+
+        hamlet2_msg_id = self.send_personal_message(
+            from_email=cordelia.email,
+            to_email=hamlet.email,
+            content='goodbye hamlet',
+        )
+
+        othello_msg_id = self.send_personal_message(
+            from_email=cordelia.email,
+            to_email=othello.email,
+            content='where art thou, othello?',
+        )
+
+        events = [
+            dict(user_profile_id=hamlet.id, message_id=hamlet1_msg_id),
+            dict(user_profile_id=hamlet.id, message_id=hamlet2_msg_id),
+            dict(user_profile_id=othello.id, message_id=othello_msg_id),
+        ]
+
+        fake_client = self.FakeClient()
+        for event in events:
+            fake_client.queue.append(('missedmessage_emails', event))
+
+        mmw = MissedMessageWorker()
+
+        time_mock = patch(
+            'zerver.worker.queue_processors.time.sleep',
+            side_effect=AbortLoop,
+        )
+
+        send_mock = patch(
+            'zerver.lib.notifications.do_send_missedmessage_events_reply_in_zulip'
+        )
+
+        with send_mock as sm, time_mock as tm:
+            with simulated_queue_client(lambda: fake_client):
+                try:
+                    mmw.setup()
+                    mmw.start()
+                except AbortLoop:
+                    pass
+
+        self.assertEqual(tm.call_args[0][0], 120)  # should sleep two minutes
+
+        args = [c[0] for c in sm.call_args_list]
+        arg_dict = {
+            arg[0].id: dict(
+                missed_messages=arg[1],
+                count=arg[2],
+            )
+            for arg in args
+        }
+
+        hamlet_info = arg_dict[hamlet.id]
+        self.assertEqual(hamlet_info['count'], 2)
+        self.assertEqual(
+            {m.content for m in hamlet_info['missed_messages']},
+            {'hi hamlet', 'goodbye hamlet'},
+        )
+
+        othello_info = arg_dict[othello.id]
+        self.assertEqual(othello_info['count'], 1)
+        self.assertEqual(
+            {m.content for m in othello_info['missed_messages']},
+            {'where art thou, othello?'}
+        )
 
     def test_mirror_worker(self) -> None:
         fake_client = self.FakeClient()