afedotov89/flexar-zulip

commit 2b433ee3d4648c40a1b5fd7fc55a13f4c2aaaed5

Add a couple cross-realm bots.
These are new:

    new-user-bot
    emailgateway

Our cross-realm bots are hard coded to have email addresses
in the `zulip.com` domain, and they're not part of ordinary
realms.

These have always been cross-realm, but new enforcement in the
frontend code of all messages having been sent by a known user means
that it's important to add these properly.

zerver/tests/test_events.py
@@ -2556,7 +2556,7 @@ class FetchQueriesTest(ZulipTestCase):
                     client_gravatar=False,
                 )
 
-        self.assert_length(queries, 30)
+        self.assert_length(queries, 32)
 
         expected_counts = dict(
             alert_words=0,
@@ -2575,7 +2575,7 @@ class FetchQueriesTest(ZulipTestCase):
             realm_embedded_bots=0,
             realm_emoji=1,
             realm_filters=1,
-            realm_user=4,
+            realm_user=6,
             realm_user_groups=1,
             stream=2,
             subscription=5,

zerver/tests/test_home.py
@@ -181,8 +181,8 @@ class HomeTest(ZulipTestCase):
             with patch('zerver.lib.cache.cache_set') as cache_mock:
                 result = self._get_home_page(stream='Denmark')
 
-        self.assert_length(queries, 42)
-        self.assert_length(cache_mock.call_args_list, 10)
+        self.assert_length(queries, 44)
+        self.assert_length(cache_mock.call_args_list, 12)
 
         html = result.content.decode('utf-8')
 
@@ -246,7 +246,7 @@ class HomeTest(ZulipTestCase):
         with queries_captured() as queries2:
             result = self._get_home_page()
 
-        self.assert_length(queries2, 36)
+        self.assert_length(queries2, 38)
 
         # Do a sanity check that our new streams were in the payload.
         html = result.content.decode('utf-8')
@@ -470,12 +470,28 @@ class HomeTest(ZulipTestCase):
         self.assertNotIn('defunct-1@zulip.com', active_emails)
 
         cross_bots = page_params['cross_realm_bots']
-        self.assertEqual(len(cross_bots), 3)
+        self.assertEqual(len(cross_bots), 5)
         cross_bots.sort(key=lambda d: d['email'])
 
         notification_bot = self.notification_bot()
 
-        self.assertEqual(cross_bots, [
+        by_email = lambda d: d['email']
+
+        self.assertEqual(sorted(cross_bots, key=by_email), sorted([
+            dict(
+                user_id=get_user('new-user-bot@zulip.com', get_realm('zulip')).id,
+                is_admin=False,
+                email='new-user-bot@zulip.com',
+                full_name='Zulip New User Bot',
+                is_bot=True
+            ),
+            dict(
+                user_id=get_user('emailgateway@zulip.com', get_realm('zulip')).id,
+                is_admin=False,
+                email='emailgateway@zulip.com',
+                full_name='Email Gateway',
+                is_bot=True
+            ),
             dict(
                 user_id=get_user('feedback@zulip.com', get_realm('zulip')).id,
                 is_admin=False,
@@ -497,7 +513,7 @@ class HomeTest(ZulipTestCase):
                 full_name='Welcome Bot',
                 is_bot=True
             ),
-        ])
+        ], key=by_email))
 
     def test_new_stream(self):
         # type: () -> None

zproject/settings.py
@@ -1474,6 +1474,12 @@ if PRODUCTION:
 # This is a debugging option only
 PROFILE_ALL_REQUESTS = False
 
-CROSS_REALM_BOT_EMAILS = set(('feedback@zulip.com', 'notification-bot@zulip.com', 'welcome-bot@zulip.com'))
+CROSS_REALM_BOT_EMAILS = {
+    'feedback@zulip.com',
+    'notification-bot@zulip.com',
+    'welcome-bot@zulip.com',
+    'new-user-bot@zulip.com',
+    'emailgateway@zulip.com',
+}
 
 CONTRIBUTORS_DATA = os.path.join(STATIC_ROOT, 'generated/github-contributors.json')

commit 3041d0b258ddf301e92df6db67ae1d89d6f537dd

cross_realm: Use bulk_get_users to fix handling of missing users.
This fixes a regression in 2b433ee3d4648c40a1b5fd7fc55a13f4c2aaaed5,
where Zulip would 500 if the newly added system bots didn't exist on
the server.

This also fixes a moderate size performance problem where we'd fetch 5
users from memcached or the database in a loop.

zerver/lib/actions.py
@@ -49,7 +49,7 @@ from zerver.lib.topic_mutes import (
     add_topic_mute,
     remove_topic_mute,
 )
-from zerver.lib.users import check_full_name
+from zerver.lib.users import bulk_get_users, check_full_name
 from zerver.lib.user_groups import create_user_group
 from zerver.models import Realm, RealmEmoji, Stream, UserProfile, UserActivity, \
     RealmDomain, \
@@ -3824,7 +3824,9 @@ def get_status_dict(requesting_user_profile):
 
 def get_cross_realm_dicts():
     # type: () -> List[Dict[str, Any]]
-    users = [get_system_bot(email) for email in get_cross_realm_emails()]
+    users = bulk_get_users(list(get_cross_realm_emails()), None,
+                           base_query=UserProfile.objects.filter(
+                               realm__string_id="zulip")).values()
     return [{'email': user.email,
              'user_id': user.id,
              'is_admin': user.is_realm_admin,

zerver/lib/users.py
@@ -1,10 +1,12 @@
-from typing import List, Text
+from typing import Dict, List, Optional, Text
 
+from django.db.models.query import QuerySet
 from django.utils.translation import ugettext as _
 
+from zerver.lib.cache import generic_bulk_cached_fetch
 from zerver.lib.request import JsonableError
 from zerver.models import UserProfile, Service, Realm, \
-    get_user_profile_by_id
+    get_user_profile_by_id, user_profile_by_email_cache_key
 
 def check_full_name(full_name_raw):
     # type: (Text) -> Text
@@ -33,6 +35,36 @@ def check_valid_interface_type(interface_type):
     # type: (int) -> None
     if interface_type not in Service.ALLOWED_INTERFACE_TYPES:
         raise JsonableError(_('Invalid interface type'))
+
+def bulk_get_users(emails: List[str], realm: Optional[Realm],
+                   base_query: 'QuerySet[UserProfile]'=None) -> Dict[str, UserProfile]:
+    if base_query is None:
+        assert realm is not None
+        base_query = UserProfile.objects.filter(realm=realm, is_active=True)
+
+    def fetch_users_by_email(emails: List[str]) -> List[UserProfile]:
+        # This should be just
+        #
+        # UserProfile.objects.select_related("realm").filter(email__iexact__in=emails,
+        #                                                    realm=realm)
+        #
+        # But chaining __in and __iexact doesn't work with Django's
+        # ORM, so we have the following hack to construct the relevant where clause
+        if len(emails) == 0:
+            return []
+
+        upper_list = ", ".join(["UPPER(%s)"] * len(emails))
+        where_clause = "UPPER(zerver_userprofile.email::text) IN (%s)" % (upper_list,)
+        return base_query.select_related("realm").extra(
+            where=[where_clause],
+            params=emails)
+
+    return generic_bulk_cached_fetch(
+        lambda email: user_profile_by_email_cache_key(email),
+        fetch_users_by_email,
+        [email.lower() for email in emails],
+        id_fetcher=lambda user_profile: user_profile.email.lower()
+    )
 
 def user_ids_to_users(user_ids: List[int], realm: Realm) -> List[UserProfile]:
     # TODO: Change this to do a single bulk query with

zerver/tests/test_events.py
@@ -2556,7 +2556,7 @@ class FetchQueriesTest(ZulipTestCase):
                     client_gravatar=False,
                 )
 
-        self.assert_length(queries, 32)
+        self.assert_length(queries, 28)
 
         expected_counts = dict(
             alert_words=0,
@@ -2575,7 +2575,7 @@ class FetchQueriesTest(ZulipTestCase):
             realm_embedded_bots=0,
             realm_emoji=1,
             realm_filters=1,
-            realm_user=6,
+            realm_user=2,
             realm_user_groups=1,
             stream=2,
             subscription=5,

zerver/tests/test_home.py
@@ -182,8 +182,8 @@ class HomeTest(ZulipTestCase):
             with patch('zerver.lib.cache.cache_set') as cache_mock:
                 result = self._get_home_page(stream='Denmark')
 
-        self.assert_length(queries, 44)
-        self.assert_length(cache_mock.call_args_list, 12)
+        self.assert_length(queries, 40)
+        self.assert_length(cache_mock.call_args_list, 7)
 
         html = result.content.decode('utf-8')
 
@@ -247,7 +247,7 @@ class HomeTest(ZulipTestCase):
         with queries_captured() as queries2:
             result = self._get_home_page()
 
-        self.assert_length(queries2, 38)
+        self.assert_length(queries2, 34)
 
         # Do a sanity check that our new streams were in the payload.
         html = result.content.decode('utf-8')

zerver/tests/test_users.py
@@ -279,6 +279,21 @@ class UserProfileTest(ZulipTestCase):
         with self.assertRaises(JsonableError):
             user_ids_to_users(real_user_ids, get_realm("zephyr"))
 
+    def test_bulk_get_users(self) -> None:
+        from zerver.lib.users import bulk_get_users
+        hamlet = self.example_email("hamlet")
+        cordelia = self.example_email("cordelia")
+        webhook_bot = self.example_email("webhook_bot")
+        result = bulk_get_users([hamlet, cordelia], get_realm("zulip"))
+        self.assertEqual(result[hamlet].email, hamlet)
+        self.assertEqual(result[cordelia].email, cordelia)
+
+        result = bulk_get_users([hamlet, cordelia, webhook_bot], None,
+                                base_query=UserProfile.objects.all())
+        self.assertEqual(result[hamlet].email, hamlet)
+        self.assertEqual(result[cordelia].email, cordelia)
+        self.assertEqual(result[webhook_bot].email, webhook_bot)
+
 class ActivateTest(ZulipTestCase):
     def test_basics(self) -> None:
         user = self.example_user('hamlet')