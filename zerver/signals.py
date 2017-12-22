afedotov89/flexar-zulip

commit 10c64531e57054d72f5abfd883e771012463a977

email-worker: Send login email through queue.
Fixes #7460

zerver/signals.py
@@ -9,6 +9,7 @@ from django.utils.timezone import \
     get_current_timezone_name as timezone_get_current_timezone_name
 from django.utils.timezone import now as timezone_now
 
+from zerver.lib.queue import queue_json_publish
 from zerver.lib.send_email import FromAddress, send_email
 from zerver.models import UserProfile
 
@@ -82,6 +83,10 @@ def email_on_new_login(sender: Any, user: UserProfile, request: Any, **kwargs: A
         context['device_info'] = device_info
         context['user'] = user
 
-        send_email('zerver/emails/notify_new_login', to_user_id=user.id,
-                   from_name='Zulip Account Security', from_address=FromAddress.NOREPLY,
-                   context=context)
+        email_dict = {
+            'template_prefix': 'zerver/emails/notify_new_login',
+            'to_user_id': user.id,
+            'from_name': 'Zulip Account Security',
+            'from_address': FromAddress.NOREPLY,
+            'context': context}
+        queue_json_publish("email_senders", email_dict)

commit cc6f8ceb27b593a5b18f77bffb47c616290226c1

emails: Fix notify_new_login emails have unserializable context.
Regression introduced in 10c6453 (PR #7569).

templates/zerver/emails/notify_new_login.source.html
@@ -10,7 +10,7 @@
 <p><b>Login details:</b></p>
 <blockquote>
     <p>Server: <a href="{{ realm_uri }}" target="_blank">{{ realm_uri }}</a></p>
-    <p>Account: <a href="mailto:{{ user.email }}" target="_blank">{{ user.email }}</a></p>
+    <p>Account: <a href="mailto:{{ user_email }}" target="_blank">{{ user_email }}</a></p>
     <p>Time: {{ device_info.login_time }}</p>
     <p>Device: {{ device_info.device_browser if device_info.device_browser else 'An unknown browser' }} on {{ device_info.device_os if device_info.device_os else 'an unknown operating system' }}.</p>
     <p>IP Address: {{ device_info.device_ip }}</p>

zerver/signals.py
@@ -81,7 +81,7 @@ def email_on_new_login(sender: Any, user: UserProfile, request: Any, **kwargs: A
 
         context = common_context(user)
         context['device_info'] = device_info
-        context['user'] = user
+        context['user_email'] = user.email
 
         email_dict = {
             'template_prefix': 'zerver/emails/notify_new_login',