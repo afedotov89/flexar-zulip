zulip/zulip

billing: Update billing system.

zilencer/management/commands/setup_stripe.py
@@ -1,0 +1,39 @@
+from django.core.management.base import BaseCommand
+from zilencer.models import Plan
+from zproject.settings import get_secret
+
+from typing import Any
+
+import stripe
+stripe.api_key = get_secret('stripe_secret_key')
+
+class Command(BaseCommand):
+    help = """Script to add the appropriate products and plans to Stripe."""
+
+    def handle(self, *args: Any, **options: Any) -> None:
+        # Zulip Cloud offerings
+        product = stripe.Product.create(
+            name="Zulip Cloud Premium",
+            type='service',
+            statement_descriptor="Zulip Cloud Premium",
+            unit_label="user")
+
+        plan = stripe.Plan.create(
+            currency='usd',
+            interval='month',
+            product=product.id,
+            amount=800,
+            billing_scheme='per_unit',
+            nickname=Plan.CLOUD_MONTHLY,
+            usage_type='licensed')
+        Plan.objects.create(nickname=Plan.CLOUD_MONTHLY, stripe_plan_id=plan.id)
+
+        plan = stripe.Plan.create(
+            currency='usd',
+            interval='year',
+            product=product.id,
+            amount=8000,
+            billing_scheme='per_unit',
+            nickname=Plan.CLOUD_ANNUAL,
+            usage_type='licensed')
+        Plan.objects.create(nickname=Plan.CLOUD_ANNUAL, stripe_plan_id=plan.id)

zilencer/models.py
@@ -3,7 +3,7 @@ from typing import Text
 
 from django.db import models
 
-from zerver.models import AbstractPushDeviceToken, Realm
+from zerver.models import AbstractPushDeviceToken, Realm, UserProfile
 
 def get_remote_server_by_uuid(uuid: Text) -> 'RemoteZulipServer':
     return RemoteZulipServer.objects.get(uuid=uuid)
@@ -33,5 +33,17 @@ class RemotePushDeviceToken(AbstractPushDeviceToken):
         return "<RemotePushDeviceToken %s %s>" % (self.server, self.user_id)
 
 class Customer(models.Model):
-    stripe_customer_id = models.CharField(max_length=255, unique=True)
-    realm = models.OneToOneField(Realm, on_delete=models.CASCADE)
+    realm = models.OneToOneField(Realm, on_delete=models.CASCADE)  # type: Realm
+    stripe_customer_id = models.CharField(max_length=255, unique=True)  # type: Text
+    billing_user = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return "<Customer %s %s>" % (self.realm, self.stripe_customer_id)
+
+class Plan(models.Model):
+    # The two possible values for nickname
+    CLOUD_MONTHLY = 'monthly'
+    CLOUD_ANNUAL = 'annual'
+    nickname = models.CharField(max_length=40, unique=True)  # type: Text
+
+    stripe_plan_id = models.CharField(max_length=255, unique=True)  # type: Text

afedotov89/flexar-zulip

zilencer/management/commands/setup_stripe.py
@@ -1,0 +1,41 @@
+from zerver.lib.management import ZulipBaseCommand
+from zilencer.models import Plan
+from zproject.settings import get_secret
+
+from typing import Any
+
+import stripe
+stripe.api_key = get_secret('stripe_secret_key')
+
+class Command(ZulipBaseCommand):
+    help = """Script to add the appropriate products and plans to Stripe."""
+
+    def handle(self, *args: Any, **options: Any) -> None:
+        Plan.objects.all().delete()
+
+        # Zulip Cloud offerings
+        product = stripe.Product.create(
+            name="Zulip Cloud Premium",
+            type='service',
+            statement_descriptor="Zulip Cloud Premium",
+            unit_label="user")
+
+        plan = stripe.Plan.create(
+            currency='usd',
+            interval='month',
+            product=product.id,
+            amount=800,
+            billing_scheme='per_unit',
+            nickname=Plan.CLOUD_MONTHLY,
+            usage_type='licensed')
+        Plan.objects.create(nickname=Plan.CLOUD_MONTHLY, stripe_plan_id=plan.id)
+
+        plan = stripe.Plan.create(
+            currency='usd',
+            interval='year',
+            product=product.id,
+            amount=8000,
+            billing_scheme='per_unit',
+            nickname=Plan.CLOUD_ANNUAL,
+            usage_type='licensed')
+        Plan.objects.create(nickname=Plan.CLOUD_ANNUAL, stripe_plan_id=plan.id)

zilencer/models.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.db import models
 
-from zerver.models import AbstractPushDeviceToken, Realm
+from zerver.models import AbstractPushDeviceToken, Realm, UserProfile
 
 def get_remote_server_by_uuid(uuid: str) -> 'RemoteZulipServer':
     return RemoteZulipServer.objects.get(uuid=uuid)
@@ -36,5 +36,17 @@ class RemotePushDeviceToken(AbstractPushDeviceToken):
         return "<RemotePushDeviceToken %s %s>" % (self.server, self.user_id)
 
 class Customer(models.Model):
-    stripe_customer_id = models.CharField(max_length=255, unique=True)
-    realm = models.OneToOneField(Realm, on_delete=models.CASCADE)
+    realm = models.OneToOneField(Realm, on_delete=models.CASCADE)  # type: Realm
+    stripe_customer_id = models.CharField(max_length=255, unique=True)  # type: str
+    billing_user = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return "<Customer %s %s>" % (self.realm, self.stripe_customer_id)
+
+class Plan(models.Model):
+    # The two possible values for nickname
+    CLOUD_MONTHLY = 'monthly'
+    CLOUD_ANNUAL = 'annual'
+    nickname = models.CharField(max_length=40, unique=True)  # type: str
+
+    stripe_plan_id = models.CharField(max_length=255, unique=True)  # type: str