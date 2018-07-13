afedotov89/flexar-zulip

commit a58d67c03205fa511dda8d1b928eb5bcd4132449

scripts: Replace calls to lsb release with our own parsing.
This improves the performance of these operations, by saving a ~50ms
Python process startup.  While not a major performance improvement, it
seems worth it, given how often these commands get run.

Fixes #9571.

scripts/lib/zulip_tools.py
@@ -15,7 +15,7 @@ import json
 import uuid
 
 if False:
-    from typing import Sequence, Set, Any
+    from typing import Sequence, Set, Any, Dict
 
 DEPLOYMENTS_DIR = "/home/zulip/deployments"
 LOCK_DIR = os.path.join(DEPLOYMENTS_DIR, "lock")
@@ -303,3 +303,12 @@ def may_be_perform_purging(dirs_to_purge, dirs_to_keep, dir_type, dry_run, verbo
     for directory in dirs_to_keep:
         if verbose:
             print("Keeping used %s: %s" % (dir_type, directory))
+
+def parse_lsb_release():
+    # type: () -> Dict[str, str]
+    distro_info = {}
+    with open('/etc/lsb-release', 'r') as fp:
+        data = [line.strip().split('=') for line in fp]
+    for k, v in data:
+        distro_info[k] = v
+    return distro_info

scripts/zulip-puppet-apply
@@ -5,6 +5,7 @@ import sys
 import subprocess
 import configparser
 import re
+from lib.zulip_tools import parse_lsb_release
 
 force = False
 extra_args = sys.argv[1:]
@@ -17,7 +18,7 @@ config = configparser.RawConfigParser()
 config.read("/etc/zulip/zulip.conf")
 
 if not os.path.exists("/etc/puppet/hiera.yaml"):
-    codename = subprocess.check_output(["lsb_release", "-cs"]).strip().decode("utf-8")
+    codename = parse_lsb_release()['DISTRIB_CODENAME']
     if codename in ["trusty", "stretch", "xenial"]:
         # Suppress warnings in old puppet about hiera.yaml not existing.
         subprocess.check_call(["touch", "/etc/puppet/hiera.yaml"])

tools/lib/provision.py
@@ -14,7 +14,7 @@ ZULIP_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__f
 
 sys.path.append(ZULIP_PATH)
 from scripts.lib.zulip_tools import run, subprocess_text_output, OKBLUE, ENDC, WARNING, \
-    get_dev_uuid_var_path, FAIL
+    get_dev_uuid_var_path, FAIL, parse_lsb_release
 from scripts.lib.setup_venv import (
     setup_virtualenv, VENV_DEPENDENCIES, THUMBOR_VENV_DEPENDENCIES
 )
@@ -103,8 +103,9 @@ else:
 if not os.path.exists("/usr/bin/lsb_release"):
     subprocess.check_call(["sudo", "apt-get", "install", "-y", "lsb-release"])
 
-vendor = subprocess_text_output(["lsb_release", "-is"])
-codename = subprocess_text_output(["lsb_release", "-cs"])
+distro_info = parse_lsb_release()
+vendor = distro_info['DISTRIB_ID']
+codename = distro_info['DISTRIB_CODENAME']
 if not (vendor in SUPPORTED_PLATFORMS and codename in SUPPORTED_PLATFORMS[vendor]):
     logging.critical("Unsupported platform: {} {}".format(vendor, codename))
     sys.exit(1)

commit 204c6be7626c9c5a95604a216fc6079f28cc0adf

zulip_tools: Fix accessing LSB data on Debian stretch.
Apparently, at least some Debian stretch systems don't have an
/etc/lsb-release, so the optimization that we did in
a58d67c03205fa511dda8d1b928eb5bcd4132449 broke our installer on
Debian.

We fix this, by falling back to calling the lsb_release command on
systems that don't have a faster way to do it.

Fixes part of #9946.

scripts/lib/zulip_tools.py
@@ -307,10 +307,28 @@ def may_be_perform_purging(dirs_to_purge, dirs_to_keep, dir_type, dry_run, verbo
 def parse_lsb_release():
     # type: () -> Dict[str, str]
     distro_info = {}
-    with open('/etc/lsb-release', 'r') as fp:
-        data = [line.strip().split('=') for line in fp]
-    for k, v in data:
-        distro_info[k] = v
+    try:
+        # For performance reasons, we read /etc/lsb-release directly,
+        # rather than using the lsb_release command; this saves ~50ms
+        # in several places in provisioning and the installer
+        with open('/etc/lsb-release', 'r') as fp:
+            data = [line.strip().split('=') for line in fp]
+        for k, v in data:
+            if k not in ['DISTRIB_CODENAME', 'DISTRIB_ID']:
+                # We only return to the caller the values that we get
+                # from lsb_release in the exception code path.
+                continue
+            distro_info[k] = v
+    except FileNotFoundError:
+        # Unfortunately, Debian stretch doesn't yet have an
+        # /etc/lsb-release, so we instead fetch the pieces of data
+        # that we use from the `lsb_release` command directly.
+        vendor = subprocess_text_output(["lsb_release", "-is"])
+        codename = subprocess_text_output(["lsb_release", "-cs"])
+        distro_info = dict(
+            DISTRIB_CODENAME=codename,
+            DISTRIB_ID=vendor
+        )
     return distro_info
 
 def file_or_package_hash_updated(paths, hash_name, is_force, package_versions=[]):