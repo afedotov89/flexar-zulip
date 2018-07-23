afedotov89/flexar-zulip

commit 49a529b4c99756853f97606d308523afb386fc40

stream settings: Fix stream_age calculation in weekly traffic calculation.

zerver/lib/actions.py
@@ -3910,7 +3910,7 @@ def get_average_weekly_stream_traffic(stream_id: int, stream_date_created: datet
     except KeyError:
         return 0
 
-    stream_age = (timezone_now().date() - stream_date_created.date()).days
+    stream_age = (timezone_now() - stream_date_created).days
 
     if stream_age >= 28:
         average_weekly_traffic = int(stream_traffic // 4)

commit 69916bbe9a836ee7bda37771a0a2af88eaebaec7

stream settings: Fix age calculation in is_old_stream.
Match the calculation in get_average_weekly_stream_traffic.
Regression introduced in 49a529b.

Fixes #10002

zerver/lib/actions.py
@@ -3986,7 +3986,7 @@ def get_average_weekly_stream_traffic(stream_id: int, stream_date_created: datet
     return round_to_2_significant_digits(average_weekly_traffic)
 
 def is_old_stream(stream_date_created: datetime.datetime) -> bool:
-    return (datetime.date.today() - stream_date_created.date()).days \
+    return (timezone_now() - stream_date_created).days \
         >= STREAM_TRAFFIC_CALCULATION_MIN_AGE_DAYS
 
 def encode_email_address(stream: Stream) -> str: