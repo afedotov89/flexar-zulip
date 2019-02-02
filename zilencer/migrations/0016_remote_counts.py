timabbott/zulip

analytics: Add APIs for submitting analytics to another server.
This adds a new API for sending basic analytics data (number of users,
number of messages sent) from a Zulip server to the Zulip Cloud
central analytics database, which will make it possible for servers to
elect to have their usage numbers counted in published stats on the
size of the Zulip ecosystem.

zilencer/migrations/0016_remote_counts.py
@@ -1,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# Generated by Django 1.11.18 on 2019-01-31 23:52
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('analytics', '0012_add_on_delete'),
+        ('zilencer', '0015_delete_billing'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='RemoteInstallationCount',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('property', models.CharField(max_length=32)),
+                ('subgroup', models.CharField(max_length=16, null=True)),
+                ('end_time', models.DateTimeField()),
+                ('value', models.BigIntegerField()),
+                ('remote_id', models.BigIntegerField(db_index=True)),
+                ('anomaly', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='analytics.Anomaly')),
+                ('server', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='zilencer.RemoteZulipServer')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='RemoteRealmCount',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('property', models.CharField(max_length=32)),
+                ('subgroup', models.CharField(max_length=16, null=True)),
+                ('end_time', models.DateTimeField()),
+                ('value', models.BigIntegerField()),
+                ('realm_id', models.BigIntegerField(db_index=True)),
+                ('remote_id', models.BigIntegerField(db_index=True)),
+                ('anomaly', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='analytics.Anomaly')),
+                ('server', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='zilencer.RemoteZulipServer')),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='remoterealmcount',
+            unique_together=set([('server', 'realm_id', 'property', 'subgroup', 'end_time')]),
+        ),
+        migrations.AlterIndexTogether(
+            name='remoterealmcount',
+            index_together=set([('property', 'end_time')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='remoteinstallationcount',
+            unique_together=set([('server', 'property', 'subgroup', 'end_time')]),
+        ),
+    ]

zilencer/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 
 from zerver.models import AbstractPushDeviceToken
+from analytics.models import BaseCount
 
 def get_remote_server_by_uuid(uuid: str) -> 'RemoteZulipServer':
     return RemoteZulipServer.objects.get(uuid=uuid)
@@ -33,3 +34,27 @@ class RemotePushDeviceToken(AbstractPushDeviceToken):
 
     def __str__(self) -> str:
         return "<RemotePushDeviceToken %s %s>" % (self.server, self.user_id)
+
+class RemoteInstallationCount(BaseCount):
+    server = models.ForeignKey(RemoteZulipServer, on_delete=models.CASCADE)  # type: RemoteZulipServer
+    # The remote_id field lets us deduplicate data from the remote server
+    remote_id = models.BigIntegerField(db_index=True)  # type: int
+
+    class Meta:
+        unique_together = ("server", "property", "subgroup", "end_time")
+
+    def __str__(self) -> str:
+        return "<InstallationCount: %s %s %s>" % (self.property, self.subgroup, self.value)
+
+class RemoteRealmCount(BaseCount):
+    server = models.ForeignKey(RemoteZulipServer, on_delete=models.CASCADE)  # type: RemoteZulipServer
+    realm_id = models.BigIntegerField(db_index=True)  # type: int
+    # The remote_id field lets us deduplicate data from the remote server
+    remote_id = models.BigIntegerField(db_index=True)  # type: int
+
+    class Meta:
+        unique_together = ("server", "realm_id", "property", "subgroup", "end_time")
+        index_together = ["property", "end_time"]
+
+    def __str__(self) -> str:
+        return "%s %s %s %s %s" % (self.server, self.realm_id, self.property, self.subgroup, self.value)

afedotov89/flexar-zulip

zilencer/migrations/0016_remote_counts.py
@@ -1,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Generated by Django 1.11.18 on 2019-02-02 06:02
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('zilencer', '0015_delete_billing'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='RemoteInstallationCount',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('property', models.CharField(max_length=32)),
+                ('subgroup', models.CharField(max_length=16, null=True)),
+                ('end_time', models.DateTimeField()),
+                ('value', models.BigIntegerField()),
+                ('remote_id', models.IntegerField(db_index=True)),
+                ('server', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='zilencer.RemoteZulipServer')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='RemoteRealmCount',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('property', models.CharField(max_length=32)),
+                ('subgroup', models.CharField(max_length=16, null=True)),
+                ('end_time', models.DateTimeField()),
+                ('value', models.BigIntegerField()),
+                ('realm_id', models.IntegerField(db_index=True)),
+                ('remote_id', models.IntegerField(db_index=True)),
+                ('server', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='zilencer.RemoteZulipServer')),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='remoterealmcount',
+            unique_together=set([('server', 'realm_id', 'property', 'subgroup', 'end_time')]),
+        ),
+        migrations.AlterIndexTogether(
+            name='remoterealmcount',
+            index_together=set([('property', 'end_time')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='remoteinstallationcount',
+            unique_together=set([('server', 'property', 'subgroup', 'end_time')]),
+        ),
+    ]

zilencer/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 
 from zerver.models import AbstractPushDeviceToken
+from analytics.models import BaseCount
 
 def get_remote_server_by_uuid(uuid: str) -> 'RemoteZulipServer':
     return RemoteZulipServer.objects.get(uuid=uuid)
@@ -33,3 +34,28 @@ class RemotePushDeviceToken(AbstractPushDeviceToken):
 
     def __str__(self) -> str:
         return "<RemotePushDeviceToken %s %s>" % (self.server, self.user_id)
+
+class RemoteInstallationCount(BaseCount):
+    server = models.ForeignKey(RemoteZulipServer, on_delete=models.CASCADE)  # type: RemoteZulipServer
+    # The remote_id field lets us deduplicate data from the remote server
+    remote_id = models.IntegerField(db_index=True)  # type: int
+
+    class Meta:
+        unique_together = ("server", "property", "subgroup", "end_time")
+
+    def __str__(self) -> str:
+        return "<InstallationCount: %s %s %s>" % (self.property, self.subgroup, self.value)
+
+# We can't subclass RealmCount because we only have a realm_id here, not a foreign key.
+class RemoteRealmCount(BaseCount):
+    server = models.ForeignKey(RemoteZulipServer, on_delete=models.CASCADE)  # type: RemoteZulipServer
+    realm_id = models.IntegerField(db_index=True)  # type: int
+    # The remote_id field lets us deduplicate data from the remote server
+    remote_id = models.IntegerField(db_index=True)  # type: int
+
+    class Meta:
+        unique_together = ("server", "realm_id", "property", "subgroup", "end_time")
+        index_together = ["property", "end_time"]
+
+    def __str__(self) -> str:
+        return "%s %s %s %s %s" % (self.server, self.realm_id, self.property, self.subgroup, self.value)