zulip/zulip

settings: Add AVATAR_CHANGES_DISABLED server setting.
Fixes #12132.
Realm setting to disable avatar changes is already present.
The `AVATAR_CHANGES_DISABLED` setting now follows the same
2-setting model as `NAME_CHANGES_DISABLED`.

frontend_tests/node_tests/settings_org.js
@@ -816,11 +816,25 @@ run_test('misc', () => {
     assert.equal($("#change_email .button").attr('disabled'), 'disabled');
 
     page_params.realm_avatar_changes_disabled = false;
+    page_params.avatar_changes_disabled = false;
     settings_account.update_avatar_change_display();
     assert.equal($("#user_avatar_upload_button .button").attr('disabled'), false);
     assert.equal($("#user_avatar_delete_button .button").attr('disabled'), false);
 
     page_params.realm_avatar_changes_disabled = true;
+    page_params.avatar_changes_disabled = false;
+    settings_account.update_avatar_change_display();
+    assert.equal($("#user_avatar_upload_button .button").attr('disabled'), 'disabled');
+    assert.equal($("#user_avatar_delete_button .button").attr('disabled'), 'disabled');
+
+    page_params.realm_avatar_changes_disabled = false;
+    page_params.avatar_changes_disabled = true;
+    settings_account.update_avatar_change_display();
+    assert.equal($("#user_avatar_upload_button .button").attr('disabled'), 'disabled');
+    assert.equal($("#user_avatar_delete_button .button").attr('disabled'), 'disabled');
+
+    page_params.realm_avatar_changes_disabled = true;
+    page_params.avatar_changes_disabled = true;
     settings_account.update_avatar_change_display();
     assert.equal($("#user_avatar_upload_button .button").attr('disabled'), 'disabled');
     assert.equal($("#user_avatar_delete_button .button").attr('disabled'), 'disabled');

zerver/views/home.py
@@ -203,6 +203,7 @@ def home_real(request: HttpRequest) -> HttpResponse:
         password_min_guesses  = settings.PASSWORD_MIN_GUESSES,
         jitsi_server_url      = settings.JITSI_SERVER_URL,
         search_pills_enabled  = settings.SEARCH_PILLS_ENABLED,
+        avatar_changes_disabled = settings.AVATAR_CHANGES_DISABLED,
 
         # Misc. extra data.
         have_initial_messages = user_has_messages,

afedotov89/flexar-zulip

frontend_tests/node_tests/settings_org.js
@@ -816,11 +816,25 @@ run_test('misc', () => {
     assert.equal($("#change_email .button").attr('disabled'), 'disabled');
 
     page_params.realm_avatar_changes_disabled = false;
+    page_params.server_avatar_changes_disabled = false;
     settings_account.update_avatar_change_display();
     assert.equal($("#user_avatar_upload_button .button").attr('disabled'), false);
     assert.equal($("#user_avatar_delete_button .button").attr('disabled'), false);
 
     page_params.realm_avatar_changes_disabled = true;
+    page_params.server_avatar_changes_disabled = false;
+    settings_account.update_avatar_change_display();
+    assert.equal($("#user_avatar_upload_button .button").attr('disabled'), 'disabled');
+    assert.equal($("#user_avatar_delete_button .button").attr('disabled'), 'disabled');
+
+    page_params.realm_avatar_changes_disabled = false;
+    page_params.server_avatar_changes_disabled = true;
+    settings_account.update_avatar_change_display();
+    assert.equal($("#user_avatar_upload_button .button").attr('disabled'), 'disabled');
+    assert.equal($("#user_avatar_delete_button .button").attr('disabled'), 'disabled');
+
+    page_params.realm_avatar_changes_disabled = true;
+    page_params.server_avatar_changes_disabled = true;
     settings_account.update_avatar_change_display();
     assert.equal($("#user_avatar_upload_button .button").attr('disabled'), 'disabled');
     assert.equal($("#user_avatar_delete_button .button").attr('disabled'), 'disabled');

zerver/views/home.py
@@ -203,6 +203,7 @@ def home_real(request: HttpRequest) -> HttpResponse:
         password_min_guesses  = settings.PASSWORD_MIN_GUESSES,
         jitsi_server_url      = settings.JITSI_SERVER_URL,
         search_pills_enabled  = settings.SEARCH_PILLS_ENABLED,
+        server_avatar_changes_disabled = settings.AVATAR_CHANGES_DISABLED,
 
         # Misc. extra data.
         have_initial_messages = user_has_messages,