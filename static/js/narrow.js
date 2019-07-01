afedotov89/flexar-zulip

commit 13ad477f0a426074429b27fb2339e4ff930810f1

search: Show stopwords in multi-operator search.
Fixes #10592.

static/js/narrow.js
@@ -754,8 +754,9 @@ exports.restore_home_state = function () {
 };
 
 function show_search_query() {
-    // Don't need to handle "search:" filter because search_query does not contain it.
-    var search_query = narrow_state.search_string();
+    // when search bar contains multiple filters, only show search queries
+    var current_filter = narrow_state.filter();
+    var search_query = current_filter.operands("search")[0];
     var query_words = search_query.split(" ");
 
     var search_string_display = $("#empty_search_stop_words_string");
@@ -763,6 +764,22 @@ function show_search_query() {
 
     // Also removes previous search_string if any
     search_string_display.text(i18n.t("You searched for:"));
+
+    // Add in stream:foo and topic:bar if present
+    if (current_filter.has_operator("stream") || current_filter.has_operator("topic")) {
+        var stream_topic_string = "";
+        var stream = current_filter.operands('stream')[0];
+        var topic = current_filter.operands('topic')[0];
+        if (stream) {
+            stream_topic_string = "stream: " + stream;
+        }
+        if (topic) {
+            stream_topic_string = stream_topic_string + " topic: " + topic;
+        }
+
+        search_string_display.append(' ');
+        search_string_display.append($('<span>').text(stream_topic_string));
+    }
 
     _.each(query_words, function (query_word) {
         search_string_display.append(' ');
@@ -799,7 +816,12 @@ function pick_empty_narrow_banner() {
     var num_operators = current_filter.operators().length;
 
     if (num_operators !== 1) {
-        // For multi-operator narrows, we just use the default banner
+        // For empty searches within multi-operator narrows, we display the stop words
+        if (current_filter.operators("search")) {
+            show_search_query();
+            return $("#empty_search_narrow_message");
+        }
+        // For other multi-operator narrows, we just use the default banner
         return default_banner;
     } else if (first_operator === "is") {
         if (first_operand === "starred") {

frontend_tests/node_tests/narrow.js
@@ -171,8 +171,14 @@ run_test('show_empty_narrow_message', () => {
     assert.equal(items.length, 2);
     assert.equal(items[0], ' ');
     assert.equal(items[1].text(), 'grail');
+});
 
-    items = [];
+run_test('show_search_stopwords', () => {
+    narrow_state.reset_current_filter();
+    var items = [];
+
+    var display = $("#empty_search_stop_words_string");
+
     display.append = (html) => {
         if (html.text) {
             items.push(html.selector + html.text());
@@ -187,6 +193,28 @@ run_test('show_empty_narrow_message', () => {
     assert.equal(items[0], '<del>what');
     assert.equal(items[1], '<del>about');
     assert.equal(items[2], '<span>grail');
+
+    items = [];
+    set_filter([['stream', 'streamA'], ['search', 'what about grail']]);
+    narrow.show_empty_narrow_message();
+    assert($('#empty_search_narrow_message').visible());
+
+    assert.equal(items.length, 4);
+    assert.equal(items[0], '<span>stream: streamA');
+    assert.equal(items[1], '<del>what');
+    assert.equal(items[2], '<del>about');
+    assert.equal(items[3], '<span>grail');
+
+    items = [];
+    set_filter([['stream', 'streamA'], ['topic', 'topicA'], ['search', 'what about grail']]);
+    narrow.show_empty_narrow_message();
+    assert($('#empty_search_narrow_message').visible());
+
+    assert.equal(items.length, 4);
+    assert.equal(items[0], '<span>stream: streamA topic: topicA');
+    assert.equal(items[1], '<del>what');
+    assert.equal(items[2], '<del>about');
+    assert.equal(items[3], '<span>grail');
 });
 
 run_test('narrow_to_compose_target', () => {

commit 63e90b5b6e636cf74865d345714708c68801ec38

narrow: Fix incorrect method of querying for operands.
Commit 13ad477f0a426074429b27fb2339e4ff930810f1 introduced a bug
where any code reaching `if(operators('search')` would be executed,
which caused inputs where we didn't have the search operator to
throw an error when we do not find a search operan later.

At least one affected cases was narrowing to an empty topic.

static/js/narrow.js
@@ -858,7 +858,7 @@ function pick_empty_narrow_banner() {
         }
 
         // For empty stream searches within other narrows, we display the stop words
-        if (current_filter.operators("search")) {
+        if (current_filter.operands("search").length > 0) {
             show_search_query();
             return $("#empty_search_narrow_message");
         }