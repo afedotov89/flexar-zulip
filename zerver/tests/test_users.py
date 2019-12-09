afedotov89/flexar-zulip

commit c2347b1a66f13c20e879f0069bd09ec8aa1b720b

models: Replace is_guest and is_realm_admin with UserProfile.role.
This new data model will be more extensible for future work on
features like a primary administrator.

zerver/models.py
@@ -397,7 +397,7 @@ class Realm(models.Model):
         notifications to all administrator users.
         """
         # TODO: Change return type to QuerySet[UserProfile]
-        return UserProfile.objects.filter(realm=self, is_realm_admin=True,
+        return UserProfile.objects.filter(realm=self, role=UserProfile.ROLE_REALM_ADMINISTRATOR,
                                           is_active=True)
 
     def get_human_admin_users(self) -> Sequence['UserProfile']:
@@ -406,7 +406,8 @@ class Realm(models.Model):
         realm's administrators (bots don't have real email addresses).
         """
         # TODO: Change return type to QuerySet[UserProfile]
-        return UserProfile.objects.filter(realm=self, is_bot=False, is_realm_admin=True,
+        return UserProfile.objects.filter(realm=self, is_bot=False,
+                                          role=UserProfile.ROLE_REALM_ADMINISTRATOR,
                                           is_active=True)
 
     def get_active_users(self) -> Sequence['UserProfile']:
@@ -793,15 +794,23 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     # See also `long_term_idle`.
     is_active = models.BooleanField(default=True, db_index=True)  # type: bool
 
-    is_realm_admin = models.BooleanField(default=False, db_index=True)  # type: bool
     is_billing_admin = models.BooleanField(default=False, db_index=True)  # type: bool
-
-    # Guest users are limited users without default access to public streams (etc.)
-    is_guest = models.BooleanField(default=False, db_index=True)  # type: bool
 
     is_bot = models.BooleanField(default=False, db_index=True)  # type: bool
     bot_type = models.PositiveSmallIntegerField(null=True, db_index=True)  # type: Optional[int]
     bot_owner = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)  # type: Optional[UserProfile]
+
+    # Each role has a superset of the permissions of the next higher
+    # numbered role.  When adding new roles, leave enough space for
+    # future roles to be inserted between currently adjacent
+    # roles. These constants appear in RealmAuditLog.extra_data, so
+    # changes to them will require a migration of RealmAuditLog.
+    # ROLE_REALM_OWNER = 100
+    ROLE_REALM_ADMINISTRATOR = 200
+    # ROLE_MODERATOR = 300
+    ROLE_MEMBER = 400
+    ROLE_GUEST = 600
+    role = models.PositiveSmallIntegerField(default=ROLE_MEMBER, db_index=True)  # type: int
 
     # Whether the user has been "soft-deactivated" due to weeks of inactivity.
     # For these users we avoid doing UserMessage table work, as an optimization
@@ -1017,6 +1026,14 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self) -> str:
         return "<UserProfile: %s %s>" % (self.email, self.realm)
+
+    @property
+    def is_realm_admin(self) -> bool:
+        return self.role == UserProfile.ROLE_REALM_ADMINISTRATOR
+
+    @property
+    def is_guest(self) -> bool:
+        return self.role == UserProfile.ROLE_GUEST
 
     @property
     def is_incoming_webhook(self) -> bool:
@@ -2136,8 +2153,9 @@ def active_user_ids(realm_id: int) -> List[int]:
 def active_non_guest_user_ids(realm_id: int) -> List[int]:
     query = UserProfile.objects.filter(
         realm_id=realm_id,
-        is_active=True,
-        is_guest=False,
+        is_active=True
+    ).exclude(
+        role=UserProfile.ROLE_GUEST
     ).values_list('id', flat=True)
     return list(query)
 

zerver/tests/test_users.py
@@ -403,10 +403,6 @@ class PermissionTest(ZulipTestCase):
         self.assertEqual(person['email'], polonius.email)
         self.assertTrue(person['is_admin'])
 
-        person = events[1]['event']['person']
-        self.assertEqual(person['email'], polonius.email)
-        self.assertFalse(person['is_guest'])
-
     def test_admin_user_can_change_profile_data(self) -> None:
         realm = get_realm('zulip')
         self.login(self.example_email("iago"))

analytics/views.py
@@ -574,7 +574,7 @@ def realm_summary_table(realm_minutes: Dict[str, float]) -> str:
     # Fetch all the realm administrator users
     realm_admins = defaultdict(list)  # type: Dict[str, List[str]]
     for up in UserProfile.objects.select_related("realm").filter(
-        is_realm_admin=True,
+        role=UserProfile.ROLE_REALM_ADMINISTRATOR,
         is_active=True
     ):
         realm_admins[up.realm.string_id].append(up.delivery_email)

corporate/lib/stripe.py
@@ -40,9 +40,9 @@ DEFAULT_INVOICE_DAYS_UNTIL_DUE = 30
 
 def get_seat_count(realm: Realm) -> int:
     non_guests = UserProfile.objects.filter(
-        realm=realm, is_active=True, is_bot=False, is_guest=False).count()
+        realm=realm, is_active=True, is_bot=False).exclude(role=UserProfile.ROLE_GUEST).count()
     guests = UserProfile.objects.filter(
-        realm=realm, is_active=True, is_bot=False, is_guest=True).count()
+        realm=realm, is_active=True, is_bot=False, role=UserProfile.ROLE_GUEST).count()
     return max(non_guests, math.ceil(guests / 5))
 
 def sign_string(string: str) -> Tuple[str, str]:

corporate/tests/test_stripe.py
@@ -218,8 +218,8 @@ class StripeTestCase(ZulipTestCase):
         realm = get_realm('zulip')
         seat_count = get_seat_count(realm)
         assert(seat_count >= 6)
-        for user in UserProfile.objects.filter(realm=realm, is_active=True, is_bot=False, is_guest=False) \
-                                       .exclude(email__in=[
+        for user in UserProfile.objects.filter(realm=realm, is_active=True, is_bot=False) \
+                                       .exclude(role=UserProfile.ROLE_GUEST).exclude(email__in=[
                                            self.example_email('hamlet'),
                                            self.example_email('iago')])[:seat_count-6]:
             user.is_active = False
@@ -740,17 +740,18 @@ class StripeTest(StripeTestCase):
 
         # Test guests
         # Adding a guest to a realm with a lot of members shouldn't change anything
-        UserProfile.objects.create(realm=realm, email='user3@zulip.com', pointer=-1, is_guest=True)
+        UserProfile.objects.create(realm=realm, email='user3@zulip.com', pointer=-1, role=UserProfile.ROLE_GUEST)
         self.assertEqual(get_seat_count(realm), initial_count)
         # Test 1 member and 5 guests
         realm = Realm.objects.create(string_id='second', name='second')
         UserProfile.objects.create(realm=realm, email='member@second.com', pointer=-1)
         for i in range(5):
             UserProfile.objects.create(realm=realm, email='guest{}@second.com'.format(i),
-                                       pointer=-1, is_guest=True)
+                                       pointer=-1, role=UserProfile.ROLE_GUEST)
         self.assertEqual(get_seat_count(realm), 1)
         # Test 1 member and 6 guests
-        UserProfile.objects.create(realm=realm, email='guest5@second.com', pointer=-1, is_guest=True)
+        UserProfile.objects.create(realm=realm, email='guest5@second.com', pointer=-1,
+                                   role=UserProfile.ROLE_GUEST)
         self.assertEqual(get_seat_count(realm), 2)
 
     def test_sign_string(self) -> None:

zerver/data_import/hipchat.py
@@ -21,6 +21,7 @@ from zerver.lib.utils import (
 from zerver.models import (
     RealmEmoji,
     Recipient,
+    UserProfile,
 )
 
 from zerver.data_import.import_util import (
@@ -101,17 +102,21 @@ def convert_user_data(user_handler: UserHandler,
         email = in_dict['email']
         full_name = in_dict['name']
         id = user_id_mapper.get(in_dict['id'])
-        is_realm_admin = in_dict['account_type'] == 'admin'
-        is_guest = in_dict['account_type'] == 'guest'
         is_mirror_dummy = False
         short_name = in_dict['mention_name']
         timezone = in_dict['timezone']
+
+        role = UserProfile.ROLE_MEMBER
+        if in_dict['account_type'] == 'admin':
+            role = UserProfile.ROLE_REALM_ADMINISTRATOR
+        if in_dict['account_type'] == 'guest':
+            role = UserProfile.ROLE_GUEST
 
         date_joined = int(timezone_now().timestamp())
         is_active = not in_dict['is_deleted']
 
         if not email:
-            if is_guest:
+            if role == UserProfile.ROLE_GUEST:
                 # Hipchat guest users don't have emails, so
                 # we just fake them.
                 email = 'guest-{id}@example.com'.format(id=id)
@@ -140,8 +145,7 @@ def convert_user_data(user_handler: UserHandler,
             full_name=full_name,
             id=id,
             is_active=is_active,
-            is_realm_admin=is_realm_admin,
-            is_guest=is_guest,
+            role=role,
             is_mirror_dummy=is_mirror_dummy,
             realm_id=realm_id,
             short_name=short_name,

zerver/data_import/hipchat_user.py
@@ -5,6 +5,9 @@ from django.utils.timezone import now as timezone_now
 from zerver.data_import.import_util import (
     build_user_profile,
 )
+from zerver.models import (
+    UserProfile,
+)
 
 class UserHandler:
     '''
@@ -55,8 +58,7 @@ class UserHandler:
             full_name=full_name,
             id=user_id,
             is_active=False,
-            is_realm_admin=False,
-            is_guest=False,
+            role=UserProfile.ROLE_MEMBER,
             is_mirror_dummy=True,
             realm_id=realm_id,
             short_name=short_name,

zerver/data_import/import_util.py
@@ -63,8 +63,7 @@ def build_user_profile(avatar_source: str,
                        full_name: str,
                        id: int,
                        is_active: bool,
-                       is_realm_admin: bool,
-                       is_guest: bool,
+                       role: int,
                        is_mirror_dummy: bool,
                        realm_id: int,
                        short_name: str,
@@ -79,8 +78,7 @@ def build_user_profile(avatar_source: str,
         id=id,
         is_mirror_dummy=is_mirror_dummy,
         is_active=is_active,
-        is_realm_admin=is_realm_admin,
-        is_guest=is_guest,
+        role=role,
         pointer=pointer,
         realm_id=realm_id,
         short_name=short_name,

commit a8cfb5bbad488bba1f3416977bcb0ec0b96c7966

models: Add is_realm_admin and is_guest setters.
Fixes #13452.

The migration from UserProfile.is_realm_admin/UserProfile.is_guest in
c2347b1a66f13c20e879f0069bd09ec8aa1b720b broke our LDAP-based support
for setting a user's role via LDAP properties, which relied on setting
those fields.  Because the django-auth-ldap feature powering that only
supports booleans (and in any case, we don't want to expose constants
like `ROLE_REALM_ADMINISTRATOR` to the LDAP configuration interface),
it makes sense to provide setters for these legacy fields for
backwards-compatibility.

We lint against using these setters directly in Zulip's codebase
directly.  The issue with using these is that when changing user's
.role we want to create appropriate RealmAuditLog entries and send
events. This isn't possible when using these setters - the log entries
and events should be created if the role change in the UserProfile is
actually save()-ed to the database - and on the level of the setter
function, it's not known whether the change will indeed be saved.

It would have to be somehow figured out on the level of post_save
signal handlers, but it doesn't seem like a good design to have such
complexity there, for the sake of setters that generally shouldn't be
used anyway - because we prefer the do_change_is_* functions.

The purpose of this change is narrowly to handle use cases like the
setattr on these boolean properties.

tools/linter_lib/custom_check.py
@@ -500,6 +500,20 @@ python_rules = RuleList(
          'include_only': set(["/management/commands/"]),
          'description': 'Raise CommandError to exit with failure in management commands',
          },
+        {'pattern': '.is_realm_admin =',
+         'description': 'Use do_change_is_admin function rather than setting UserProfile\'s is_realm_admin attribute directly.',
+         'exclude': set([
+             'zerver/migrations/0248_userprofile_role_start.py',
+             'zerver/tests/test_users.py',
+         ]),
+         },
+        {'pattern': '.is_guest =',
+         'description': 'Use do_change_is_guest function rather than setting UserProfile\'s is_guest attribute directly.',
+         'exclude': set([
+             'zerver/migrations/0248_userprofile_role_start.py',
+             'zerver/tests/test_users.py',
+         ]),
+         },
         *whitespace_rules,
         *comma_whitespace_rule,
     ],

zerver/models.py
@@ -1055,9 +1055,27 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def is_realm_admin(self) -> bool:
         return self.role == UserProfile.ROLE_REALM_ADMINISTRATOR
 
+    @is_realm_admin.setter
+    def is_realm_admin(self, value: bool) -> None:
+        if value:
+            self.role = UserProfile.ROLE_REALM_ADMINISTRATOR
+        elif self.role == UserProfile.ROLE_REALM_ADMINISTRATOR:
+            # We need to be careful to not accidentally change
+            # ROLE_GUEST to ROLE_MEMBER here.
+            self.role = UserProfile.ROLE_MEMBER
+
     @property
     def is_guest(self) -> bool:
         return self.role == UserProfile.ROLE_GUEST
+
+    @is_guest.setter
+    def is_guest(self, value: bool) -> None:
+        if value:
+            self.role = UserProfile.ROLE_GUEST
+        elif self.role == UserProfile.ROLE_GUEST:
+            # We need to be careful to not accidentally change
+            # ROLE_REALM_ADMINISTRATOR to ROLE_MEMBER here.
+            self.role = UserProfile.ROLE_MEMBER
 
     @property
     def is_incoming_webhook(self) -> bool:

zerver/tests/test_users.py
@@ -75,6 +75,33 @@ class PermissionTest(ZulipTestCase):
         with self.assertRaises(AssertionError):
             do_change_is_admin(user_profile, True, permission='totally-not-valid-perm')
 
+    def test_role_setters(self) -> None:
+        user_profile = self.example_user('hamlet')
+
+        user_profile.is_realm_admin = True
+        self.assertEqual(user_profile.is_realm_admin, True)
+        self.assertEqual(user_profile.role, UserProfile.ROLE_REALM_ADMINISTRATOR)
+
+        user_profile.is_guest = False
+        self.assertEqual(user_profile.is_guest, False)
+        self.assertEqual(user_profile.role, UserProfile.ROLE_REALM_ADMINISTRATOR)
+
+        user_profile.is_realm_admin = False
+        self.assertEqual(user_profile.is_realm_admin, False)
+        self.assertEqual(user_profile.role, UserProfile.ROLE_MEMBER)
+
+        user_profile.is_guest = True
+        self.assertEqual(user_profile.is_guest, True)
+        self.assertEqual(user_profile.role, UserProfile.ROLE_GUEST)
+
+        user_profile.is_realm_admin = False
+        self.assertEqual(user_profile.is_guest, True)
+        self.assertEqual(user_profile.role, UserProfile.ROLE_GUEST)
+
+        user_profile.is_guest = False
+        self.assertEqual(user_profile.is_guest, False)
+        self.assertEqual(user_profile.role, UserProfile.ROLE_MEMBER)
+
     def test_get_admin_users(self) -> None:
         user_profile = self.example_user('hamlet')
         do_change_is_admin(user_profile, False)