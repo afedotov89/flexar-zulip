mateuszmandera/zulip

auth: Pass request kwarg in authenticate() calls with username+password.
These authenticate() calls use either Email or LDAP backends, which will
be rate limited and will need access to the request object.

zerver/views/registration.py
@@ -232,7 +232,7 @@ def accounts_register(request: HttpRequest) -> HttpResponse:
             # But if the realm is using LDAPAuthBackend, we need to verify
             # their LDAP password (which will, as a side effect, create
             # the user account) here using authenticate.
-            auth_result = authenticate(request,
+            auth_result = authenticate(request=request,
                                        username=email,
                                        password=password,
                                        realm=realm,

afedotov89/flexar-zulip

zerver/views/registration.py
@@ -273,7 +273,7 @@ def accounts_register(request: HttpRequest) -> HttpResponse:
             # pregeg_user.realm_creation carries the information about whether
             # we're in realm creation mode, and the ldap flow will handle
             # that and create the user with the appropriate parameters.
-            user_profile = authenticate(request,
+            user_profile = authenticate(request=request,
                                         username=email,
                                         password=password,
                                         realm=realm,