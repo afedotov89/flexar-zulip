afedotov89/flexar-zulip

commit cd982f302838dfc61b6b4c206a304f82f5a810ac

stream: Add support for sending user_id to endpoint when subscribing users.
This commit modifies the backend to accept user ids when subscribing
users to streams.

It also migrates all existing tests to use this API, aside from a
small set of tests for the legacy API.

zerver/views/streams.py
@@ -30,7 +30,7 @@ from zerver.lib.validator import check_string, check_int, check_list, check_dict
     check_bool, check_variable_type, check_capped_string, check_color, check_dict_only, \
     check_int_in, to_non_negative_int
 from zerver.models import UserProfile, Stream, Realm, UserMessage, \
-    get_system_bot, get_active_user
+    get_system_bot, get_active_user, get_active_user_profile_by_id_in_realm
 
 from collections import defaultdict
 import ujson
@@ -40,16 +40,19 @@ class PrincipalError(JsonableError):
     data_fields = ['principal']
     http_status_code = 403
 
-    def __init__(self, principal: str) -> None:
-        self.principal: str = principal
+    def __init__(self, principal: Union[int, str]) -> None:
+        self.principal: Union[int, str] = principal
 
     @staticmethod
     def msg_format() -> str:
         return _("User not authorized to execute queries on behalf of '{principal}'")
 
-def principal_to_user_profile(agent: UserProfile, principal: str) -> UserProfile:
+def principal_to_user_profile(agent: UserProfile, principal: Union[str, int]) -> UserProfile:
     try:
-        return get_active_user(principal, agent.realm)
+        if isinstance(principal, str):
+            return get_active_user(principal, agent.realm)
+        else:
+            return get_active_user_profile_by_id_in_realm(principal, agent.realm)
     except UserProfile.DoesNotExist:
         # We have to make sure we don't leak information about which users
         # are registered for Zulip in a different realm.  We could do
@@ -249,11 +252,20 @@ def compose_views(
 def remove_subscriptions_backend(
         request: HttpRequest, user_profile: UserProfile,
         streams_raw: Iterable[str]=REQ("subscriptions", validator=check_list(check_string)),
-        principals: Optional[Iterable[str]]=REQ(validator=check_list(check_string), default=None),
+        principals: Optional[Union[List[str], List[int]]]=REQ(validator=check_variable_type([
+            check_list(check_string), check_list(check_int)]), default=None),
 ) -> HttpResponse:
 
-    removing_someone_else = principals and \
-        set(principals) != {user_profile.email}
+    # TODO: Extract this as a function to improve readability
+    removing_someone_else = False
+    if principals is not None and len(principals) > 0:
+        if len(principals) == 1:
+            if isinstance(principals[0], int):
+                removing_someone_else = principals[0] != user_profile.id
+            else:
+                removing_someone_else = principals[0] != user_profile.email
+        else:
+            removing_someone_else = True
 
     if removing_someone_else and not user_profile.is_realm_admin:
         # You can only unsubscribe other people from a stream if you are a realm
@@ -315,7 +327,8 @@ def add_subscriptions_backend(
             Stream.STREAM_POST_POLICY_TYPES), default=Stream.STREAM_POST_POLICY_EVERYONE),
         history_public_to_subscribers: Optional[bool]=REQ(validator=check_bool, default=None),
         announce: bool=REQ(validator=check_bool, default=False),
-        principals: List[str]=REQ(validator=check_list(check_string), default=[]),
+        principals: List[Union[str, int]]=REQ(validator=check_variable_type([
+            check_list(check_string), check_list(check_int)]), default=[]),
         authorization_errors_fatal: bool=REQ(validator=check_bool, default=True),
 ) -> HttpResponse:
     stream_dicts = []

zerver/models.py
@@ -2240,6 +2240,12 @@ def get_active_user(email: str, realm: Realm) -> UserProfile:
 def get_user_profile_by_id_in_realm(uid: int, realm: Realm) -> UserProfile:
     return UserProfile.objects.select_related().get(id=uid, realm=realm)
 
+def get_active_user_profile_by_id_in_realm(uid: int, realm: Realm) -> UserProfile:
+    user_profile = get_user_profile_by_id_in_realm(uid, realm)
+    if not user_profile.is_active:
+        raise UserProfile.DoesNotExist()
+    return user_profile
+
 def get_user_including_cross_realm(email: str, realm: Optional[Realm]=None) -> UserProfile:
     if is_cross_realm_bot_email(email):
         return get_system_bot(email)

zerver/openapi/zulip.yaml
@@ -4420,13 +4420,18 @@ components:
       name: principals
       in: query
       description: |
-        A list of email addresses of the users that will be subscribed/unsubscribed
-        to the streams specified in the `subscriptions` argument. If not provided, then
-        the requesting user/bot is subscribed.
+        A list of user ids (preferred) or Zulip display email
+        addresses of the users to be subscribed to or unsubscribed
+        from the streams specified in the `subscriptions` argument. If
+        not provided, then the requesting user/bot is subscribed.
+
+        **Changes**: The integer format is new in Zulip 2.2 (Feature level 9).
       schema:
         type: array
         items:
-          type: string
+          oneOf:
+            - type: string
+            - type: integer
         default: []
       example: ['ZOE@zulip.com']
     ReactionType:

zerver/tests/test_openapi.py
@@ -466,6 +466,9 @@ do not match the types declared in the implementation of {}.\n""".format(functio
                         subtypes.append(VARMAP[st])
                     self.assertTrue(len(subtypes) > 1)
                     sub_type = self.get_type_by_priority(subtypes)
+                elif "oneOf" in items.keys():
+                    sub_type = VARMAP[element["schema"]["items"]["oneOf"][0]["type"]]
+                    self.assertIsNotNone(sub_type)
                 else:
                     sub_type = VARMAP[schema["items"]["type"]]
                     self.assertIsNotNone(sub_type)

commit 19832cbed8dfb8d70c98bd369086a5bc8ef08b42

test_subs: Add tests for unsubscribing multiple users from stream.
This commit adds tests for unsusbcribing multiple users from a
stream and fixes the missing coverage issue introduced in cd982f3.

zerver/views/streams.py
@@ -264,7 +264,7 @@ def remove_subscriptions_backend(
                 removing_someone_else = principals[0] != user_profile.id
             else:
                 removing_someone_else = principals[0] != user_profile.email
-        else:  # nocoverage # TODO: Fix me.
+        else:
             removing_someone_else = True
 
     if removing_someone_else and not user_profile.is_realm_admin:

zerver/tests/test_subs.py
@@ -974,6 +974,7 @@ class StreamAdminTest(ZulipTestCase):
     def attempt_unsubscribe_of_principal(self, query_count: int, is_admin: bool=False,
                                          is_subbed: bool=True, invite_only: bool=False,
                                          other_user_subbed: bool=True, using_legacy_emails: bool=False,
+                                         unsubscribe_multiple_users: bool=False,
                                          other_sub_users: Optional[List[UserProfile]]=None) -> HttpResponse:
 
         # Set up the main user, who is in most cases an admin.
@@ -991,15 +992,24 @@ class StreamAdminTest(ZulipTestCase):
         # Set up the principal to be unsubscribed.
         other_user_profile = self.example_user('cordelia')
         if using_legacy_emails:
-            principal = other_user_profile.email
+            principals = [other_user_profile.email]
         else:
-            principal = other_user_profile.id
+            principals = [other_user_profile.id]
+
+        if unsubscribe_multiple_users:
+            second_user_to_unsubscribe = self.example_user('prospero')
+            if using_legacy_emails:
+                principals.append(second_user_to_unsubscribe.email)
+            else:
+                principals.append(second_user_to_unsubscribe.id)
 
         # Subscribe the admin and/or principal as specified in the flags.
         if is_subbed:
             self.subscribe(user_profile, stream_name)
         if other_user_subbed:
             self.subscribe(other_user_profile, stream_name)
+            if unsubscribe_multiple_users:
+                self.subscribe(second_user_to_unsubscribe, stream_name)
         if other_sub_users:
             for user in other_sub_users:
                 self.subscribe(user, stream_name)
@@ -1008,7 +1018,7 @@ class StreamAdminTest(ZulipTestCase):
             result = self.client_delete(
                 "/json/users/me/subscriptions",
                 {"subscriptions": ujson.dumps([stream_name]),
-                 "principals": ujson.dumps([principal])})
+                 "principals": ujson.dumps(principals)})
         self.assert_length(queries, query_count)
 
         # If the removal succeeded, then assert that Cordelia is no longer subscribed.
@@ -1038,6 +1048,17 @@ class StreamAdminTest(ZulipTestCase):
             other_user_subbed=True)
         json = self.assert_json_success(result)
         self.assertEqual(len(json["removed"]), 1)
+        self.assertEqual(len(json["not_removed"]), 0)
+
+    def test_admin_remove_multiple_users_from_stream(self) -> None:
+        """
+        If you're an admin, you can remove multiple users from a stream,
+        """
+        result = self.attempt_unsubscribe_of_principal(
+            query_count=30, is_admin=True, is_subbed=True, invite_only=False,
+            other_user_subbed=True, unsubscribe_multiple_users=True)
+        json = self.assert_json_success(result)
+        self.assertEqual(len(json["removed"]), 2)
         self.assertEqual(len(json["not_removed"]), 0)
 
     def test_admin_remove_others_from_subbed_private_stream(self) -> None:
@@ -1077,6 +1098,14 @@ class StreamAdminTest(ZulipTestCase):
             other_user_subbed=True, using_legacy_emails=True)
         json = self.assert_json_success(result)
         self.assertEqual(len(json["removed"]), 1)
+        self.assertEqual(len(json["not_removed"]), 0)
+
+    def test_admin_remove_multiple_users_from_stream_legacy_emails(self) -> None:
+        result = self.attempt_unsubscribe_of_principal(
+            query_count=30, is_admin=True, is_subbed=True, invite_only=False,
+            other_user_subbed=True, unsubscribe_multiple_users=True, using_legacy_emails=True)
+        json = self.assert_json_success(result)
+        self.assertEqual(len(json["removed"]), 2)
         self.assertEqual(len(json["not_removed"]), 0)
 
     def test_create_stream_policy_setting(self) -> None: