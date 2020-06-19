zulip/zulip

openapi: Make api-docs filename match with operationIds.
api docs filenames are basically the operationId of their endpoint
in zulip.yaml with `_` replaced by `-`. But some operationIds have
changed, so change the affected filenames. Make changes in other
files accordingly.

zerver/tests/test_docs.py
@@ -113,18 +113,18 @@ class DocPageTest(ZulipTestCase):
         self._test('/api/installation-instructions', 'No download required!')
         self._test('/api/send-message', 'steal away your hearts')
         self._test('/api/render-message', '**foo**')
-        self._test('/api/get-all-streams', 'include_public')
+        self._test('/api/get-streams', 'include_public')
         self._test('/api/get-stream-id', 'The name of the stream to access.')
-        self._test('/api/get-subscribed-streams', 'Get all streams that the user is subscribed to.')
-        self._test('/api/get-all-users', 'client_gravatar')
+        self._test('/api/get-subscriptions', 'Get all streams that the user is subscribed to.')
+        self._test('/api/get-users', 'client_gravatar')
         self._test('/api/register-queue', 'apply_markdown')
-        self._test('/api/get-events-from-queue', 'dont_block')
+        self._test('/api/get-events', 'dont_block')
         self._test('/api/delete-queue', 'Delete a previously registered queue')
         self._test('/api/update-message', 'propagate_mode')
-        self._test('/api/get-profile', 'takes no arguments')
-        self._test('/api/add-subscriptions', 'authorization_errors_fatal')
+        self._test('/api/get-own-user', 'takes no arguments')
+        self._test('/api/subscribe', 'authorization_errors_fatal')
         self._test('/api/create-user', 'zuliprc-admin')
-        self._test('/api/remove-subscriptions', 'not_removed')
+        self._test('/api/unsubscribe', 'not_removed')
         self._test('/team/', 'industry veterans')
         self._test('/history/', 'Cambridge, Massachusetts')
         # Test the i18n version of one of these pages.

afedotov89/flexar-zulip

zerver/tests/test_docs.py
@@ -112,18 +112,18 @@ class DocPageTest(ZulipTestCase):
         self._test('/api/installation-instructions', 'No download required!')
         self._test('/api/send-message', 'steal away your hearts')
         self._test('/api/render-message', '**foo**')
-        self._test('/api/get-all-streams', 'include_public')
+        self._test('/api/get-streams', 'include_public')
         self._test('/api/get-stream-id', 'The name of the stream to access.')
-        self._test('/api/get-subscribed-streams', 'Get all streams that the user is subscribed to.')
-        self._test('/api/get-all-users', 'client_gravatar')
+        self._test('/api/get-subscriptions', 'Get all streams that the user is subscribed to.')
+        self._test('/api/get-users', 'client_gravatar')
         self._test('/api/register-queue', 'apply_markdown')
-        self._test('/api/get-events-from-queue', 'dont_block')
+        self._test('/api/get-events', 'dont_block')
         self._test('/api/delete-queue', 'Delete a previously registered queue')
         self._test('/api/update-message', 'propagate_mode')
-        self._test('/api/get-profile', 'takes no parameters')
-        self._test('/api/add-subscriptions', 'authorization_errors_fatal')
+        self._test('/api/get-own-user', 'takes no parameters')
+        self._test('/api/subscribe', 'authorization_errors_fatal')
         self._test('/api/create-user', 'zuliprc-admin')
-        self._test('/api/remove-subscriptions', 'not_removed')
+        self._test('/api/unsubscribe', 'not_removed')
         self._test('/team/', 'industry veterans')
         self._test('/history/', 'Cambridge, Massachusetts')
         # Test the i18n version of one of these pages.