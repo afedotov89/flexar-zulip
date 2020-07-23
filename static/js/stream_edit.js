afedotov89/flexar-zulip

commit d552d355374e85bc1ac6cf7e58a62074d8e9961d

stream_edit: Restore keyboard accessibility to checkboxes.
Listen to change events from the checkbox and pay attention to its
actual value, rather than simulating it by toggling booleans on click
events.

Signed-off-by: Anders Kaseorg <anders@zulip.com>

static/js/stream_edit.js
@@ -407,52 +407,35 @@ exports.show_settings_for = function (node) {
     show_subscription_settings(sub_settings);
 };
 
-function stream_is_muted_clicked(e) {
-    e.preventDefault();
+function stream_is_muted_changed(e) {
     const sub = get_sub_for_target(e.target);
     if (!sub) {
-        blueslip.error("stream_is_muted_clicked() fails");
+        blueslip.error("stream_is_muted_changed() fails");
         return;
     }
 
     const sub_settings = exports.settings_for_sub(sub);
     const notification_checkboxes = sub_settings.find(".sub_notification_setting");
 
-    subs.toggle_home(sub, `#stream_change_property_status${sub.stream_id}`);
-
-    if (!sub.is_muted) {
-        sub_settings.find(".mute-note").addClass("hide-mute-note");
-        notification_checkboxes.removeClass("muted-sub");
-        notification_checkboxes.find("input[type='checkbox']").prop("disabled", false);
-    } else {
-        sub_settings.find(".mute-note").removeClass("hide-mute-note");
-        notification_checkboxes.addClass("muted-sub");
-        notification_checkboxes.find("input[type='checkbox']").attr("disabled", true);
-    }
+    subs.set_muted(sub, `#stream_change_property_status${sub.stream_id}`, e.target.checked);
+    sub_settings.find(".mute-note").toggleClass("hide-mute-note", !sub.is_muted);
+    notification_checkboxes.toggleClass("muted-sub", sub.is_muted);
+    notification_checkboxes.find("input[type='checkbox']").prop("disabled", sub.is_muted);
 }
 
-exports.stream_setting_clicked = function (e) {
-    if (e.currentTarget.id === "sub_is_muted_setting") {
+exports.stream_setting_changed = function (e, from_notification_settings) {
+    if (e.target.name === "is_muted") {
         return;
     }
-    e.preventDefault();
-    e.stopPropagation();
 
     const sub = get_sub_for_target(e.target);
-    let checkbox = $(e.currentTarget).find(".sub_setting_control");
-    let status_element = "#stream_change_property_status" + sub.stream_id;
-    // sub data is being changed from the notification settings page.
-    if (checkbox.length === 0) {
-        checkbox = $(e.currentTarget);
-        status_element = checkbox.closest(".subsection-parent").find(".alert-notification");
-    }
-    const setting = checkbox.attr("name");
+    const status_element = from_notification_settings
+        ? $(e.target).closest(".subsection-parent").find(".alert-notification")
+        : $("#stream_change_property_status" + sub.stream_id);
+    const setting = e.target.name;
     if (!sub) {
-        blueslip.error("undefined sub in stream_setting_clicked()");
+        blueslip.error("undefined sub in stream_setting_changed()");
         return;
-    }
-    if (checkbox.prop("disabled")) {
-        return false;
     }
     if (exports.is_notification_setting(setting) && sub[setting] === null) {
         if (setting === "wildcard_mentions_notify") {
@@ -461,7 +444,7 @@ exports.stream_setting_clicked = function (e) {
             sub[setting] = page_params["enable_stream_" + setting];
         }
     }
-    exports.set_stream_property(sub, setting, !sub[setting], status_element);
+    exports.set_stream_property(sub, setting, e.target.checked, status_element);
 };
 
 exports.bulk_set_stream_property = function (sub_data, status_element) {
@@ -692,9 +675,17 @@ exports.initialize = function () {
         e.stopPropagation();
     });
 
-    $("#subscriptions_table").on("click", "#sub_is_muted_setting", stream_is_muted_clicked);
+    $("#subscriptions_table").on(
+        "change",
+        "#sub_is_muted_setting .sub_setting_control",
+        stream_is_muted_changed,
+    );
 
-    $("#subscriptions_table").on("click", ".sub_setting_checkbox", exports.stream_setting_clicked);
+    $("#subscriptions_table").on(
+        "change",
+        ".sub_setting_checkbox .sub_setting_control",
+        exports.stream_setting_changed,
+    );
 
     $("#subscriptions_table").on("keyup", ".subscriber_list_add form", (e) => {
         if (e.which === 13) {

static/js/subs.js
@@ -95,8 +95,8 @@ exports.active_stream = function () {
     }
 };
 
-exports.toggle_home = function (sub, status_element) {
-    stream_muting.update_is_muted(sub, !sub.is_muted);
+exports.set_muted = function (sub, status_element, is_muted) {
+    stream_muting.update_is_muted(sub, is_muted);
     stream_edit.set_stream_property(sub, "is_muted", sub.is_muted, status_element);
 };
 

static/js/settings_notifications.js
@@ -98,7 +98,7 @@ exports.set_up = function () {
         e.stopPropagation();
         const input_elem = $(e.currentTarget);
         if (input_elem.parents("#stream-specific-notify-table").length) {
-            stream_edit.stream_setting_clicked(e);
+            stream_edit.stream_setting_changed(e, true);
             return;
         }
         const setting_name = input_elem.attr("name");

static/js/stream_popover.js
@@ -402,7 +402,7 @@ exports.register_stream_handlers = function () {
     $("body").on("click", ".toggle_home", (e) => {
         const sub = stream_popover_sub(e);
         exports.hide_stream_popover();
-        subs.toggle_home(sub);
+        subs.set_muted(sub, !sub.is_muted);
         e.stopPropagation();
     });
 

static/templates/stream_settings_checkbox.hbs
@@ -5,11 +5,11 @@
   {{#if notification_setting}}sub_notification_setting {{#if is_muted}}muted-sub{{/if}}{{/if}}{{/if}} new-style">
     <label class="checkbox">
         <input id="{{setting_name}}_{{stream_id}}" name="{{setting_name}}"
-          class="sub_setting_control" type="checkbox" tabindex="-1"
+          class="sub_setting_control" type="checkbox"
           {{#if is_checked}}checked{{/if}}{{#if is_disabled}}disabled="disabled"{{/if}} />
         <span></span>
     </label>
-    <label class="subscription-control-label">{{label}}</label>
+    <label class="subscription-control-label" for="{{setting_name}}_{{stream_id}}">{{label}}</label>
 
     {{!-- Tooltips for settings --}}
     {{#if (eq setting_name "is_muted")}}

commit b9c0e06617a51a799d95a3ecaebb0aa6ae36907b

subs: Fix set_muted parameter order.
The status_element parameter is optional, and the other caller in
stream_popover.js does not provide it.  This fixes a regression in
commit d552d355374e85bc1ac6cf7e58a62074d8e9961d (#15868).

Signed-off-by: Anders Kaseorg <anders@zulip.com>

static/js/stream_edit.js
@@ -413,7 +413,7 @@ function stream_is_muted_changed(e) {
     const sub_settings = exports.settings_for_sub(sub);
     const notification_checkboxes = sub_settings.find(".sub_notification_setting");
 
-    subs.set_muted(sub, `#stream_change_property_status${sub.stream_id}`, e.target.checked);
+    subs.set_muted(sub, e.target.checked, `#stream_change_property_status${sub.stream_id}`);
     sub_settings.find(".mute-note").toggleClass("hide-mute-note", !sub.is_muted);
     notification_checkboxes.toggleClass("muted-sub", sub.is_muted);
     notification_checkboxes.find("input[type='checkbox']").prop("disabled", sub.is_muted);

static/js/subs.js
@@ -95,7 +95,7 @@ exports.active_stream = function () {
     }
 };
 
-exports.set_muted = function (sub, status_element, is_muted) {
+exports.set_muted = function (sub, is_muted, status_element) {
     stream_muting.update_is_muted(sub, is_muted);
     stream_edit.set_stream_property(sub, "is_muted", sub.is_muted, status_element);
 };