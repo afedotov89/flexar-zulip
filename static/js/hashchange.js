afedotov89/flexar-zulip

commit e5087724bed87ef9ad8d6691026fbe08728d67c6

hashchange: Render default view on empty hash.
Instead of changing the hash to the default_view hash, we render
the default_view directly in case of recent topics and all messages.

This also fixes the bug that user is unable to go back in
browser window history.

static/js/hashchange.js
@@ -95,7 +95,21 @@ function activate_home_tab() {
 }
 
 export function show_default_view() {
-    window.location.hash = page_params.default_view;
+    // We only allow all_messages and recent_topics
+    // to be rendered without a hash.
+    if (page_params.default_view === "recent_topics") {
+        recent_topics.show();
+    } else if (page_params.default_view === "all_messages") {
+        activate_home_tab();
+    } else {
+        // NOTE: Setting a hash which is not rendered on
+        // empty hash (like a stream narrow) will
+        // introduce a bug that user will not be able to
+        // go back in broswer history. See
+        // https://chat.zulip.org/#narrow/stream/9-issues/topic/Browser.20back.20button.20on.20RT
+        // for detailed description of the issue.
+        window.location.hash = page_params.default_view;
+    }
 }
 
 // Returns true if this function performed a narrow
@@ -112,8 +126,11 @@ function do_hashchange_normal(from_reload) {
             ui_util.change_tab_to("#message_feed_container");
             const operators = hash_util.parse_narrow(hash);
             if (operators === undefined) {
-                // If the narrow URL didn't parse, clear
-                // window.location.hash and send them to the home tab
+                // If the narrow URL didn't parse,
+                // send them to default_view.
+                // We cannot clear hash here since
+                // it will block user from going back
+                // in browser history.
                 show_default_view();
                 return false;
             }

commit 1aa5e81da021901aa0dc76711bd5dc8e226d4a82

hotkey: Render default_view via changing hash.
Directly rendering the default view on pressing `escape` key will lead
to default_view being rendered on the current window hash.  So, we set
empty hash to load default view and let hashchange handle it.

This fixes a bug introduced in
e5087724bed87ef9ad8d6691026fbe08728d67c6.

static/js/hashchange.js
@@ -94,7 +94,14 @@ function show_all_message_view() {
     setTimeout(navigate.maybe_scroll_to_selected, 0);
 }
 
-export function show_default_view() {
+export function set_hash_to_default_view() {
+    window.location.hash = "";
+}
+
+function show_default_view() {
+    // This function should only be called from the hashchange
+    // handlers, as it does not set the hash to "".
+    //
     // We only allow all_messages and recent_topics
     // to be rendered without a hash.
     if (page_params.default_view === "recent_topics") {

static/js/hotkey.js
@@ -332,7 +332,7 @@ export function process_escape_key(e) {
         return true;
     }
 
-    hashchange.show_default_view();
+    hashchange.set_hash_to_default_view();
     return true;
 }
 