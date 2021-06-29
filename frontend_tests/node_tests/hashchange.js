afedotov89/flexar-zulip

commit 7dc2a7605e035262ce840cf2b1b5af9bb6885000

hashchange: Show default view when the hash is unknown.
Fixes #18870.

static/js/hashchange.js
@@ -179,6 +179,8 @@ function do_hashchange_normal(from_reload) {
         case "#about-zulip":
             blueslip.error("overlay logic skipped for: " + hash);
             break;
+        default:
+            show_default_view();
     }
     return false;
 }

frontend_tests/node_tests/hashchange.js
@@ -170,14 +170,29 @@ run_test("hash_interactions", ({override}) => {
     window_stub = $.create("window-stub");
     page_params.default_view = "recent_topics";
 
-    override(recent_topics_ui, "show", () => {});
     override(recent_topics_util, "is_visible", () => false);
     const helper = test_helper({override, change_tab: true});
 
-    window.location.hash = "#all_messages";
+    let recent_topics_ui_shown = false;
+    override(recent_topics_ui, "show", () => {
+        recent_topics_ui_shown = true;
+    });
+    window.location.hash = "#unknown_hash";
 
     browser_history.clear_for_testing();
     hashchange.initialize();
+    // If it's an unknown hash it should show the default view.
+    assert.equal(recent_topics_ui_shown, true);
+    helper.assert_events([
+        [overlays, "close_for_hash_change"],
+        [message_viewport, "stop_auto_scrolling"],
+    ]);
+
+    override(recent_topics_ui, "show", () => {});
+    window.location.hash = "#all_messages";
+
+    helper.clear_events();
+    window_stub.trigger("hashchange");
     helper.assert_events([
         [overlays, "close_for_hash_change"],
         [message_viewport, "stop_auto_scrolling"],

commit 0990107e9dfd17d34f176ebd647a18b0e7e6d7b7

hashchange: Don't narrow to default_view on `#reload`.
When the hash changes to `#reload...` before a reload, the app tries
to show default_view since there is no `case` handled for it. What we
want to do is ignore this hashchange, since we're about to be reloaded
(so it's not helpful) and if the timing is wrong, it can cause the
browser to reload back to "Recent topics" rather than saving the
user's view.

This fixes a bug introduced in 7dc2a7605e035262ce840cf2b1b5af9bb6885000.

With tests from Riken Shan.

static/js/hashchange.js
@@ -128,6 +128,7 @@ function do_hashchange_normal(from_reload) {
     // Even if the URL bar says #%41%42%43%44, the value here will
     // be #ABCD.
     const hash = window.location.hash.split("/");
+
     switch (hash[0]) {
         case "#narrow": {
             maybe_hide_recent_topics();
@@ -311,6 +312,13 @@ function hashchanged(from_reload, e) {
     const was_internal_change = browser_history.save_old_hash();
 
     if (was_internal_change) {
+        return undefined;
+    }
+
+    // TODO: Migrate the `#reload` syntax to use slashes as separators
+    // so that this can be part of the main switch statement.
+    if (window.location.hash.startsWith("#reload")) {
+        // We don't want to change narrow if app is undergoing reload.
         return undefined;
     }
 

frontend_tests/node_tests/hashchange.js
@@ -188,7 +188,6 @@ run_test("hash_interactions", ({override}) => {
         [message_viewport, "stop_auto_scrolling"],
     ]);
 
-    override(recent_topics_ui, "show", () => {});
     window.location.hash = "#all_messages";
 
     helper.clear_events();
@@ -247,6 +246,15 @@ run_test("hash_interactions", ({override}) => {
         [overlays, "close_for_hash_change"],
         [subs, "launch"],
     ]);
+
+    recent_topics_ui_shown = false;
+    window.location.hash = "#reload:send_after_reload=0...";
+
+    helper.clear_events();
+    window_stub.trigger("hashchange");
+    helper.assert_events([]);
+    // If it's reload hash it shouldn't show the default view.
+    assert.equal(recent_topics_ui_shown, false);
 
     window.location.hash = "#keyboard-shortcuts/whatever";
 