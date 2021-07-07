afedotov89/flexar-zulip

commit 0453029ab8c28b11431ff5ef684ac6e680ab2bb0

embed_links: Only log warning if worker times out.
Throwing an exception is excessive in case of this worker, as it's
expected for it to time out sometimes if the urls take too long to
process.

With a test added by tabbott.

zerver/worker/queue_processors.py
@@ -763,6 +763,19 @@ class FetchLinksEmbedData(QueueProcessingWorker):
             )
             do_update_embedded_data(message.sender, message, message.content, rendering_result)
 
+    def timer_expired(
+        self, limit: int, events: List[Dict[str, Any]], signal: int, frame: FrameType
+    ) -> None:
+        assert len(events) == 1
+        event = events[0]
+
+        logging.warning(
+            "Timed out after %s seconds while fetching URLs for message %s: %s",
+            limit,
+            event["message_id"],
+            event["urls"],
+        )
+
 
 @assign_queue("outgoing_webhooks")
 class OutgoingWebhookWorker(QueueProcessingWorker):

zerver/tests/test_queue_worker.py
@@ -1,5 +1,6 @@
 import base64
 import os
+import signal
 import time
 from collections import defaultdict
 from inspect import isabstract
@@ -23,6 +24,7 @@ from zerver.tornado.event_queue import build_offline_notification
 from zerver.worker import queue_processors
 from zerver.worker.queue_processors import (
     EmailSendingWorker,
+    FetchLinksEmbedData,
     LoopQueueProcessingWorker,
     MissedMessageWorker,
     QueueProcessingWorker,
@@ -581,6 +583,37 @@ class WorkerTest(ZulipTestCase):
         self.assert_length(events, 1)
         event = events[0]
         self.assertEqual(event["type"], "timeout")
+
+    def test_embed_links_timeout(self) -> None:
+        @queue_processors.assign_queue("timeout_worker", is_test_queue=True)
+        class TimeoutWorker(FetchLinksEmbedData):
+            MAX_CONSUME_SECONDS = 1
+
+            def consume(self, data: Mapping[str, Any]) -> None:
+                # Send SIGALRM to ourselves to simulate a timeout.
+                pid = os.getpid()
+                os.kill(pid, signal.SIGALRM)
+
+        fake_client = self.FakeClient()
+        fake_client.enqueue(
+            "timeout_worker",
+            {
+                "type": "timeout",
+                "message_id": 15,
+                "urls": ["first", "second"],
+            },
+        )
+
+        with simulated_queue_client(lambda: fake_client):
+            worker = TimeoutWorker()
+            worker.setup()
+            worker.ENABLE_TIMEOUTS = True
+            with self.assertLogs(level="WARNING") as m:
+                worker.start()
+                self.assertEqual(
+                    m.records[0].message,
+                    "Timed out after 1 seconds while fetching URLs for message 15: ['first', 'second']",
+                )
 
     def test_worker_noname(self) -> None:
         class TestWorker(queue_processors.QueueProcessingWorker):

commit 152f0a9b58ec6d99153e71769513dbc9278f0c84

embed_links: Interrupt consume() function on worker timeout.
This fixes a bug introduced in 0453029ab8c28b11431ff5ef684ac6e680ab2bb0
which made the worker simply log a warning about the timeout and then
continue consume()ing the event that should have also been interrupted.

The idea here is to introduce an exception which can be used to
interrupt the consume() process without triggering the regular handling
of exceptions that happens in _handle_consume_exception.

zerver/worker/queue_processors.py
@@ -115,6 +115,16 @@ class WorkerTimeoutException(Exception):
         return f"Timed out after {self.limit * self.event_count} seconds processing {self.event_count} events"
 
 
+class InterruptConsumeException(Exception):
+    """
+    This exception is to be thrown inside event consume function
+    if the intention is to simply interrupt the processing
+    of the current event and normally continue the work of the queue.
+    """
+
+    pass
+
+
 class WorkerDeclarationException(Exception):
     pass
 
@@ -341,6 +351,11 @@ class QueueProcessingWorker(ABC):
         raise WorkerTimeoutException(limit, len(events))
 
     def _handle_consume_exception(self, events: List[Dict[str, Any]], exception: Exception) -> None:
+        if isinstance(exception, InterruptConsumeException):
+            # The exception signals that no further error handling
+            # is needed and the worker can proceed.
+            return
+
         with configure_scope() as scope:
             scope.set_context(
                 "events",
@@ -775,6 +790,7 @@ class FetchLinksEmbedData(QueueProcessingWorker):
             event["message_id"],
             event["urls"],
         )
+        raise InterruptConsumeException
 
 
 @assign_queue("outgoing_webhooks")