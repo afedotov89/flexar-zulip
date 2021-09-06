amanagr/zulip

login_to_access: Take user to web_public_compatible hash on exit.
We save the last web_public_compatible hash that user visited
before the modal was shown and take user to that hash when
user exits the modal.

static/js/browser_history.js
@@ -7,6 +7,13 @@ export const state = {
     hash_before_overlay: null,
     old_hash: window.location.hash,
     changing_hash: false,
+    // If the spectator's hash changes to a restricted hash, then we store the old hash
+    // so that we can take user back to the allowed hash.
+    // TODO: Store #narrow old hashes. Currently they are not stored here since, the #narrow
+    // hashes are changed without calling `hashchanged` in many ways.
+    web_public_old_hash: hash_util.is_web_public_compatible(window.location.hash)
+        ? window.location.hash
+        : "#",
 };
 
 export function clear_for_testing() {
@@ -72,4 +79,8 @@ export function update_hash_internally_if_required(hash) {
     if (window.location.hash !== hash) {
         update(hash);
     }
+}
+
+export function return_to_web_public_hash() {
+    window.location.hash = state.web_public_old_hash;
 }

static/js/hashchange.js
@@ -310,11 +310,15 @@ function hashchanged(from_reload, e) {
 
     const was_internal_change = browser_history.save_old_hash();
 
+    const is_hash_web_public_compatible = hash_util.is_web_public_compatible(current_hash);
+    if (is_hash_web_public_compatible) {
+        browser_history.state.web_public_old_hash = current_hash;
+    }
+
     if (was_internal_change) {
         return undefined;
     }
 
-    const is_hash_web_public_compatible = hash_util.is_web_public_compatible(current_hash);
     if (page_params.is_spectator && !is_hash_web_public_compatible) {
         login_to_access.show();
         return undefined;

afedotov89/flexar-zulip

static/js/browser_history.js
@@ -7,6 +7,13 @@ export const state = {
     hash_before_overlay: null,
     old_hash: window.location.hash,
     changing_hash: false,
+    // If the spectator's hash changes to a restricted hash, then we store the old hash
+    // so that we can take user back to the allowed hash.
+    // TODO: Store #narrow old hashes. Currently they are not stored here since, the #narrow
+    // hashes are changed without calling `hashchanged` in many ways.
+    spectator_old_hash: hash_util.is_web_public_compatible(window.location.hash)
+        ? window.location.hash
+        : "#",
 };
 
 export function clear_for_testing() {
@@ -72,4 +79,8 @@ export function update_hash_internally_if_required(hash) {
     if (window.location.hash !== hash) {
         update(hash);
     }
+}
+
+export function return_to_web_public_hash() {
+    window.location.hash = state.spectator_old_hash;
 }

static/js/hashchange.js
@@ -318,6 +318,11 @@ function hashchanged(from_reload, e) {
 
     const was_internal_change = browser_history.save_old_hash();
 
+    const is_hash_web_public_compatible = hash_util.is_web_public_compatible(current_hash);
+    if (is_hash_web_public_compatible) {
+        browser_history.state.spectator_old_hash = current_hash;
+    }
+
     if (was_internal_change) {
         return undefined;
     }
@@ -329,7 +334,6 @@ function hashchanged(from_reload, e) {
         return undefined;
     }
 
-    const is_hash_web_public_compatible = hash_util.is_web_public_compatible(current_hash);
     if (page_params.is_spectator && !is_hash_web_public_compatible) {
         login_to_access.show();
         return undefined;