sahil839/zulip

create_user: Initialize settings according to realm-level defaults.
This commit modifies the copy_user_settings code such that instead
of source user profile, we can have two types of sources - a user
profile and RealmUserDefault table of realm and then set the
settings from RealmUserDefault only is there is no user profile
as a source.

We also rename copy_user_settings to copy_default_settings for
clarity.

zerver/tests/test_users.py
@@ -26,7 +26,7 @@ from zerver.lib.actions import (
     get_recipient_info,
 )
 from zerver.lib.avatar import avatar_url, get_gravatar_url
-from zerver.lib.create_user import copy_user_settings
+from zerver.lib.create_user import copy_default_settings
 from zerver.lib.events import do_events_register
 from zerver.lib.exceptions import JsonableError
 from zerver.lib.send_email import (
@@ -54,6 +54,7 @@ from zerver.models import (
     PreregistrationUser,
     Realm,
     RealmDomain,
+    RealmUserDefault,
     Recipient,
     ScheduledEmail,
     Stream,
@@ -1134,7 +1135,7 @@ class UserProfileTest(ZulipTestCase):
         self.assertIsNone(get_source_profile("iago@zulip.com", 0))
         self.assertIsNone(get_source_profile("iago@zulip.com", lear_realm_id))
 
-    def test_copy_user_settings(self) -> None:
+    def test_copy_default_settings_from_another_user(self) -> None:
         iago = self.example_user("iago")
         cordelia = self.example_user("cordelia")
         hamlet = self.example_user("hamlet")
@@ -1167,7 +1168,7 @@ class UserProfileTest(ZulipTestCase):
         # introducing the user to clients.
         events: List[Mapping[str, Any]] = []
         with self.tornado_redirected_to_list(events, expected_num_events=0):
-            copy_user_settings(cordelia, iago)
+            copy_default_settings(cordelia, iago)
 
         # We verify that cordelia and iago match, but hamlet has the defaults.
         self.assertEqual(iago.full_name, "Cordelia, Lear's daughter")
@@ -1204,6 +1205,34 @@ class UserProfileTest(ZulipTestCase):
 
         hotspots = list(UserHotspot.objects.filter(user=iago).values_list("hotspot", flat=True))
         self.assertEqual(hotspots, hotspots_completed)
+
+    def test_copy_default_settings_from_realm_user_default(self) -> None:
+        cordelia = self.example_user("cordelia")
+        realm = get_realm("zulip")
+        realm_user_default = RealmUserDefault.objects.get(realm=realm)
+
+        realm_user_default.default_view = "recent_topics"
+        realm_user_default.emojiset = "google"
+        realm_user_default.color_scheme = UserProfile.COLOR_SCHEME_LIGHT
+        realm_user_default.enable_offline_email_notifications = False
+        realm_user_default.enable_stream_push_notifications = True
+        realm_user_default.enter_sends = True
+        realm_user_default.save()
+
+        # Check that we didn't send an realm_user update events to
+        # users; this work is happening before the user account is
+        # created, so any changes will be reflected in the "add" event
+        # introducing the user to clients.
+        events: List[Mapping[str, Any]] = []
+        with self.tornado_redirected_to_list(events, expected_num_events=0):
+            copy_default_settings(realm_user_default, cordelia)
+
+        self.assertEqual(cordelia.default_view, "recent_topics")
+        self.assertEqual(cordelia.emojiset, "google")
+        self.assertEqual(cordelia.color_scheme, UserProfile.COLOR_SCHEME_LIGHT)
+        self.assertEqual(cordelia.enable_offline_email_notifications, False)
+        self.assertEqual(cordelia.enable_stream_push_notifications, True)
+        self.assertEqual(cordelia.enter_sends, True)
 
     def test_get_user_by_id_in_realm_including_cross_realm(self) -> None:
         realm = get_realm("zulip")

afedotov89/flexar-zulip

zerver/tests/test_users.py
@@ -25,7 +25,7 @@ from zerver.lib.actions import (
     get_recipient_info,
 )
 from zerver.lib.avatar import avatar_url, get_gravatar_url
-from zerver.lib.create_user import copy_user_settings
+from zerver.lib.create_user import copy_default_settings
 from zerver.lib.events import do_events_register
 from zerver.lib.exceptions import JsonableError
 from zerver.lib.send_email import (
@@ -54,6 +54,7 @@ from zerver.models import (
     PreregistrationUser,
     Realm,
     RealmDomain,
+    RealmUserDefault,
     Recipient,
     ScheduledEmail,
     Stream,
@@ -795,7 +796,7 @@ class QueryCountTest(ZulipTestCase):
                         acting_user=None,
                     )
 
-        self.assert_length(queries, 83)
+        self.assert_length(queries, 84)
         self.assert_length(cache_tries, 27)
 
         peer_add_events = [event for event in events if event["event"].get("op") == "peer_add"]
@@ -1151,7 +1152,7 @@ class UserProfileTest(ZulipTestCase):
         self.assertIsNone(get_source_profile("iago@zulip.com", 0))
         self.assertIsNone(get_source_profile("iago@zulip.com", lear_realm_id))
 
-    def test_copy_user_settings(self) -> None:
+    def test_copy_default_settings_from_another_user(self) -> None:
         iago = self.example_user("iago")
         cordelia = self.example_user("cordelia")
         hamlet = self.example_user("hamlet")
@@ -1184,7 +1185,7 @@ class UserProfileTest(ZulipTestCase):
         # introducing the user to clients.
         events: List[Mapping[str, Any]] = []
         with self.tornado_redirected_to_list(events, expected_num_events=0):
-            copy_user_settings(cordelia, iago)
+            copy_default_settings(cordelia, iago)
 
         # We verify that cordelia and iago match, but hamlet has the defaults.
         self.assertEqual(iago.full_name, "Cordelia, Lear's daughter")
@@ -1221,6 +1222,34 @@ class UserProfileTest(ZulipTestCase):
 
         hotspots = list(UserHotspot.objects.filter(user=iago).values_list("hotspot", flat=True))
         self.assertEqual(hotspots, hotspots_completed)
+
+    def test_copy_default_settings_from_realm_user_default(self) -> None:
+        cordelia = self.example_user("cordelia")
+        realm = get_realm("zulip")
+        realm_user_default = RealmUserDefault.objects.get(realm=realm)
+
+        realm_user_default.default_view = "recent_topics"
+        realm_user_default.emojiset = "twitter"
+        realm_user_default.color_scheme = UserProfile.COLOR_SCHEME_LIGHT
+        realm_user_default.enable_offline_email_notifications = False
+        realm_user_default.enable_stream_push_notifications = True
+        realm_user_default.enter_sends = True
+        realm_user_default.save()
+
+        # Check that we didn't send an realm_user update events to
+        # users; this work is happening before the user account is
+        # created, so any changes will be reflected in the "add" event
+        # introducing the user to clients.
+        events: List[Mapping[str, Any]] = []
+        with self.tornado_redirected_to_list(events, expected_num_events=0):
+            copy_default_settings(realm_user_default, cordelia)
+
+        self.assertEqual(cordelia.default_view, "recent_topics")
+        self.assertEqual(cordelia.emojiset, "twitter")
+        self.assertEqual(cordelia.color_scheme, UserProfile.COLOR_SCHEME_LIGHT)
+        self.assertEqual(cordelia.enable_offline_email_notifications, False)
+        self.assertEqual(cordelia.enable_stream_push_notifications, True)
+        self.assertEqual(cordelia.enter_sends, True)
 
     def test_get_user_by_id_in_realm_including_cross_realm(self) -> None:
         realm = get_realm("zulip")