EmmalineLake/zulip

realm_domains_modal: Migrate modal to dialog_widget.

frontend_tests/node_tests/dispatch.js
@@ -61,6 +61,7 @@ const settings_profile_fields = mock_esm("../../static/js/settings_profile_field
 const settings_realm_user_settings_defaults = mock_esm(
     "../../static/js/settings_realm_user_settings_defaults",
 );
+const settings_realm_domains = mock_esm("../../static/js/settings_realm_domains");
 const settings_streams = mock_esm("../../static/js/settings_streams");
 const settings_user_groups = mock_esm("../../static/js/settings_user_groups");
 const settings_users = mock_esm("../../static/js/settings_users");
@@ -548,16 +549,19 @@ run_test("realm_playgrounds", ({override}) => {
 run_test("realm_domains", ({override}) => {
     let event = event_fixtures.realm_domains__add;
     page_params.realm_domains = [];
-    override(settings_org, "populate_realm_domains", noop);
+    override(settings_org, "populate_realm_domains_label", noop);
+    override(settings_realm_domains, "maybe_populate_realm_domains_table", noop);
     dispatch(event);
     assert_same(page_params.realm_domains, [event.realm_domain]);
 
-    override(settings_org, "populate_realm_domains", noop);
+    override(settings_org, "populate_realm_domains_label", noop);
+    override(settings_realm_domains, "maybe_populate_realm_domains_table", noop);
     event = event_fixtures.realm_domains__change;
     dispatch(event);
     assert_same(page_params.realm_domains, [event.realm_domain]);
 
-    override(settings_org, "populate_realm_domains", noop);
+    override(settings_org, "populate_realm_domains_label", noop);
+    override(settings_realm_domains, "maybe_populate_realm_domains_table", noop);
     event = event_fixtures.realm_domains__remove;
     dispatch(event);
     assert_same(page_params.realm_domains, []);

static/js/server_events_dispatch.js
@@ -51,6 +51,7 @@ import * as settings_notifications from "./settings_notifications";
 import * as settings_org from "./settings_org";
 import * as settings_playgrounds from "./settings_playgrounds";
 import * as settings_profile_fields from "./settings_profile_fields";
+import * as settings_realm_domains from "./settings_realm_domains";
 import * as settings_realm_user_settings_defaults from "./settings_realm_user_settings_defaults";
 import * as settings_streams from "./settings_streams";
 import * as settings_user_groups from "./settings_user_groups";
@@ -355,7 +356,10 @@ export function dispatch_normal_event(event) {
                 switch (event.op) {
                     case "add":
                         page_params.realm_domains.push(event.realm_domain);
-                        settings_org.populate_realm_domains(page_params.realm_domains);
+                        settings_org.populate_realm_domains_label(page_params.realm_domains);
+                        settings_realm_domains.maybe_populate_realm_domains_table(
+                            page_params.realm_domains,
+                        );
                         break;
                     case "change":
                         for (i = 0; i < page_params.realm_domains.length; i += 1) {
@@ -365,7 +369,10 @@ export function dispatch_normal_event(event) {
                                 break;
                             }
                         }
-                        settings_org.populate_realm_domains(page_params.realm_domains);
+                        settings_org.populate_realm_domains_label(page_params.realm_domains);
+                        settings_realm_domains.maybe_populate_realm_domains_table(
+                            page_params.realm_domains,
+                        );
                         break;
                     case "remove":
                         for (i = 0; i < page_params.realm_domains.length; i += 1) {
@@ -374,7 +381,10 @@ export function dispatch_normal_event(event) {
                                 break;
                             }
                         }
-                        settings_org.populate_realm_domains(page_params.realm_domains);
+                        settings_org.populate_realm_domains_label(page_params.realm_domains);
+                        settings_realm_domains.maybe_populate_realm_domains_table(
+                            page_params.realm_domains,
+                        );
                         break;
                     default:
                         blueslip.error("Unexpected event type realm_domains/" + event.op);

afedotov89/flexar-zulip

frontend_tests/node_tests/dispatch.js
@@ -60,6 +60,7 @@ const settings_profile_fields = mock_esm("../../static/js/settings_profile_field
 const settings_realm_user_settings_defaults = mock_esm(
     "../../static/js/settings_realm_user_settings_defaults",
 );
+const settings_realm_domains = mock_esm("../../static/js/settings_realm_domains");
 const settings_streams = mock_esm("../../static/js/settings_streams");
 const settings_user_groups = mock_esm("../../static/js/settings_user_groups");
 const settings_users = mock_esm("../../static/js/settings_users");
@@ -616,16 +617,19 @@ run_test("realm_playgrounds", ({override}) => {
 run_test("realm_domains", ({override}) => {
     let event = event_fixtures.realm_domains__add;
     page_params.realm_domains = [];
-    override(settings_org, "populate_realm_domains", noop);
+    override(settings_org, "populate_realm_domains_label", noop);
+    override(settings_realm_domains, "populate_realm_domains_table", noop);
     dispatch(event);
     assert_same(page_params.realm_domains, [event.realm_domain]);
 
-    override(settings_org, "populate_realm_domains", noop);
+    override(settings_org, "populate_realm_domains_label", noop);
+    override(settings_realm_domains, "populate_realm_domains_table", noop);
     event = event_fixtures.realm_domains__change;
     dispatch(event);
     assert_same(page_params.realm_domains, [event.realm_domain]);
 
-    override(settings_org, "populate_realm_domains", noop);
+    override(settings_org, "populate_realm_domains_label", noop);
+    override(settings_realm_domains, "populate_realm_domains_table", noop);
     event = event_fixtures.realm_domains__remove;
     dispatch(event);
     assert_same(page_params.realm_domains, []);

static/js/server_events_dispatch.js
@@ -53,6 +53,7 @@ import * as settings_notifications from "./settings_notifications";
 import * as settings_org from "./settings_org";
 import * as settings_playgrounds from "./settings_playgrounds";
 import * as settings_profile_fields from "./settings_profile_fields";
+import * as settings_realm_domains from "./settings_realm_domains";
 import * as settings_realm_user_settings_defaults from "./settings_realm_user_settings_defaults";
 import * as settings_streams from "./settings_streams";
 import * as settings_user_groups from "./settings_user_groups";
@@ -378,7 +379,10 @@ export function dispatch_normal_event(event) {
                 switch (event.op) {
                     case "add":
                         page_params.realm_domains.push(event.realm_domain);
-                        settings_org.populate_realm_domains(page_params.realm_domains);
+                        settings_org.populate_realm_domains_label(page_params.realm_domains);
+                        settings_realm_domains.populate_realm_domains_table(
+                            page_params.realm_domains,
+                        );
                         break;
                     case "change":
                         for (i = 0; i < page_params.realm_domains.length; i += 1) {
@@ -388,7 +392,10 @@ export function dispatch_normal_event(event) {
                                 break;
                             }
                         }
-                        settings_org.populate_realm_domains(page_params.realm_domains);
+                        settings_org.populate_realm_domains_label(page_params.realm_domains);
+                        settings_realm_domains.populate_realm_domains_table(
+                            page_params.realm_domains,
+                        );
                         break;
                     case "remove":
                         for (i = 0; i < page_params.realm_domains.length; i += 1) {
@@ -397,7 +404,10 @@ export function dispatch_normal_event(event) {
                                 break;
                             }
                         }
-                        settings_org.populate_realm_domains(page_params.realm_domains);
+                        settings_org.populate_realm_domains_label(page_params.realm_domains);
+                        settings_realm_domains.populate_realm_domains_table(
+                            page_params.realm_domains,
+                        );
                         break;
                     default:
                         blueslip.error("Unexpected event type realm_domains/" + event.op);