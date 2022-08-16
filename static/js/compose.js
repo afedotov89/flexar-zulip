afedotov89/flexar-zulip

commit 07fd99b7f90acea99a052d47bc58352f45ce53d3

compose: Focus 'Write` button on entering preview mode.
Till now, switching back to writing mode after preview mode, needed
the user to first focus on the `Write` (unpreview) button by tabbing
to it (if using keyboard) and then select it.

To make things easier, especially when using keyboard, now the `Write`
button will be automatically focused on entering preview mode, so
going back to writing mode only needs one 'Enter` keystroke.

static/js/compose.js
@@ -681,6 +681,7 @@ export function initialize() {
         $("#compose-textarea").hide();
         $("#compose .markdown_preview").hide();
         $("#compose .undo_markdown_preview").show();
+        $("#compose .undo_markdown_preview").trigger("focus");
         $("#compose .preview_message_area").show();
 
         render_and_show_preview(

commit 8c9065bb19b15fc774c3da3348be6d155e5aafaf

compose: Fix enter returning user to preview mode.
I'm not sure whether the bug this fixes was a regression resulting
from 07fd99b7f90acea99a052d47bc58352f45ce53d3, or an old bug, but
focusing the compose box is not sufficient to end preview mode; we
should be calling the function that's explicitly for that.

static/js/compose.js
@@ -273,8 +273,8 @@ export function enter_with_preview_open() {
         // If enter_sends is enabled, we attempt to send the message
         finish();
     } else {
-        // Otherwise, we return to the compose box and focus it
-        $("#compose-textarea").trigger("focus");
+        // Otherwise, we return to the normal compose state.
+        clear_preview_area();
     }
 }
 