afedotov89/flexar-zulip

commit 9b0d15f8900b66c30b3b56ee588e52e410091546

python: Use a real parser for email addresses.
Now that we can assume Python 3.6+, we can use the
email.headerregistry module to replace hacky manual email address
parsing.

Signed-off-by: Anders Kaseorg <anders@zulip.com>

zerver/lib/email_validation.py
@@ -1,3 +1,4 @@
+from email.headerregistry import Address
 from typing import Callable, Dict, Optional, Set, Tuple
 
 from django.core import validators
@@ -13,15 +14,13 @@ from zerver.models import (
     EmailContainsPlusError,
     Realm,
     RealmDomain,
-    email_to_domain,
-    email_to_username,
     get_users_by_delivery_email,
     is_cross_realm_bot_email,
 )
 
 
 def validate_disposable(email: str) -> None:
-    if is_disposable_domain(email_to_domain(email)):
+    if is_disposable_domain(Address(addr_spec=email).domain):
         raise DisposableEmailError
 
 
@@ -59,10 +58,11 @@ def get_realm_email_validator(realm: Realm) -> Callable[[str], None]:
         a small whitelist.
         """
 
-        if "+" in email_to_username(email):
+        address = Address(addr_spec=email)
+        if "+" in address.username:
             raise EmailContainsPlusError
 
-        domain = email_to_domain(email)
+        domain = address.domain
 
         if domain in allowed_domains:
             return

zerver/views/message_send.py
@@ -1,4 +1,5 @@
 import sys
+from email.headerregistry import Address
 from typing import Iterable, Optional, Sequence, Union, cast
 
 from dateutil.parser import parse as dateparser
@@ -32,7 +33,6 @@ from zerver.models import (
     Realm,
     RealmDomain,
     UserProfile,
-    email_to_domain,
     get_user_including_cross_realm,
 )
 
@@ -97,7 +97,7 @@ def same_realm_zephyr_user(user_profile: UserProfile, email: str) -> bool:
     except ValidationError:
         return False
 
-    domain = email_to_domain(email)
+    domain = Address(addr_spec=email).domain
 
     # Assumes allow_subdomains=False for all RealmDomain's corresponding to
     # these realms.
@@ -116,7 +116,9 @@ def same_realm_irc_user(user_profile: UserProfile, email: str) -> bool:
     except ValidationError:
         return False
 
-    domain = email_to_domain(email).replace("irc.", "")
+    domain = Address(addr_spec=email).domain
+    if domain.startswith("irc."):
+        domain = domain[len("irc.") :]
 
     # Assumes allow_subdomains=False for all RealmDomain's corresponding to
     # these realms.
@@ -131,7 +133,7 @@ def same_realm_jabber_user(user_profile: UserProfile, email: str) -> bool:
 
     # If your Jabber users have a different email domain than the
     # Zulip users, this is where you would do any translation.
-    domain = email_to_domain(email)
+    domain = Address(addr_spec=email).domain
 
     # Assumes allow_subdomains=False for all RealmDomain's corresponding to
     # these realms.

zerver/actions/message_send.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 from collections import defaultdict
+from email.headerregistry import Address
 from typing import (
     TYPE_CHECKING,
     AbstractSet,
@@ -92,11 +93,11 @@ if TYPE_CHECKING:
 
 
 def compute_irc_user_fullname(email: str) -> str:
-    return email.split("@")[0] + " (IRC)"
+    return Address(addr_spec=email).username + " (IRC)"
 
 
 def compute_jabber_user_fullname(email: str) -> str:
-    return email.split("@")[0] + " (XMPP)"
+    return Address(addr_spec=email).username + " (XMPP)"
 
 
 def get_user_profile_delivery_email_cache_key(

frontend_tests/node_tests/filter.js
@@ -2,6 +2,8 @@
 
 const {strict: assert} = require("assert");
 
+const {parseOneAddress} = require("email-addresses");
+
 const {mock_esm, with_overrides, zrequire} = require("../zjsunit/namespace");
 const {run_test} = require("../zjsunit/test");
 const blueslip = require("../zjsunit/zblueslip");
@@ -1575,7 +1577,7 @@ test("navbar_helpers", () => {
             icon: "envelope",
             title: properly_separated_names([joe.full_name]),
             redirect_url_with_search:
-                "/#narrow/pm-with/" + joe.user_id + "-" + joe.email.split("@")[0],
+                "/#narrow/pm-with/" + joe.user_id + "-" + parseOneAddress(joe.email).local,
         },
         {
             operator: group_pm,
@@ -1610,13 +1612,13 @@ test("navbar_helpers", () => {
         {
             operator: sender_me,
             redirect_url_with_search:
-                "/#narrow/sender/" + me.user_id + "-" + me.email.split("@")[0],
+                "/#narrow/sender/" + me.user_id + "-" + parseOneAddress(me.email).local,
             is_common_narrow: false,
         },
         {
             operator: sender_joe,
             redirect_url_with_search:
-                "/#narrow/sender/" + joe.user_id + "-" + joe.email.split("@")[0],
+                "/#narrow/sender/" + joe.user_id + "-" + parseOneAddress(joe.email).local,
             is_common_narrow: false,
         },
     ];

static/js/people.js
@@ -1,5 +1,6 @@
 import md5 from "blueimp-md5";
 import {format, utcToZonedTime} from "date-fns-tz";
+import {parseOneAddress} from "email-addresses";
 
 import * as typeahead from "../shared/js/typeahead";
 
@@ -508,7 +509,7 @@ export function pm_with_url(message) {
     } else {
         const person = get_by_user_id(user_ids[0]);
         if (person && person.email) {
-            suffix = person.email.split("@")[0].toLowerCase();
+            suffix = parseOneAddress(person.email).local.toLowerCase();
         } else {
             blueslip.error("Unknown people in message");
             suffix = "unk";
@@ -582,7 +583,7 @@ export function emails_to_slug(emails_string) {
     const emails = emails_string.split(",");
 
     if (emails.length === 1) {
-        slug += emails[0].split("@")[0].toLowerCase();
+        slug += parseOneAddress(emails[0]).local.toLowerCase();
     } else {
         slug += "group";
     }

tools/tail-ses
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import sys
+from email.headerregistry import Address
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 from scripts.lib.setup_path import setup_path
@@ -32,7 +33,7 @@ def main() -> None:
     session = boto3.Session()
 
     from_address = settings.NOREPLY_EMAIL_ADDRESS
-    from_host = from_address.split("@")[1]
+    from_host = Address(addr_spec=from_address).domain
 
     ses: SESClient = session.client("ses")
     possible_identities = []

version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 133
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (195, 0)
+PROVISION_VERSION = (195, 1)

zerver/actions/realm_settings.py
@@ -1,3 +1,4 @@
+from email.headerregistry import Address
 from typing import Any, Dict, Literal, Optional
 
 import orjson
@@ -370,7 +371,9 @@ def do_scrub_realm(realm: Realm, *, acting_user: Optional[UserProfile]) -> None:
         do_delete_messages_by_sender(user)
         do_delete_avatar_image(user, acting_user=acting_user)
         user.full_name = f"Scrubbed {generate_key()[:15]}"
-        scrubbed_email = f"scrubbed-{generate_key()[:15]}@{realm.host}"
+        scrubbed_email = Address(
+            username=f"scrubbed-{generate_key()[:15]}", domain=realm.host
+        ).addr_spec
         user.email = scrubbed_email
         user.delivery_email = scrubbed_email
         user.save(update_fields=["full_name", "email", "delivery_email"])

commit 3ddd5fa8f96e34d9736ed62d22c78be792615547

email_validation: Restore case-insensitive domain validation.
This was broken by commit 9b0d15f8900b66c30b3b56ee588e52e410091546
(#22604), because email_to_domain implicitly lowercased the result.

No adjustment is needed for is_disposable_domain, which already
lowercases its argument.

Signed-off-by: Anders Kaseorg <anders@zulip.com>

zerver/lib/email_validation.py
@@ -62,7 +62,7 @@ def get_realm_email_validator(realm: Realm) -> Callable[[str], None]:
         if "+" in address.username:
             raise EmailContainsPlusError
 
-        domain = address.domain
+        domain = address.domain.lower()
 
         if domain in allowed_domains:
             return

zerver/views/message_send.py
@@ -97,7 +97,7 @@ def same_realm_zephyr_user(user_profile: UserProfile, email: str) -> bool:
     except ValidationError:
         return False
 
-    domain = Address(addr_spec=email).domain
+    domain = Address(addr_spec=email).domain.lower()
 
     # Assumes allow_subdomains=False for all RealmDomain's corresponding to
     # these realms.
@@ -116,7 +116,7 @@ def same_realm_irc_user(user_profile: UserProfile, email: str) -> bool:
     except ValidationError:
         return False
 
-    domain = Address(addr_spec=email).domain
+    domain = Address(addr_spec=email).domain.lower()
     if domain.startswith("irc."):
         domain = domain[len("irc.") :]
 
@@ -133,7 +133,7 @@ def same_realm_jabber_user(user_profile: UserProfile, email: str) -> bool:
 
     # If your Jabber users have a different email domain than the
     # Zulip users, this is where you would do any translation.
-    domain = Address(addr_spec=email).domain
+    domain = Address(addr_spec=email).domain.lower()
 
     # Assumes allow_subdomains=False for all RealmDomain's corresponding to
     # these realms.