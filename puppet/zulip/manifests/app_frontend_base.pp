afedotov89/flexar-zulip

commit 550be1d48f291febf695adb554b062756dee40aa

puppet: Always create a uwsgi master control socket.
This is potentially useful even with rolling restarts disabled.

puppet/zulip/manifests/app_frontend_base.pp
@@ -119,12 +119,6 @@ class zulip::app_frontend_base {
     notify  => Service[$zulip::common::supervisor_service],
   }
 
-  $uwsgi_rolling_restart = zulipconf('application_server', 'rolling_restart', '')
-  if $uwsgi_rolling_restart == '' {
-    file { '/home/zulip/deployments/uwsgi-control':
-      ensure => absent,
-    }
-  }
   $uwsgi_listen_backlog_limit = zulipconf('application_server', 'uwsgi_listen_backlog_limit', 128)
   $uwsgi_buffer_size = zulipconf('application_server', 'uwsgi_buffer_size', 8192)
   $uwsgi_processes = zulipconf('application_server', 'uwsgi_processes', $uwsgi_default_processes)

puppet/zulip/templates/uwsgi.ini.template.erb
@@ -15,6 +15,9 @@ chown-socket=zulip:zulip
 socket=/home/zulip/deployments/uwsgi-socket
 listen=<%= @uwsgi_listen_backlog_limit %>
 
+# Create a control socket, allowing fancier runtime control
+master-fifo=/home/zulip/deployments/uwsgi-control
+
 
 
 # How many serving processes to fork
@@ -46,9 +49,6 @@ hook-post-fork=chdir:/home/zulip/deployments/current
 # lazy-apps are required for rolling restarts:
 # https://uwsgi-docs.readthedocs.io/en/latest/articles/TheArtOfGracefulReloading.html#preforking-vs-lazy-apps-vs-lazy
 lazy-apps=true
-
-# Create a control socket, allowing fancier runtime control
-master-fifo=/home/zulip/deployments/uwsgi-control
 
 
 

commit 0d70876ec0d3d1a46c7a9729ff1b9df340a7d0c2

puppet: Bring back uwsgi_rolling_restart config.
550be1d48f29 mistakenly deleted this variable assignment, which made
the `zulip.conf` configuration setting not work -- uwsgi's `lazy_apps`
were not enabled, which are required for rolling restart.

puppet/zulip/manifests/app_frontend_base.pp
@@ -152,6 +152,7 @@ class zulip::app_frontend_base {
     notify  => Service[$zulip::common::supervisor_service],
   }
 
+  $uwsgi_rolling_restart = zulipconf('application_server', 'rolling_restart', '')
   $uwsgi_listen_backlog_limit = zulipconf('application_server', 'uwsgi_listen_backlog_limit', 128)
   $uwsgi_processes = zulipconf('application_server', 'uwsgi_processes', $uwsgi_default_processes)
   $somaxconn = 2 * Integer($uwsgi_listen_backlog_limit)