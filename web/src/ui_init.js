afedotov89/flexar-zulip

commit 06f160581d2fed40be09f37869c81f68d0e2db9b

ui_init: Properly call reject with an error, not an xhr.
If the spectator registration call fails, properly log the error and
call `reject` with an error object, not the xhr that `channel.post`
calls its error callback with.

This does nothing to address the UI question of what to do should this
request fail.

web/src/ui_init.js
@@ -731,8 +731,19 @@ $(async () => {
             }),
             client_gravatar: false,
         };
-        const {result, msg, ...state} = await new Promise((success, error) => {
-            channel.post({url: "/json/register", data, success, error});
+        const {result, msg, ...state} = await new Promise((resolve, reject) => {
+            channel.post({
+                url: "/json/register",
+                data,
+                resolve,
+                error(xhr) {
+                    blueslip.error("Spectator failed to register", {
+                        status: xhr.status,
+                        bodt: xhr.responseText,
+                    });
+                    reject(new Error("Spectator failed to register"));
+                },
+            });
         });
         Object.assign(page_params, state);
     }

commit 17e4f487e11479bf8b8730e43ba960835e33976f

ui_init: Fix loading spectators view.
06f160581d2fed40be09f37869c81f68d0e2db9b broke the success code path.

web/src/ui_init.js
@@ -735,7 +735,7 @@ $(async () => {
             channel.post({
                 url: "/json/register",
                 data,
-                resolve,
+                success: resolve,
                 error(xhr) {
                     blueslip.error("Spectator failed to register", {
                         status: xhr.status,