afedotov89/flexar-zulip

commit 9c4177dbdd52f004dddd68365d80b153bbb4011a

mention: Send notifications for @topic wildcard mentions.
This commit completes the notifications part of the @topic
wildcard mention feature.

Notifications are sent to the topic participants for the
@topic wildcard mention.

zerver/actions/message_edit.py
@@ -258,7 +258,7 @@ def get_mentions_for_message_updates(message_id: int) -> Set[int]:
 def update_user_message_flags(
     rendering_result: MessageRenderingResult, ums: Iterable[UserMessage]
 ) -> None:
-    wildcard = rendering_result.mentions_stream_wildcard
+    wildcard_mentioned = rendering_result.has_wildcard_mention()
     mentioned_ids = rendering_result.mentions_user_ids
     ids_with_alert_words = rendering_result.user_ids_with_alert_words
     changed_ums: Set[UserMessage] = set()
@@ -280,7 +280,7 @@ def update_user_message_flags(
         mentioned = um.user_profile_id in mentioned_ids
         update_flag(um, mentioned, UserMessage.flags.mentioned)
 
-        update_flag(um, wildcard, UserMessage.flags.wildcard_mentioned)
+        update_flag(um, wildcard_mentioned, UserMessage.flags.wildcard_mentioned)
 
     for um in changed_ums:
         um.save(update_fields=["flags"])
@@ -489,6 +489,15 @@ def do_update_message(
         else:
             event["stream_wildcard_mention_user_ids"] = []
             event["stream_wildcard_mention_in_followed_topic_user_ids"] = []
+
+        if rendering_result.mentions_topic_wildcard:
+            event["topic_wildcard_mention_user_ids"] = list(info.topic_wildcard_mention_user_ids)
+            event["topic_wildcard_mention_in_followed_topic_user_ids"] = list(
+                info.topic_wildcard_mention_in_followed_topic_user_ids
+            )
+        else:
+            event["topic_wildcard_mention_user_ids"] = []
+            event["topic_wildcard_mention_in_followed_topic_user_ids"] = []
 
         do_update_mobile_push_notification(
             target_message,
@@ -1249,7 +1258,7 @@ def check_update_message(
         )
         links_for_embed |= rendering_result.links_for_preview
 
-        if message.is_stream_message() and rendering_result.mentions_stream_wildcard:
+        if message.is_stream_message() and rendering_result.has_wildcard_mention():
             stream = access_stream_by_id(user_profile, message.recipient.type_id)[0]
             if not wildcard_mention_allowed(message.sender, stream):
                 raise JsonableError(

zerver/tests/test_message_edit.py
@@ -2039,6 +2039,58 @@ class EditMessageTest(EditMessageTestCase):
         )
 
     @mock.patch("zerver.actions.message_edit.send_event")
+    def test_topic_wildcard_mention_in_followed_topic(
+        self, mock_send_event: mock.MagicMock
+    ) -> None:
+        stream_name = "Macbeth"
+        hamlet = self.example_user("hamlet")
+        cordelia = self.example_user("cordelia")
+        self.make_stream(stream_name, history_public_to_subscribers=True)
+        self.subscribe(hamlet, stream_name)
+        self.subscribe(cordelia, stream_name)
+        self.login_user(hamlet)
+
+        do_set_user_topic_visibility_policy(
+            user_profile=hamlet,
+            stream=get_stream(stream_name, cordelia.realm),
+            topic="test",
+            visibility_policy=UserTopic.VisibilityPolicy.FOLLOWED,
+        )
+        message_id = self.send_stream_message(hamlet, stream_name, "Hello everyone")
+
+        def notify(user_id: int) -> Dict[str, Any]:
+            return {
+                "id": user_id,
+                "flags": ["wildcard_mentioned"],
+            }
+
+        users_to_be_notified = sorted(map(notify, [cordelia.id, hamlet.id]), key=itemgetter("id"))
+        result = self.client_patch(
+            f"/json/messages/{message_id}",
+            {
+                "content": "Hello @**topic**",
+            },
+        )
+        self.assert_json_success(result)
+
+        # Extract the send_event call where event type is 'update_message'.
+        # Here we assert 'topic_wildcard_mention_in_followed_topic_user_ids'
+        # has been set properly.
+        called = False
+        for call_args in mock_send_event.call_args_list:
+            (arg_realm, arg_event, arg_notified_users) = call_args[0]
+            if arg_event["type"] == "update_message":
+                self.assertEqual(arg_event["type"], "update_message")
+                self.assertEqual(
+                    arg_event["topic_wildcard_mention_in_followed_topic_user_ids"], [hamlet.id]
+                )
+                self.assertEqual(
+                    sorted(arg_notified_users, key=itemgetter("id")), users_to_be_notified
+                )
+                called = True
+        self.assertTrue(called)
+
+    @mock.patch("zerver.actions.message_edit.send_event")
     def test_stream_wildcard_mention_in_followed_topic(
         self, mock_send_event: mock.MagicMock
     ) -> None:
@@ -2089,6 +2141,95 @@ class EditMessageTest(EditMessageTestCase):
                 )
                 called = True
         self.assertTrue(called)
+
+    @mock.patch("zerver.actions.message_edit.send_event")
+    def test_topic_wildcard_mention(self, mock_send_event: mock.MagicMock) -> None:
+        stream_name = "Macbeth"
+        hamlet = self.example_user("hamlet")
+        cordelia = self.example_user("cordelia")
+        self.make_stream(stream_name, history_public_to_subscribers=True)
+        self.subscribe(hamlet, stream_name)
+        self.subscribe(cordelia, stream_name)
+        self.login_user(hamlet)
+        message_id = self.send_stream_message(hamlet, stream_name, "Hello everyone")
+
+        def notify(user_id: int) -> Dict[str, Any]:
+            return {
+                "id": user_id,
+                "flags": ["wildcard_mentioned"],
+            }
+
+        users_to_be_notified = sorted(map(notify, [cordelia.id, hamlet.id]), key=itemgetter("id"))
+        result = self.client_patch(
+            f"/json/messages/{message_id}",
+            {
+                "content": "Hello @**topic**",
+            },
+        )
+        self.assert_json_success(result)
+
+        # Extract the send_event call where event type is 'update_message'.
+        # Here we assert topic_wildcard_mention_user_ids has been set properly.
+        called = False
+        for call_args in mock_send_event.call_args_list:
+            (arg_realm, arg_event, arg_notified_users) = call_args[0]
+            if arg_event["type"] == "update_message":
+                self.assertEqual(arg_event["type"], "update_message")
+                self.assertEqual(arg_event["topic_wildcard_mention_user_ids"], [hamlet.id])
+                self.assertEqual(
+                    sorted(arg_notified_users, key=itemgetter("id")), users_to_be_notified
+                )
+                called = True
+        self.assertTrue(called)
+
+    def test_topic_wildcard_mention_restrictions_when_editing(self) -> None:
+        cordelia = self.example_user("cordelia")
+        shiva = self.example_user("shiva")
+        self.login("cordelia")
+        stream_name = "Macbeth"
+        self.make_stream(stream_name, history_public_to_subscribers=True)
+        self.subscribe(cordelia, stream_name)
+        self.subscribe(shiva, stream_name)
+        message_id = self.send_stream_message(cordelia, stream_name, "Hello everyone")
+
+        realm = cordelia.realm
+        do_set_realm_property(
+            realm,
+            "wildcard_mention_policy",
+            Realm.WILDCARD_MENTION_POLICY_MODERATORS,
+            acting_user=None,
+        )
+
+        with mock.patch("zerver.lib.message.num_subscribers_for_stream_id", return_value=17):
+            result = self.client_patch(
+                "/json/messages/" + str(message_id),
+                {
+                    "content": "Hello @**topic**",
+                },
+            )
+        self.assert_json_error(
+            result, "You do not have permission to use wildcard mentions in this stream."
+        )
+
+        with mock.patch("zerver.lib.message.num_subscribers_for_stream_id", return_value=14):
+            result = self.client_patch(
+                "/json/messages/" + str(message_id),
+                {
+                    "content": "Hello @**topic**",
+                },
+            )
+        self.assert_json_success(result)
+
+        self.login("shiva")
+        message_id = self.send_stream_message(shiva, stream_name, "Hi everyone")
+        with mock.patch("zerver.lib.message.num_subscribers_for_stream_id", return_value=17):
+            result = self.client_patch(
+                "/json/messages/" + str(message_id),
+                {
+                    "content": "Hello @**topic**",
+                },
+            )
+        self.assert_json_success(result)
 
     @mock.patch("zerver.actions.message_edit.send_event")
     def test_stream_wildcard_mention(self, mock_send_event: mock.MagicMock) -> None:

templates/zerver/emails/missed_message.html
@@ -29,8 +29,12 @@
     {% trans %}You are receiving this because you were personally mentioned.{% endtrans %}<br />
     {% elif mentioned_user_group_name %}
     {% trans %}You are receiving this because @{{ mentioned_user_group_name }} was mentioned.{% endtrans %}<br />
+    {% elif topic_wildcard_mentioned_in_followed_topic %}
+    {% trans %}You are receiving this because all topic participants were mentioned in #{{ stream_name }} > {{ topic_name }}.{% endtrans %}<br />
     {% elif stream_wildcard_mentioned_in_followed_topic %}
     {% trans %}You are receiving this because you have wildcard mention notifications enabled for topics you follow.{% endtrans %}<br />
+    {% elif topic_wildcard_mentioned %}
+    {% trans %}You are receiving this because all topic participants were mentioned in #{{ stream_name }} > {{ topic_name }}.{% endtrans %}<br />
     {% elif stream_wildcard_mentioned %}
     {% trans %}You are receiving this because everyone was mentioned in #{{ stream_name }}.{% endtrans %}<br />
     {% elif followed_topic_email_notify %}

web/src/markdown.js
@@ -133,6 +133,7 @@ function parse_with_options({raw_content, helper_config, options}) {
                     display_text = mention;
                 } else {
                     // Topic Wildcard mention
+                    mentioned_wildcard = true;
                     display_text = "@" + mention;
                     classes = "topic-mention";
                 }

web/tests/markdown.test.js
@@ -711,10 +711,10 @@ test("message_flags", () => {
     input = "testing this @**all** @**Cordelia, Lear's daughter**";
     message = {topic: "No links here", raw_content: input};
     markdown.apply_markdown(message);
-
     assert.equal(message.is_me_message, false);
     assert.equal(message.flags.includes("mentioned"), true);
     assert.equal(message.flags.includes("wildcard_mentioned"), true);
+
     input = "test @**everyone**";
     message = {topic: "No links here", raw_content: input};
     markdown.apply_markdown(message);
@@ -729,6 +729,13 @@ test("message_flags", () => {
     assert.equal(message.flags.includes("wildcard_mentioned"), true);
     assert.equal(message.flags.includes("mentioned"), false);
 
+    input = "test @**topic**";
+    message = {topic: "No links here", raw_content: input};
+    markdown.apply_markdown(message);
+    assert.equal(message.is_me_message, false);
+    assert.equal(message.flags.includes("wildcard_mentioned"), true);
+    assert.equal(message.flags.includes("mentioned"), false);
+
     input = "test @all";
     message = {topic: "No links here", raw_content: input};
     markdown.apply_markdown(message);
@@ -736,6 +743,12 @@ test("message_flags", () => {
     assert.equal(message.flags.includes("mentioned"), false);
 
     input = "test @everyone";
+    message = {topic: "No links here", raw_content: input};
+    markdown.apply_markdown(message);
+    assert.equal(message.flags.includes("wildcard_mentioned"), false);
+    assert.equal(message.flags.includes("mentioned"), false);
+
+    input = "test @topic";
     message = {topic: "No links here", raw_content: input};
     markdown.apply_markdown(message);
     assert.equal(message.flags.includes("wildcard_mentioned"), false);
@@ -778,6 +791,18 @@ test("message_flags", () => {
     assert.equal(message.flags.includes("mentioned"), false);
 
     input = "> test @**all**";
+    message = {topic: "No links here", raw_content: input};
+    markdown.apply_markdown(message);
+    assert.equal(message.flags.includes("wildcard_mentioned"), false);
+    assert.equal(message.flags.includes("mentioned"), false);
+
+    input = "test @_**topic**";
+    message = {topic: "No links here", raw_content: input};
+    markdown.apply_markdown(message);
+    assert.equal(message.flags.includes("wildcard_mentioned"), false);
+    assert.equal(message.flags.includes("mentioned"), false);
+
+    input = "> test @**topic**";
     message = {topic: "No links here", raw_content: input};
     markdown.apply_markdown(message);
     assert.equal(message.flags.includes("wildcard_mentioned"), false);

zerver/actions/message_send.py
@@ -609,9 +609,9 @@ def build_message_send_dict(
         members = mention_data.get_group_members(group_id)
         rendering_result.mentions_user_ids.update(members)
 
-    # Only send data to Tornado about stream wildcard mentions if message
-    # rendering determined the message had an actual stream wildcard
-    # mention in it (and not e.g. stream wildcard mention syntax inside a
+    # Only send data to Tornado about stream or topic wildcard mentions if message
+    # rendering determined the message had an actual stream or topic wildcard
+    # mention in it (and not e.g. stream or topic wildcard mention syntax inside a
     # code block).
     if rendering_result.mentions_stream_wildcard:
         stream_wildcard_mention_user_ids = info.stream_wildcard_mention_user_ids
@@ -622,6 +622,14 @@ def build_message_send_dict(
         stream_wildcard_mention_user_ids = set()
         stream_wildcard_mention_in_followed_topic_user_ids = set()
 
+    if rendering_result.mentions_topic_wildcard:
+        topic_wildcard_mention_user_ids = info.topic_wildcard_mention_user_ids
+        topic_wildcard_mention_in_followed_topic_user_ids = (
+            info.topic_wildcard_mention_in_followed_topic_user_ids
+        )
+    else:
+        topic_wildcard_mention_user_ids = set()
+        topic_wildcard_mention_in_followed_topic_user_ids = set()
     """
     Once we have the actual list of mentioned ids from message
     rendering, we can patch in "default bots" (aka normal bots)
@@ -656,7 +664,9 @@ def build_message_send_dict(
         default_bot_user_ids=info.default_bot_user_ids,
         service_bot_tuples=info.service_bot_tuples,
         all_bot_user_ids=info.all_bot_user_ids,
+        topic_wildcard_mention_user_ids=topic_wildcard_mention_user_ids,
         stream_wildcard_mention_user_ids=stream_wildcard_mention_user_ids,
+        topic_wildcard_mention_in_followed_topic_user_ids=topic_wildcard_mention_in_followed_topic_user_ids,
         stream_wildcard_mention_in_followed_topic_user_ids=stream_wildcard_mention_in_followed_topic_user_ids,
         links_for_embed=links_for_embed,
         widget_content=widget_content_dict,
@@ -680,12 +690,17 @@ def create_user_messages(
     mark_as_read_user_ids: Set[int],
     limit_unread_user_ids: Optional[Set[int]],
     scheduled_message_to_self: bool,
+    topic_wildcard_mention_user_ids: Set[int],
+    topic_wildcard_mention_in_followed_topic_user_ids: Set[int],
 ) -> List[UserMessageLite]:
     # These properties on the Message are set via
     # render_markdown by code in the Markdown inline patterns
     ids_with_alert_words = rendering_result.user_ids_with_alert_words
     sender_id = message.sender.id
     is_stream_message = message.is_stream_message()
+    all_topic_wildcard_mention_user_ids = topic_wildcard_mention_user_ids.union(
+        topic_wildcard_mention_in_followed_topic_user_ids
+    )
 
     base_flags = 0
     if rendering_result.mentions_stream_wildcard:
@@ -732,6 +747,11 @@ def create_user_messages(
             flags |= UserMessage.flags.mentioned
         if user_profile_id in ids_with_alert_words:
             flags |= UserMessage.flags.has_alert_word
+        if (
+            rendering_result.mentions_topic_wildcard
+            and user_profile_id in all_topic_wildcard_mention_user_ids
+        ):
+            flags |= UserMessage.flags.wildcard_mentioned
 
         if (
             user_profile_id in long_term_idle_user_ids
@@ -857,6 +877,8 @@ def do_send_messages(
                 mark_as_read_user_ids=mark_as_read_user_ids,
                 limit_unread_user_ids=send_request.limit_unread_user_ids,
                 scheduled_message_to_self=scheduled_message_to_self,
+                topic_wildcard_mention_user_ids=send_request.topic_wildcard_mention_user_ids,
+                topic_wildcard_mention_in_followed_topic_user_ids=send_request.topic_wildcard_mention_in_followed_topic_user_ids,
             )
 
             for um in user_messages:
@@ -953,9 +975,11 @@ def do_send_messages(
                 pm_mention_email_disabled_user_ids=send_request.pm_mention_email_disabled_user_ids,
                 stream_push_user_ids=send_request.stream_push_user_ids,
                 stream_email_user_ids=send_request.stream_email_user_ids,
+                topic_wildcard_mention_user_ids=send_request.topic_wildcard_mention_user_ids,
                 stream_wildcard_mention_user_ids=send_request.stream_wildcard_mention_user_ids,
                 followed_topic_push_user_ids=send_request.followed_topic_push_user_ids,
                 followed_topic_email_user_ids=send_request.followed_topic_email_user_ids,
+                topic_wildcard_mention_in_followed_topic_user_ids=send_request.topic_wildcard_mention_in_followed_topic_user_ids,
                 stream_wildcard_mention_in_followed_topic_user_ids=send_request.stream_wildcard_mention_in_followed_topic_user_ids,
                 muted_sender_user_ids=send_request.muted_sender_user_ids,
                 all_bot_user_ids=send_request.all_bot_user_ids,
@@ -981,9 +1005,13 @@ def do_send_messages(
             ),
             stream_push_user_ids=list(send_request.stream_push_user_ids),
             stream_email_user_ids=list(send_request.stream_email_user_ids),
+            topic_wildcard_mention_user_ids=list(send_request.topic_wildcard_mention_user_ids),
             stream_wildcard_mention_user_ids=list(send_request.stream_wildcard_mention_user_ids),
             followed_topic_push_user_ids=list(send_request.followed_topic_push_user_ids),
             followed_topic_email_user_ids=list(send_request.followed_topic_email_user_ids),
+            topic_wildcard_mention_in_followed_topic_user_ids=list(
+                send_request.topic_wildcard_mention_in_followed_topic_user_ids
+            ),
             stream_wildcard_mention_in_followed_topic_user_ids=list(
                 send_request.stream_wildcard_mention_in_followed_topic_user_ids
             ),
@@ -1522,7 +1550,7 @@ def check_message(
 
     if (
         stream is not None
-        and message_send_dict.rendering_result.mentions_stream_wildcard
+        and message_send_dict.rendering_result.has_wildcard_mention()
         and not wildcard_mention_allowed(sender, stream)
     ):
         raise JsonableError(

zerver/lib/email_notifications.py
@@ -455,14 +455,23 @@ def do_send_missedmessage_events_reply_in_zulip(
         for message in missed_messages
     )
 
-    context.update(
-        mention="mentioned" in unique_triggers
+    mention = (
+        "mentioned" in unique_triggers
+        or "topic_wildcard_mentioned" in unique_triggers
         or "stream_wildcard_mentioned" in unique_triggers
-        or "stream_wildcard_mentioned_in_followed_topic" in unique_triggers,
+        or "topic_wildcard_mentioned_in_followed_topic" in unique_triggers
+        or "stream_wildcard_mentioned_in_followed_topic" in unique_triggers
+    )
+
+    context.update(
+        mention=mention,
         personal_mentioned=personal_mentioned,
+        topic_wildcard_mentioned="topic_wildcard_mentioned" in unique_triggers,
         stream_wildcard_mentioned="stream_wildcard_mentioned" in unique_triggers,
         stream_email_notify="stream_email_notify" in unique_triggers,
         followed_topic_email_notify="followed_topic_email_notify" in unique_triggers,
+        topic_wildcard_mentioned_in_followed_topic="topic_wildcard_mentioned_in_followed_topic"
+        in unique_triggers,
         stream_wildcard_mentioned_in_followed_topic="stream_wildcard_mentioned_in_followed_topic"
         in unique_triggers,
         mentioned_user_group_name=mentioned_user_group_name,
@@ -526,10 +535,14 @@ def do_send_missedmessage_events_reply_in_zulip(
                 {
                     m["message"].sender
                     for m in missed_messages
-                    if m["trigger"] == NotificationTriggers.MENTION
-                    or m["trigger"] == NotificationTriggers.STREAM_WILDCARD_MENTION
-                    or m["trigger"]
-                    == NotificationTriggers.STREAM_WILDCARD_MENTION_IN_FOLLOWED_TOPIC
+                    if m["trigger"]
+                    in [
+                        NotificationTriggers.MENTION,
+                        NotificationTriggers.TOPIC_WILDCARD_MENTION,
+                        NotificationTriggers.STREAM_WILDCARD_MENTION,
+                        NotificationTriggers.TOPIC_WILDCARD_MENTION_IN_FOLLOWED_TOPIC,
+                        NotificationTriggers.STREAM_WILDCARD_MENTION_IN_FOLLOWED_TOPIC,
+                    ]
                 }
             )
         message = missed_messages[0]["message"]

zerver/lib/markdown/__init__.py
@@ -128,6 +128,9 @@ class MessageRenderingResult:
     user_ids_with_alert_words: Set[int]
     potential_attachment_path_ids: List[str]
 
+    def has_wildcard_mention(self) -> bool:
+        return self.mentions_stream_wildcard or self.mentions_topic_wildcard
+
 
 @dataclass
 class DbData:

commit b2eb26eb4908640bfea4845a22186101c94c7dbc

message_edit: Fix wildcard_mentioned flag set for all user-messages.
Earlier, for topic wildcard mentions, the 'wildcard_mentioned'
flag was set for all the user-messages. (similar to stream wildcard
mention).

The flag should be set for the topic participants only.

The bug was introduced in 9c4177d.

zerver/actions/message_edit.py
@@ -1,7 +1,7 @@
 import datetime
 import itertools
 from collections import defaultdict
-from typing import Any, Dict, Iterable, List, Optional, Set, Tuple
+from typing import AbstractSet, Any, Dict, Iterable, List, Optional, Set, Tuple
 
 from django.conf import settings
 from django.db import transaction
@@ -256,9 +256,10 @@ def get_mentions_for_message_updates(message_id: int) -> Set[int]:
 
 
 def update_user_message_flags(
-    rendering_result: MessageRenderingResult, ums: Iterable[UserMessage]
+    rendering_result: MessageRenderingResult,
+    ums: Iterable[UserMessage],
+    topic_participant_user_ids: AbstractSet[int] = set(),
 ) -> None:
-    wildcard_mentioned = rendering_result.has_wildcard_mention()
     mentioned_ids = rendering_result.mentions_user_ids
     ids_with_alert_words = rendering_result.user_ids_with_alert_words
     changed_ums: Set[UserMessage] = set()
@@ -280,7 +281,11 @@ def update_user_message_flags(
         mentioned = um.user_profile_id in mentioned_ids
         update_flag(um, mentioned, UserMessage.flags.mentioned)
 
-        update_flag(um, wildcard_mentioned, UserMessage.flags.wildcard_mentioned)
+        if rendering_result.mentions_stream_wildcard:
+            update_flag(um, True, UserMessage.flags.wildcard_mentioned)
+        elif rendering_result.mentions_topic_wildcard:
+            topic_wildcard_mentioned = um.user_profile_id in topic_participant_user_ids
+            update_flag(um, topic_wildcard_mentioned, UserMessage.flags.wildcard_mentioned)
 
     for um in changed_ums:
         um.save(update_fields=["flags"])
@@ -416,8 +421,6 @@ def do_update_message(
             members = mention_data.get_group_members(group_id)
             rendering_result.mentions_user_ids.update(members)
 
-        update_user_message_flags(rendering_result, ums)
-
         # One could imagine checking realm.allow_edit_history here and
         # modifying the events based on that setting, but doing so
         # doesn't really make sense.  We need to send the edit event
@@ -496,9 +499,13 @@ def do_update_message(
             event["topic_wildcard_mention_in_followed_topic_user_ids"] = list(
                 info.topic_wildcard_mention_in_followed_topic_user_ids
             )
+            topic_participant_user_ids = info.topic_participant_user_ids
         else:
             event["topic_wildcard_mention_user_ids"] = []
             event["topic_wildcard_mention_in_followed_topic_user_ids"] = []
+            topic_participant_user_ids = set()
+
+        update_user_message_flags(rendering_result, ums, topic_participant_user_ids)
 
         do_update_mobile_push_notification(
             target_message,

zerver/tests/test_message_edit.py
@@ -2060,13 +2060,19 @@ class EditMessageTest(EditMessageTestCase):
         )
         message_id = self.send_stream_message(hamlet, stream_name, "Hello everyone")
 
-        def notify(user_id: int) -> Dict[str, Any]:
-            return {
-                "id": user_id,
-                "flags": ["wildcard_mentioned"],
-            }
-
-        users_to_be_notified = sorted(map(notify, [cordelia.id, hamlet.id]), key=itemgetter("id"))
+        users_to_be_notified = sorted(
+            [
+                {
+                    "id": hamlet.id,
+                    "flags": ["wildcard_mentioned"],
+                },
+                {
+                    "id": cordelia.id,
+                    "flags": [],
+                },
+            ],
+            key=itemgetter("id"),
+        )
         result = self.client_patch(
             f"/json/messages/{message_id}",
             {
@@ -2155,13 +2161,19 @@ class EditMessageTest(EditMessageTestCase):
         self.login_user(hamlet)
         message_id = self.send_stream_message(hamlet, stream_name, "Hello everyone")
 
-        def notify(user_id: int) -> Dict[str, Any]:
-            return {
-                "id": user_id,
-                "flags": ["wildcard_mentioned"],
-            }
-
-        users_to_be_notified = sorted(map(notify, [cordelia.id, hamlet.id]), key=itemgetter("id"))
+        users_to_be_notified = sorted(
+            [
+                {
+                    "id": hamlet.id,
+                    "flags": ["wildcard_mentioned"],
+                },
+                {
+                    "id": cordelia.id,
+                    "flags": [],
+                },
+            ],
+            key=itemgetter("id"),
+        )
         result = self.client_patch(
             f"/json/messages/{message_id}",
             {