afedotov89/flexar-zulip

commit 81f215a95503216cfb4708f8b9cc06f1f837acb4

users: Use browser locale to set the default language of new user.
This commit reads the browser locale during user registration, and
sets it as default language of user if it is supported by Zulip.
Otherwise, it is set to realm's default language.

zerver/actions/create_user.py
@@ -344,6 +344,7 @@ def do_create_user(
     tos_version: Optional[str] = None,
     timezone: str = "",
     avatar_source: str = UserProfile.AVATAR_FROM_GRAVATAR,
+    default_language: str = "en",
     default_sending_stream: Optional[Stream] = None,
     default_events_register_stream: Optional[Stream] = None,
     default_all_public_streams: Optional[bool] = None,
@@ -367,6 +368,7 @@ def do_create_user(
             tos_version=tos_version,
             timezone=timezone,
             avatar_source=avatar_source,
+            default_language=default_language,
             default_sending_stream=default_sending_stream,
             default_events_register_stream=default_events_register_stream,
             default_all_public_streams=default_all_public_streams,

zerver/lib/create_user.py
@@ -84,6 +84,7 @@ def create_user_profile(
     is_mirror_dummy: bool,
     tos_version: Optional[str],
     timezone: Optional[str],
+    default_language: str = "en",
     tutorial_status: str = UserProfile.TUTORIAL_WAITING,
     enter_sends: bool = False,
     force_id: Optional[int] = None,
@@ -116,7 +117,7 @@ def create_user_profile(
         tutorial_status=tutorial_status,
         enter_sends=enter_sends,
         onboarding_steps=orjson.dumps([]).decode(),
-        default_language=realm.default_language,
+        default_language=default_language,
         delivery_email=email,
         **extra_kwargs,
     )
@@ -143,6 +144,7 @@ def create_user(
     timezone: str = "",
     avatar_source: str = UserProfile.AVATAR_FROM_GRAVATAR,
     is_mirror_dummy: bool = False,
+    default_language: str = "en",
     default_sending_stream: Optional[Stream] = None,
     default_events_register_stream: Optional[Stream] = None,
     default_all_public_streams: Optional[bool] = None,
@@ -162,6 +164,7 @@ def create_user(
         is_mirror_dummy,
         tos_version,
         timezone,
+        default_language,
         force_id=force_id,
         force_date_joined=force_date_joined,
     )

zerver/tests/test_signup.py
@@ -3933,8 +3933,7 @@ class UserSignUpTest(InviteUserBase):
 
     def test_user_default_language_and_timezone(self) -> None:
         """
-        Check if the default language of new user is the default language
-        of the realm.
+        Check if the default language of new user is set using the browser locale
         """
         email = self.nonreg_email("newguy")
         password = "newpassword"
@@ -3954,12 +3953,42 @@ class UserSignUpTest(InviteUserBase):
         self.assertEqual(result.status_code, 200)
 
         # Pick a password and agree to the ToS.
-        result = self.submit_reg_form_for_user(email, password, timezone=timezone)
+        result = self.submit_reg_form_for_user(
+            email, password, timezone=timezone, HTTP_ACCEPT_LANGUAGE="fr,en;q=0.9"
+        )
+        self.assertEqual(result.status_code, 302)
+
+        user_profile = self.nonreg_user("newguy")
+        self.assertNotEqual(user_profile.default_language, realm.default_language)
+        self.assertEqual(user_profile.default_language, "fr")
+        self.assertEqual(user_profile.timezone, timezone)
+        from django.core.mail import outbox
+
+        outbox.pop()
+
+    def test_default_language_with_unsupported_browser_locale(self) -> None:
+        email = self.nonreg_email("newguy")
+        password = "newpassword"
+        realm = get_realm("zulip")
+        do_set_realm_property(realm, "default_language", "de", acting_user=None)
+
+        result = self.client_post("/accounts/home/", {"email": email})
+        self.assertEqual(result.status_code, 302)
+        self.assertTrue(result["Location"].endswith(f"/accounts/send_confirm/{email}"))
+        result = self.client_get(result["Location"])
+        self.assert_in_response("Check your email so we can get started.", result)
+
+        # Visit the confirmation link.
+        confirmation_url = self.get_confirmation_url_from_outbox(email)
+        result = self.client_get(confirmation_url)
+        self.assertEqual(result.status_code, 200)
+
+        # Pick a password and agree to the ToS.
+        result = self.submit_reg_form_for_user(email, password, HTTP_ACCEPT_LANGUAGE="en-IND")
         self.assertEqual(result.status_code, 302)
 
         user_profile = self.nonreg_user("newguy")
         self.assertEqual(user_profile.default_language, realm.default_language)
-        self.assertEqual(user_profile.timezone, timezone)
         from django.core.mail import outbox
 
         outbox.pop()

zerver/views/registration.py
@@ -44,6 +44,7 @@ from zerver.forms import (
 )
 from zerver.lib.email_validation import email_allowed_for_realm, validate_email_not_already_in_realm
 from zerver.lib.exceptions import RateLimited
+from zerver.lib.i18n import get_default_language_for_new_user
 from zerver.lib.pysa import mark_sanitized
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.send_email import EmailNotDeliveredException, FromAddress, send_email
@@ -432,6 +433,12 @@ def accounts_register(
             do_change_password(user_profile, password)
             do_change_full_name(user_profile, full_name, user_profile)
             do_change_user_setting(user_profile, "timezone", timezone, acting_user=user_profile)
+            do_change_user_setting(
+                user_profile,
+                "default_language",
+                get_default_language_for_new_user(request, realm),
+                acting_user=None,
+            )
             # TODO: When we clean up the `do_activate_mirror_dummy_user` code path,
             # make it respect invited_as_admin / is_realm_admin.
 
@@ -445,6 +452,7 @@ def accounts_register(
                 role=role,
                 tos_version=settings.TERMS_OF_SERVICE_VERSION,
                 timezone=timezone,
+                default_language=get_default_language_for_new_user(request, realm),
                 default_stream_groups=default_stream_groups,
                 source_profile=source_profile,
                 realm_creation=realm_creation,

commit 811fd60976cdc5365ceca93b41c039708e726b10

i18n: Fix default language for users created via API/LDAP.
This fixes a regression introduced in
81f215a95503216cfb4708f8b9cc06f1f837acb4, where the realm's default
language would be ignored for users created via API/LDAP/SAML,
resulting in all such users having English as their default language.

The API/LDAP/SAML account creation code paths don't have a request,
and thus cannot pull default language from the user's browser.

We have the `realm.default_language` field intended for this use case,
but it was not being passed through the system.

Rather than pass `realm.default_language` through from each caller, we
make the low-level user creation code set this field, as that seems
more robust to the creation of future callers.

zerver/actions/create_user.py
@@ -406,7 +406,7 @@ def do_create_user(
     tos_version: Optional[str] = None,
     timezone: str = "",
     avatar_source: str = UserProfile.AVATAR_FROM_GRAVATAR,
-    default_language: str = "en",
+    default_language: Optional[str] = None,
     default_sending_stream: Optional[Stream] = None,
     default_events_register_stream: Optional[Stream] = None,
     default_all_public_streams: Optional[bool] = None,

zerver/lib/bulk_create.py
@@ -58,6 +58,7 @@ def bulk_create_users(
             False,
             tos_version,
             timezone,
+            default_language=realm.default_language,
             tutorial_status=UserProfile.TUTORIAL_FINISHED,
             email_address_visibility=email_address_visibility,
         )

zerver/lib/create_user.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import UserManager
 from django.utils.timezone import now as timezone_now
 
 from zerver.lib.hotspots import copy_hotspots
+from zerver.lib.i18n import get_default_language_for_new_user
 from zerver.lib.timezone import canonicalize_timezone
 from zerver.lib.upload import copy_avatar
 from zerver.models import (
@@ -92,7 +93,7 @@ def create_user_profile(
     is_mirror_dummy: bool,
     tos_version: Optional[str],
     timezone: str,
-    default_language: str = "en",
+    default_language: str,
     tutorial_status: str = UserProfile.TUTORIAL_WAITING,
     force_id: Optional[int] = None,
     force_date_joined: Optional[datetime] = None,
@@ -152,7 +153,7 @@ def create_user(
     timezone: str = "",
     avatar_source: str = UserProfile.AVATAR_FROM_GRAVATAR,
     is_mirror_dummy: bool = False,
-    default_language: str = "en",
+    default_language: Optional[str] = None,
     default_sending_stream: Optional[Stream] = None,
     default_events_register_stream: Optional[Stream] = None,
     default_all_public_streams: Optional[bool] = None,
@@ -173,6 +174,12 @@ def create_user(
         # There is no privacy motivation for limiting access to bot email addresses,
         # so we hardcode them to EMAIL_ADDRESS_VISIBILITY_EVERYONE.
         user_email_address_visibility = UserProfile.EMAIL_ADDRESS_VISIBILITY_EVERYONE
+
+    # Users created via the API or LDAP/SAML syncing code paths will
+    # usually not have a default_language value, and should fall back
+    # to the realm default.
+    if default_language is None:
+        default_language = get_default_language_for_new_user(realm, request=None)
 
     user_profile = create_user_profile(
         realm,

zerver/lib/i18n.py
@@ -85,7 +85,13 @@ def get_browser_language_code(request: HttpRequest) -> Optional[str]:
     return None
 
 
-def get_default_language_for_new_user(realm: Realm, *, request: HttpRequest) -> str:
+def get_default_language_for_new_user(realm: Realm, *, request: Optional[HttpRequest]) -> str:
+    if request is None:
+        # Users created via the API or LDAP will not have a
+        # browser/request associated with them, and should just use
+        # the realm's default language.
+        return realm.default_language
+
     browser_language_code = get_browser_language_code(request)
     if browser_language_code is not None:
         return browser_language_code

zerver/tests/test_auth_backends.py
@@ -6312,7 +6312,9 @@ class TestLDAP(ZulipLDAPTestCase):
         }
     )
     def test_login_success_when_user_does_not_exist_with_valid_subdomain(self) -> None:
-        RealmDomain.objects.create(realm=self.backend._realm, domain="acme.com")
+        realm = self.backend._realm
+        do_set_realm_property(realm, "default_language", "ja", acting_user=None)
+        RealmDomain.objects.create(realm=realm, domain="acme.com")
         with self.settings(LDAP_APPEND_DOMAIN="acme.com"):
             user_profile = self.backend.authenticate(
                 request=mock.MagicMock(),
@@ -6324,6 +6326,8 @@ class TestLDAP(ZulipLDAPTestCase):
             self.assertEqual(user_profile.delivery_email, "newuser@acme.com")
             self.assertEqual(user_profile.full_name, "New LDAP fullname")
             self.assertEqual(user_profile.realm.string_id, "zulip")
+            # Verify the user got the realm's default language.
+            self.assertEqual(user_profile.default_language, "ja")
 
             # Verify avatar gets created
             self.assertEqual(user_profile.avatar_source, UserProfile.AVATAR_FROM_USER)

zerver/tests/test_mirror_users.py
@@ -173,6 +173,7 @@ class MirroredMessageUsersTest(ZulipTestCase):
             kwargs["bot_owner"] = None
             kwargs["tos_version"] = None
             kwargs["timezone"] = timezone_now()
+            kwargs["default_language"] = "en"
             kwargs["email_address_visibility"] = UserProfile.EMAIL_ADDRESS_VISIBILITY_EVERYONE
             create_user_profile(**kwargs).save()
             raise IntegrityError

zerver/tests/test_signup.py
@@ -3996,6 +3996,9 @@ class UserSignUpTest(ZulipTestCase):
         req.META["HTTP_ACCEPT_LANGUAGE"] = ""
         self.assertEqual(get_default_language_for_new_user(realm, request=req), "hi")
 
+        # Test code path for users created via the API or LDAP
+        self.assertEqual(get_default_language_for_new_user(realm, request=None), "hi")
+
 
 class DeactivateUserTest(ZulipTestCase):
     def test_deactivate_user(self) -> None:

zerver/tests/test_users.py
@@ -938,6 +938,7 @@ class AdminCreateUserTest(ZulipTestCase):
         realm = admin.realm
         self.login_user(admin)
         do_change_user_role(admin, UserProfile.ROLE_REALM_ADMINISTRATOR, acting_user=None)
+        do_set_realm_property(realm, "default_language", "ja", acting_user=None)
         valid_params = dict(
             email="romeo@zulip.net",
             password="xxxx",
@@ -1026,6 +1027,8 @@ class AdminCreateUserTest(ZulipTestCase):
         self.assertEqual(new_user.full_name, "Romeo Montague")
         self.assertEqual(new_user.id, result["user_id"])
         self.assertEqual(new_user.tos_version, UserProfile.TOS_VERSION_BEFORE_FIRST_LOGIN)
+        # Make sure the new user got the realm's default language
+        self.assertEqual(new_user.default_language, "ja")
 
         # Make sure the recipient field is set correctly.
         self.assertEqual(