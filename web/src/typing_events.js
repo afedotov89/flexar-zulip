zulip/zulip

typing_notifications: Frontend work to display notifs for streams.
Display/hide "X is typing" notification
on receiving start/stop typing event for streams.

static/js/typing_events.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 
 import render_typing_notifications from "../templates/typing_notifications.hbs";
 
+import * as blueslip from "./blueslip";
 import * as narrow_state from "./narrow_state";
 import {page_params} from "./page_params";
 import * as people from "./people";
@@ -27,8 +28,12 @@ const MAX_USERS_TO_DISPLAY_NAME = 3;
 // that make typing indicators work.
 
 function get_users_typing_for_narrow() {
+    if (narrow_state.narrowed_to_topic()) {
+        return typing_data.get_stream_typists(narrow_state.stream_id(), narrow_state.topic());
+    }
+
     if (!narrow_state.narrowed_to_pms()) {
-        // Narrow is neither pm-with nor is: private
+        // Narrow is neither pm-with nor is: private nor topic
         return [];
     }
 
@@ -69,14 +74,31 @@ export function render_notifications_for_narrow() {
     }
 }
 
+function get_key(event) {
+    let key;
+    let recipients;
+    switch (event.message_type) {
+        case "stream":
+            key = typing_data.get_topic_key(event.stream_id, event.topic);
+            break;
+        case "private":
+            recipients = event.recipients.map((user) => user.user_id);
+            recipients.sort();
+
+            key = typing_data.get_pms_key(recipients);
+            break;
+        default:
+            blueslip.error("Unexpected message_type in typing event: " + event.message_type);
+            break;
+    }
+    return key;
+}
+
 export function hide_notification(event) {
-    const recipients = event.recipients.map((user) => user.user_id);
-    recipients.sort();
+    const key = get_key(event);
+    typing_data.clear_inbound_timer(key);
 
-    const pms_typing_key = typing_data.get_pms_key(recipients);
-    typing_data.clear_inbound_timer(pms_typing_key);
-
-    const removed = typing_data.remove_typist(pms_typing_key, event.sender.user_id);
+    const removed = typing_data.remove_typist(key, event.sender.user_id, event.message_type);
 
     if (removed) {
         render_notifications_for_narrow();
@@ -84,18 +106,15 @@ export function hide_notification(event) {
 }
 
 export function display_notification(event) {
-    const recipients = event.recipients.map((user) => user.user_id);
-    recipients.sort();
-
     const sender_id = event.sender.user_id;
     event.sender.name = people.get_by_user_id(sender_id).full_name;
 
-    const pms_typing_key = typing_data.get_pms_key(recipients);
-    typing_data.add_typist(pms_typing_key, sender_id);
+    const key = get_key(event);
+    typing_data.add_typist(key, sender_id, event.message_type);
 
     render_notifications_for_narrow();
 
-    typing_data.kickstart_inbound_timer(pms_typing_key, TYPING_STARTED_EXPIRY_PERIOD, () => {
+    typing_data.kickstart_inbound_timer(key, TYPING_STARTED_EXPIRY_PERIOD, () => {
         hide_notification(event);
     });
 }

afedotov89/flexar-zulip

Co-authored-by: Prakhar Pratyush <prakhar841301@gmail.com>

web/src/typing_events.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 
 import render_typing_notifications from "../templates/typing_notifications.hbs";
 
+import * as blueslip from "./blueslip";
 import * as narrow_state from "./narrow_state";
 import {page_params} from "./page_params";
 import * as people from "./people";
@@ -27,8 +28,12 @@ const MAX_USERS_TO_DISPLAY_NAME = 3;
 // that make typing indicators work.
 
 function get_users_typing_for_narrow() {
+    if (narrow_state.narrowed_to_topic()) {
+        return typing_data.get_topic_typists(narrow_state.stream_id(), narrow_state.topic());
+    }
+
     if (!narrow_state.narrowed_to_pms()) {
-        // Narrow is neither "dm:" nor "is:dm".
+        // Narrow is neither "dm:" nor "is:dm" nor topic.
         return [];
     }
 
@@ -69,14 +74,31 @@ export function render_notifications_for_narrow() {
     }
 }
 
+function get_key(event) {
+    let key;
+    let recipients;
+    switch (event.message_type) {
+        case "stream":
+            key = typing_data.get_topic_key(event.stream_id, event.topic);
+            break;
+        case "direct":
+            recipients = event.recipients.map((user) => user.user_id);
+            recipients.sort();
+
+            key = typing_data.get_direct_message_conversation_key(recipients);
+            break;
+        default:
+            blueslip.error("Unexpected message_type in typing event: " + event.message_type);
+            break;
+    }
+    return key;
+}
+
 export function hide_notification(event) {
-    const recipients = event.recipients.map((user) => user.user_id);
-    recipients.sort();
+    const key = get_key(event);
+    typing_data.clear_inbound_timer(key);
 
-    const conversation_key = typing_data.get_direct_message_conversation_key(recipients);
-    typing_data.clear_inbound_timer(conversation_key);
-
-    const removed = typing_data.remove_typist(conversation_key, event.sender.user_id);
+    const removed = typing_data.remove_typist(key, event.sender.user_id);
 
     if (removed) {
         render_notifications_for_narrow();
@@ -84,18 +106,15 @@ export function hide_notification(event) {
 }
 
 export function display_notification(event) {
-    const recipients = event.recipients.map((user) => user.user_id);
-    recipients.sort();
-
     const sender_id = event.sender.user_id;
     event.sender.name = people.get_by_user_id(sender_id).full_name;
 
-    const conversation_key = typing_data.get_direct_message_conversation_key(recipients);
-    typing_data.add_typist(conversation_key, sender_id);
+    const key = get_key(event);
+    typing_data.add_typist(key, sender_id);
 
     render_notifications_for_narrow();
 
-    typing_data.kickstart_inbound_timer(conversation_key, typing_started_expiry_period, () => {
+    typing_data.kickstart_inbound_timer(key, typing_started_expiry_period, () => {
         hide_notification(event);
     });
 }