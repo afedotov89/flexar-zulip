scantist-ossops-m2/zulip

left_sidebar: Rename All messages to Global Feed.
Fixes part of #27802.

web/e2e-tests/message-basics.test.ts
@@ -110,7 +110,7 @@ async function un_narrow(page: Page): Promise<void> {
     await page.waitForSelector(".message-list .message_row", {visible: true});
     // Assert that there is only one message list.
     assert.equal((await page.$$(".message-list")).length, 1);
-    assert.strictEqual(await page.title(), "All messages - Zulip Dev - Zulip");
+    assert.strictEqual(await page.title(), "Global feed - Zulip Dev - Zulip");
 }
 
 async function un_narrow_by_clicking_org_icon(page: Page): Promise<void> {

web/src/message_view_header.ts
@@ -49,7 +49,7 @@ function get_message_view_header_context(filter: Filter | undefined): MessageVie
     }
     if (filter === undefined) {
         return {
-            title: $t({defaultMessage: "All messages"}),
+            title: $t({defaultMessage: "Global feed"}),
             zulip_icon: "all-messages",
         };
     }

web/src/settings_config.ts
@@ -99,7 +99,7 @@ export const web_home_view_values = {
     },
     all_messages: {
         code: "all_messages",
-        description: $t({defaultMessage: "All messages"}),
+        description: $t({defaultMessage: "Global feed"}),
     },
 };
 

web/tests/narrow.test.js
@@ -789,7 +789,7 @@ run_test("narrow_compute_title", () => {
 
     inbox_util.set_visible(false);
     filter = new Filter([{operator: "in", operand: "home"}]);
-    assert.equal(narrow_title.compute_narrow_title(filter), "translated: All messages");
+    assert.equal(narrow_title.compute_narrow_title(filter), "translated: Global feed");
 
     // Search & uncommon narrows
     filter = new Filter([{operator: "search", operand: "potato"}]);

afedotov89/flexar-zulip

web/e2e-tests/message-basics.test.ts
@@ -110,7 +110,7 @@ async function un_narrow(page: Page): Promise<void> {
     await page.waitForSelector(".message-list .message_row", {visible: true});
     // Assert that there is only one message list.
     assert.equal((await page.$$(".message-list")).length, 1);
-    assert.strictEqual(await page.title(), "All messages - Zulip Dev - Zulip");
+    assert.strictEqual(await page.title(), "Combined feed - Zulip Dev - Zulip");
 }
 
 async function un_narrow_by_clicking_org_icon(page: Page): Promise<void> {

web/src/message_view_header.ts
@@ -49,7 +49,7 @@ function get_message_view_header_context(filter: Filter | undefined): MessageVie
     }
     if (filter === undefined) {
         return {
-            title: $t({defaultMessage: "All messages"}),
+            title: $t({defaultMessage: "Combined feed"}),
             zulip_icon: "all-messages",
         };
     }

web/src/settings_config.ts
@@ -99,7 +99,7 @@ export const web_home_view_values = {
     },
     all_messages: {
         code: "all_messages",
-        description: $t({defaultMessage: "All messages"}),
+        description: $t({defaultMessage: "Combined feed"}),
     },
 };
 

web/tests/narrow.test.js
@@ -789,7 +789,7 @@ run_test("narrow_compute_title", () => {
 
     inbox_util.set_visible(false);
     filter = new Filter([{operator: "in", operand: "home"}]);
-    assert.equal(narrow_title.compute_narrow_title(filter), "translated: All messages");
+    assert.equal(narrow_title.compute_narrow_title(filter), "translated: Combined feed");
 
     // Search & uncommon narrows
     filter = new Filter([{operator: "search", operand: "potato"}]);