afedotov89/flexar-zulip

commit 9a001f64c53bd2a08fcb891c9bd3b5a6e6ee7492

invites: Do not show streams if user cannot subscribe others.
We do not show the streams list in invite modal if the user
does not have permission to subscribe others to stream and
show a notice mentioning it.

web/src/invite.ts
@@ -340,6 +340,7 @@ function open_invite_user_modal(e: JQuery.ClickEvent<Document, undefined>): void
         new_stream_announcements_stream: stream_data.get_new_stream_announcements_stream(),
         show_select_default_streams_option: stream_data.get_default_stream_ids().length !== 0,
         user_has_email_set: !settings_data.user_email_not_configured(),
+        can_subscribe_other_users: settings_data.user_can_subscribe_other_users(),
     });
 
     function invite_user_modal_post_render(): void {

web/styles/app_components.css
@@ -529,7 +529,8 @@ div.overlay {
 
 .upgrade-tip,
 .upgrade-or-sponsorship-tip,
-.tip {
+.tip,
+.invite-stream-notice {
     position: relative;
     display: block;
     background-color: hsl(46deg 63% 95%);

web/styles/dark_theme.css
@@ -825,7 +825,8 @@
 
     .upgrade-tip,
     .upgrade-or-sponsorship-tip,
-    .tip {
+    .tip,
+    .invite-stream-notice {
         color: inherit;
         background-color: hsl(46deg 28% 38% / 27%);
         border: 1px solid hsl(49deg 38% 46%);

web/templates/invite_user_modal.hbs
@@ -3,14 +3,20 @@
     {{#if development_environment}}
     <div class="alert" id="dev_env_msg"></div>
     {{/if}}
-    {{#unless user_has_email_set }}
+    {{#if (not user_has_email_set) }}
     <div class="tip">
         {{#tr}}
             You must <z-link>configure your email</z-link> to access this feature.
             {{#*inline "z-link"}}<a href="/help/demo-organizations#configure-email-for-demo-organization-owner" target="_blank" rel="noopener noreferrer">{{> @partial-block }}</a>{{/inline}}
         {{/tr}}
     </div>
-    {{/unless}}
+    {{else if (not can_subscribe_other_users)}}
+    <div class="invite-stream-notice">
+        {{#tr}}The users you invite will be automatically added to <z-link>default streams</z-link> for this organization, as you do not have permission to configure which streams new users join.
+        {{#*inline "z-link"}}<a target="_blank" rel="noopener noreferrer" href="#organization/default-streams-list">{{> @partial-block}}</a>{{/inline}}
+        {{/tr}}
+    </div>
+    {{/if}}
     <div class="input-group">
         <div id="invite_users_option_tabs_container" class="new-style"></div>
         <div id="invitee_emails_container">
@@ -55,6 +61,7 @@
             {{/if}}
         </select>
     </div>
+    {{#if can_subscribe_other_users}}
     <div>
         <label>{{t "Channels they should join" }}</label>
         <div id="streams_to_add">
@@ -90,4 +97,5 @@
             </div>
         </div>
     </div>
+    {{/if}}
 </form>

commit c9126c98d6879ec8ee42b58c3b010e7e3e4979ad

invite: Fix invite modal bug for users who can't subscribe others.
The bug was due to the element being accessed when not present
in the DOM. This function fixes it by calling the function to
setup streams UI only when the concerned elements are present
in DOM, i.e. when user can subscribe others.

The bug was introduced in 9a001f64c53.

web/src/invite.ts
@@ -364,7 +364,10 @@ function open_invite_user_modal(e: JQuery.ClickEvent<Document, undefined>): void
 
         set_custom_time_inputs_visibility();
         set_expires_on_text();
-        set_streams_to_join_list_visibility();
+
+        if (settings_data.user_can_subscribe_other_users()) {
+            set_streams_to_join_list_visibility();
+        }
 
         $("#invite-user-modal").on("click", ".setup-tips-container .banner_content a", () => {
             dialog_widget.close();