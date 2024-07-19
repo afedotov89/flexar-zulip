afedotov89/flexar-zulip

commit 8c4a59e0c1c249065379ec0dabedf02de95c3337

stream_settings: Update save discard buttons when live-updating.
This commit adds code to hide save discard buttons and
discard the changes if another user changed a setting
in the same subsection.

web/src/stream_ui_updates.js
@@ -399,7 +399,12 @@ export function update_setting_element(sub, setting_name) {
     }
 
     const $elem = $(`#id_${CSS.escape(setting_name)}`);
-    settings_org.discard_stream_property_element_changes($elem, sub);
+    const $subsection = $elem.closest(".settings-subsection-parent");
+    if ($subsection.find(".save-button-controls").hasClass("hide")) {
+        settings_org.discard_stream_property_element_changes($elem);
+    } else {
+        settings_org.discard_stream_settings_subsection_changes($subsection, sub);
+    }
 }
 
 export function enable_or_disable_add_subscribers_elements(

commit ce1abffa4d3aab9a832b9d574fa47637300714b1

stream_ui_updates: Pass missing parameter.
This bug was introduced in 8c4a59e0c1c249.

web/src/stream_ui_updates.js
@@ -408,7 +408,7 @@ export function update_setting_element(sub, setting_name) {
     const $elem = $(`#id_${CSS.escape(setting_name)}`);
     const $subsection = $elem.closest(".settings-subsection-parent");
     if ($subsection.find(".save-button-controls").hasClass("hide")) {
-        settings_org.discard_stream_property_element_changes($elem);
+        settings_org.discard_stream_property_element_changes($elem, sub);
     } else {
         settings_org.discard_stream_settings_subsection_changes($subsection, sub);
     }