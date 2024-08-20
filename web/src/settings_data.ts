zulip/zulip

user_groups: Update code to check whether user can edit a user group.
Earlier there was only a realm level setting for configuring
who can edit user groups. A new group level setting is also added
for configuring who can manage that particular group.
Now, a user group can be edited by a user if it is allowed from
realm level setting or group level setting.

This commit make changes in frontend to also use group level setting
in determining whether a group can be edited by user or not and disables
changing the group settings when group cannot be edited by user.

web/src/settings_data.ts
@@ -161,27 +161,32 @@ export function user_can_move_messages_between_streams(): boolean {
     return user_has_permission(page_params.realm_move_messages_between_streams_policy);
 }
 
-export function user_can_edit_user_groups(): boolean {
+export function user_can_edit_user_groups_from_realm_level_permission(): boolean {
     return user_has_permission(page_params.realm_user_group_edit_policy);
 }
 
 export function can_edit_user_group(group_id: number): boolean {
-    if (!page_params.user_id) {
-        return false;
-    }
-
-    if (!user_can_edit_user_groups()) {
-        return false;
-    }
+    let can_edit_from_realm_level_permission: boolean =
+        user_can_edit_user_groups_from_realm_level_permission();
 
     // Admins and moderators are allowed to edit user groups even if they
     // are not a member of that user group. Members can edit user groups
     // only if they belong to that group.
-    if (page_params.is_admin || page_params.is_moderator) {
-        return true;
+    if (
+        !page_params.is_admin &&
+        !page_params.is_moderator &&
+        !user_groups.is_direct_member_of(page_params.user_id, group_id)
+    ) {
+        can_edit_from_realm_level_permission = false;
     }
 
-    return user_groups.is_direct_member_of(page_params.user_id, group_id);
+    const user_group = user_groups.get_user_group_from_id(group_id);
+    const can_edit_from_group_level_permission = user_groups.is_user_in_group(
+        user_group.can_manage_group,
+        page_params.user_id,
+    );
+
+    return can_edit_from_group_level_permission || can_edit_from_realm_level_permission;
 }
 
 export function user_can_create_user_groups(): boolean {

afedotov89/flexar-zulip

web/src/settings_data.ts
@@ -162,7 +162,7 @@ export function user_can_move_messages_between_streams(): boolean {
     return user_has_permission(realm.realm_move_messages_between_streams_policy);
 }
 
-export function user_can_edit_user_groups(): boolean {
+export function user_can_edit_all_user_groups(): boolean {
     return user_has_permission(realm.realm_user_group_edit_policy);
 }
 
@@ -171,18 +171,22 @@ export function can_edit_user_group(group_id: number): boolean {
         return false;
     }
 
-    if (!user_can_edit_user_groups()) {
-        return false;
+    let can_edit_all_user_groups = user_can_edit_all_user_groups();
+
+    if (
+        !current_user.is_admin &&
+        !current_user.is_moderator &&
+        !user_groups.is_direct_member_of(current_user.user_id, group_id)
+    ) {
+        can_edit_all_user_groups = false;
     }
 
-    // Admins and moderators are allowed to edit user groups even if they
-    // are not a member of that user group. Members can edit user groups
-    // only if they belong to that group.
-    if (current_user.is_admin || current_user.is_moderator) {
+    if (can_edit_all_user_groups) {
         return true;
     }
 
-    return user_groups.is_direct_member_of(current_user.user_id, group_id);
+    const group = user_groups.get_user_group_from_id(group_id);
+    return user_groups.is_user_in_group(group.can_manage_group, current_user.user_id);
 }
 
 export function user_can_create_user_groups(): boolean {