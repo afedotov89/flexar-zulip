zulip/zulip

user_groups: Add function for determining group creation permissions.
Earlier in frontend there was a single function to determine whether
user can create and edit user groups.
This commit adds a separate function for determining group creation
permissions.

web/src/settings_data.ts
@@ -184,6 +184,10 @@ export function can_edit_user_group(group_id: number): boolean {
     return user_groups.is_direct_member_of(page_params.user_id, group_id);
 }
 
+export function user_can_create_user_groups(): boolean {
+    return user_has_permission(page_params.realm_user_group_edit_policy);
+}
+
 export function user_can_add_custom_emoji(): boolean {
     return user_has_permission(page_params.realm_add_custom_emoji_policy);
 }

afedotov89/flexar-zulip

web/src/settings_data.ts
@@ -185,6 +185,10 @@ export function can_edit_user_group(group_id: number): boolean {
     return user_groups.is_direct_member_of(current_user.user_id, group_id);
 }
 
+export function user_can_create_user_groups(): boolean {
+    return user_has_permission(realm.realm_user_group_edit_policy);
+}
+
 export function user_can_add_custom_emoji(): boolean {
     return user_has_permission(realm.realm_add_custom_emoji_policy);
 }