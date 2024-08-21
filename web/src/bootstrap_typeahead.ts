afedotov89/flexar-zulip

commit 36eec51e947b3d9d6f547fe36583d32b0f7595a8

web: Use util.the for accessing element of single-item lists.

web/src/bootstrap_typeahead.ts
@@ -171,7 +171,7 @@ import getCaretCoordinates from "textarea-caret";
 import * as tippy from "tippy.js";
 
 import * as scroll_util from "./scroll_util";
-import {get_string_diff} from "./util";
+import {get_string_diff, the} from "./util";
 
 function get_pseudo_keycode(
     event: JQuery.KeyDownEvent | JQuery.KeyUpEvent | JQuery.KeyPressEvent,
@@ -322,7 +322,7 @@ export class Typeahead<ItemType extends string | object> {
     }
 
     select(e?: JQuery.ClickEvent | JQuery.KeyUpEvent | JQuery.KeyDownEvent): this {
-        const val = this.values.get(this.$menu.find(".active")[0]!);
+        const val = this.values.get(the(this.$menu.find(".active")));
         // It's possible that we got here from pressing enter with nothing highlighted.
         if (!this.requireHighlight && val === undefined) {
             return this.hide();
@@ -359,7 +359,7 @@ export class Typeahead<ItemType extends string | object> {
     }
 
     set_value(): void {
-        const val = this.values.get(this.$menu.find(".active")[0]!);
+        const val = this.values.get(the(this.$menu.find(".active")));
         assert(typeof val === "string");
         if (this.input_element.type === "contenteditable") {
             this.input_element.$element.text(val);
@@ -392,7 +392,7 @@ export class Typeahead<ItemType extends string | object> {
 
         const input_element = this.input_element;
         if (!this.non_tippy_parent_element) {
-            this.instance = tippy.default(input_element.$element[0]!, {
+            this.instance = tippy.default(the(input_element.$element), {
                 // Lets typeahead take the width needed to fit the content
                 // and wraps it if it overflows the visible container.
                 maxWidth: "none",
@@ -425,7 +425,7 @@ export class Typeahead<ItemType extends string | object> {
                 interactive: true,
                 appendTo: () => document.body,
                 showOnCreate: true,
-                content: this.$container[0]!,
+                content: the(this.$container),
                 // We expect the typeahead creator to handle when to hide / show the typeahead.
                 trigger: "manual",
                 arrow: false,
@@ -435,8 +435,8 @@ export class Typeahead<ItemType extends string | object> {
 
                     if (input_element.type === "textarea") {
                         const caret = getCaretCoordinates(
-                            input_element.$element[0]!,
-                            input_element.$element[0]!.selectionStart,
+                            the(input_element.$element),
+                            the(input_element.$element).selectionStart,
                         );
                         // Used to consider the scroll height of textbox in the vertical offset.
                         const scrollTop = input_element.$element.scrollTop() ?? 0;
@@ -554,7 +554,7 @@ export class Typeahead<ItemType extends string | object> {
     render(final_items: ItemType[], matching_items: ItemType[]): this {
         const $items: JQuery[] = final_items.map((item) => {
             const $i = $(ITEM_HTML);
-            this.values.set($i[0]!, item);
+            this.values.set(the($i), item);
             const item_html = this.highlighter_html(item, this.query) ?? "";
             const $item_html = $i.find("a").html(item_html);
 
@@ -743,12 +743,12 @@ export class Typeahead<ItemType extends string | object> {
 
                 this.select(e);
 
-                if (this.input_element.$element[0]!.id === "stream_message_recipient_topic") {
+                if (the(this.input_element.$element).id === "stream_message_recipient_topic") {
                     assert(this.input_element.type === "input");
                     // Move the cursor to the end of the topic
                     const topic_length = this.input_element.$element.val()!.length;
-                    this.input_element.$element[0]!.selectionStart = topic_length;
-                    this.input_element.$element[0]!.selectionEnd = topic_length;
+                    the(this.input_element.$element).selectionStart = topic_length;
+                    the(this.input_element.$element).selectionEnd = topic_length;
                 }
 
                 break;
@@ -775,7 +775,7 @@ export class Typeahead<ItemType extends string | object> {
                 // when shift (keycode 16) + tabbing to the topic field
                 if (
                     pseudo_keycode === 16 &&
-                    this.input_element.$element[0]!.id === "stream_message_recipient_topic"
+                    the(this.input_element.$element).id === "stream_message_recipient_topic"
                 ) {
                     return;
                 }

web/src/about_zulip.ts
@@ -7,6 +7,7 @@ import * as browser_history from "./browser_history";
 import {show_copied_confirmation} from "./copied_tooltip";
 import * as overlays from "./overlays";
 import {realm} from "./state_data";
+import * as util from "./util";
 
 export function launch(): void {
     overlays.open_overlay({
@@ -19,12 +20,12 @@ export function launch(): void {
 
     const zulip_version_clipboard = new ClipboardJS("#about-zulip .fa-copy.zulip-version");
     zulip_version_clipboard.on("success", () => {
-        show_copied_confirmation($("#about-zulip .fa-copy.zulip-version")[0]!);
+        show_copied_confirmation(util.the($("#about-zulip .fa-copy.zulip-version")));
     });
 
     const zulip_merge_base_clipboard = new ClipboardJS("#about-zulip .fa-copy.zulip-merge-base");
     zulip_merge_base_clipboard.on("success", () => {
-        show_copied_confirmation($("#about-zulip .fa-copy.zulip-merge-base")[0]!);
+        show_copied_confirmation(util.the($("#about-zulip .fa-copy.zulip-merge-base")));
     });
 }
 

web/src/audible_notifications.ts
@@ -1,6 +1,7 @@
 import $ from "jquery";
 
 import {user_settings} from "./user_settings";
+import * as util from "./util";
 
 export function initialize(): void {
     update_notification_sound_source($("audio#user-notification-sound-audio"), user_settings);
@@ -22,6 +23,6 @@ export function update_notification_sound_source(
     if (notification_sound !== "none") {
         // Load it so that it is ready to be played; without this the old sound
         // is played.
-        $container_elem[0]!.load();
+        util.the($container_elem).load();
     }
 }

web/src/billing/helpers.ts
@@ -2,6 +2,7 @@ import $ from "jquery";
 import {z} from "zod";
 
 import * as loading from "../loading";
+import * as util from "../util";
 
 export type FormDataObject = Record<string, string>;
 
@@ -162,7 +163,7 @@ export function update_discount_details(
 }
 
 export function is_valid_input($elem: JQuery<HTMLFormElement>): boolean {
-    return $elem[0]!.checkValidity();
+    return util.the($elem).checkValidity();
 }
 
 export function redirect_to_billing_with_successful_upgrade(billing_base_url: string): void {

web/src/buddy_list.ts
@@ -26,6 +26,7 @@ import * as stream_data from "./stream_data";
 import type {StreamSubscription} from "./sub_store";
 import {INTERACTIVE_HOVER_DELAY} from "./tippyjs";
 import {user_settings} from "./user_settings";
+import * as util from "./util";
 
 function get_formatted_sub_count(sub_count: number): string {
     if (sub_count < 1000) {
@@ -172,7 +173,7 @@ export class BuddyList extends BuddyListConf {
                     // This will default to "bottom" placement for this tooltip.
                     placement = "auto";
                 }
-                tippy.default($elem[0]!, {
+                tippy.default(util.the($elem), {
                     // Because the buddy list subheadings are potential click targets
                     // for purposes having nothing to do with the subscriber count
                     // (collapsing/expanding), we delay showing the tooltip until the
@@ -765,9 +766,7 @@ export class BuddyList extends BuddyListConf {
     fill_screen_with_content(): void {
         let height = this.height_to_fill();
 
-        const elem = scroll_util
-            .get_scroll_element($(this.scroll_container_selector))
-            .expectOne()[0]!;
+        const elem = util.the(scroll_util.get_scroll_element($(this.scroll_container_selector)));
 
         // Add a fudge factor.
         height += 10;

web/src/common.ts
@@ -2,6 +2,7 @@ import $ from "jquery";
 import * as tippy from "tippy.js";
 
 import {$t} from "./i18n";
+import * as util from "./util";
 
 export const status_classes = "alert-error alert-success alert-info alert-warning alert-loading";
 
@@ -108,7 +109,7 @@ function set_password_toggle_label(
 ): void {
     $(password_selector).attr("aria-label", label);
     if (tippy_tooltips) {
-        const element: tippy.ReferenceElement = $(password_selector)[0]!;
+        const element: tippy.ReferenceElement = util.the($(password_selector));
         const tippy_instance = element._tippy ?? tippy.default(element);
         tippy_instance.setContent(label);
     } else {

web/src/compose_recipient.ts
@@ -281,7 +281,7 @@ function on_hidden_callback(): void {
         // Always move focus to the topic input even if it's not empty,
         // since it's likely the user will want to update the topic
         // after updating the stream.
-        ui_util.place_caret_at_end($("input#stream_message_recipient_topic")[0]!);
+        ui_util.place_caret_at_end(util.the($("input#stream_message_recipient_topic")));
     } else {
         if (compose_state.private_message_recipient().length === 0) {
             $("#private_message_recipient").trigger("focus").trigger("select");

web/src/compose_ui.ts
@@ -123,9 +123,9 @@ export function insert_and_scroll_into_view(
         // to support `undo`, we can use a faster method.
         $textarea.val(content);
     } else if (replace_all) {
-        setFieldText($textarea[0]!, content);
+        setFieldText(util.the($textarea), content);
     } else {
-        insertTextIntoField($textarea[0]!, content);
+        insertTextIntoField(util.the($textarea), content);
     }
     // Blurring and refocusing ensures the cursor / selection is in view
     // in chromium browsers.
@@ -300,7 +300,7 @@ export function replace_syntax(
     // for details.
 
     const old_text = $textarea.val();
-    replaceFieldText($textarea[0]!, old_syntax, () => new_syntax, "after-replacement");
+    replaceFieldText(util.the($textarea), old_syntax, () => new_syntax, "after-replacement");
     const new_text = $textarea.val();
 
     // When replacing content in a textarea, we need to move the cursor

commit 57ceb8c8078e9926c03813220afbabb3cf4b9fa4

bootstrap_typeahead: Fix bug on select with no active item.
This bug was introduced in 36eec51e947b3d9d6f547fe36583d32b0f7595a8
It's possible that `find(".active")` returns `undefined` here.

web/src/bootstrap_typeahead.ts
@@ -322,7 +322,8 @@ export class Typeahead<ItemType extends string | object> {
     }
 
     select(e?: JQuery.ClickEvent | JQuery.KeyUpEvent | JQuery.KeyDownEvent): this {
-        const val = this.values.get(the(this.$menu.find(".active")));
+        const active_option = this.$menu.find(".active")[0];
+        const val = active_option ? this.values.get(active_option) : undefined;
         // It's possible that we got here from pressing enter with nothing highlighted.
         if (!this.requireHighlight && val === undefined) {
             return this.hide();