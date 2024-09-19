kmixco/Zulip

user_groups: Do not show "role:" prefix for system groups.
Instead of showing the actual names like "role:everyone",
"role:moderators", etc. for system groups, we show
"Everyone", "Moderators", etc. for system user group in
pills, typeaheads and popovers.

Though system groups are not shown in typeahead as of
this commit, we update the typeahead code as well to
not conside "role:" prefix while matching with the
query as we would soon show system groups in typeahead
when we would add new UI for group-based settings.

Previously, only "role:everyone" group was shown as
"Everyone" in popover and pills but for other system
groups their original names for shown and this commit
changes that behavior to be same for all system groups.

The original display_name field for
settings_config.system_user_groups_list objects, which
was used for dropdown widgets, is also renamed so that
we can use display_name field for the names to be used
in other places.

web/tests/user_groups.test.js
@@ -495,7 +495,7 @@ run_test("get_realm_user_groups_for_dropdown_list_widget", () => {
 
 run_test("get_display_group_name", () => {
     const admins = {
-        name: "Admins",
+        name: "role:administrators",
         description: "foo",
         id: 1,
         members: new Set([1]),
@@ -509,7 +509,19 @@ run_test("get_display_group_name", () => {
         is_system_group: false,
         direct_subgroup_ids: new Set([1]),
     };
+    const students = {
+        name: "Students",
+        id: 3,
+        members: new Set([1, 3]),
+        is_system_group: false,
+        direct_subgroup_ids: new Set([]),
+    };
 
-    assert.equal(user_groups.get_display_group_name(admins), "Admins");
-    assert.equal(user_groups.get_display_group_name(all), "translated: Everyone");
+    user_groups.initialize({
+        realm_user_groups: [admins, all, students],
+    });
+
+    assert.equal(user_groups.get_display_group_name(admins.name), "translated: Admins");
+    assert.equal(user_groups.get_display_group_name(all.name), "translated: Everyone");
+    assert.equal(user_groups.get_display_group_name(students.name), "Students");
 });

afedotov89/flexar-zulip

web/tests/user_groups.test.js
@@ -528,7 +528,7 @@ run_test("get_realm_user_groups_for_dropdown_list_widget", () => {
 
 run_test("get_display_group_name", () => {
     const admins = {
-        name: "Admins",
+        name: "role:administrators",
         description: "foo",
         id: 1,
         members: new Set([1]),
@@ -542,7 +542,19 @@ run_test("get_display_group_name", () => {
         is_system_group: false,
         direct_subgroup_ids: new Set([1]),
     };
+    const students = {
+        name: "Students",
+        id: 3,
+        members: new Set([1, 3]),
+        is_system_group: false,
+        direct_subgroup_ids: new Set([]),
+    };
 
-    assert.equal(user_groups.get_display_group_name(admins), "Admins");
-    assert.equal(user_groups.get_display_group_name(all), "translated: Everyone");
+    user_groups.initialize({
+        realm_user_groups: [admins, all, students],
+    });
+
+    assert.equal(user_groups.get_display_group_name(admins.name), "translated: Administrators");
+    assert.equal(user_groups.get_display_group_name(all.name), "translated: Everyone");
+    assert.equal(user_groups.get_display_group_name(students.name), "Students");
 });