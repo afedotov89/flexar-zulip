zulip/zulip

stream: Show `(archived)` at the end of deactivated stream names.
When a stream is deactivated the title area and messages are
re-rendered to update the stream name with `(archived)` suffix.

web/src/stream_data.ts
@@ -647,6 +647,11 @@ export function get_stream_privacy_policy(stream_id: number): string {
     return settings_config.stream_privacy_policy_values.private_with_public_history.code;
 }
 
+export function is_stream_archived(stream_id: number): boolean {
+    const sub = sub_store.get(stream_id);
+    return sub ? sub.archived : false;
+}
+
 export function is_web_public(stream_id: number): boolean {
     const sub = sub_store.get(stream_id);
     return sub ? sub.is_web_public : false;

afedotov89/flexar-zulip

web/src/stream_data.ts
@@ -629,6 +629,11 @@ export function get_stream_privacy_policy(stream_id: number): string {
     return settings_config.stream_privacy_policy_values.private_with_public_history.code;
 }
 
+export function is_stream_archived(stream_id: number): boolean {
+    const sub = sub_store.get(stream_id);
+    return sub ? sub.is_archived : false;
+}
+
 export function is_web_public(stream_id: number): boolean {
     const sub = sub_store.get(stream_id);
     return sub ? sub.is_web_public : false;