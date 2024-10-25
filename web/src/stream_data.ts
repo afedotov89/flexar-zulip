zulip/zulip

can_post_messages_in_stream: Add condition check for archived streams.
Prevents users from sending any message in an archived stream.

web/src/stream_data.ts
@@ -582,6 +582,10 @@ export function can_unsubscribe_others(sub: StreamSubscription): boolean {
 }
 
 export function can_post_messages_in_stream(stream: StreamSubscription): boolean {
+    if (stream.archived) {
+        return false;
+    }
+
     if (page_params.is_spectator) {
         return false;
     }

web/tests/stream_data.test.js
@@ -974,6 +974,9 @@ test("can_post_messages_in_stream", () => {
 
     page_params.is_spectator = true;
     assert.equal(stream_data.can_post_messages_in_stream(social), false);
+
+    social.archived = true;
+    assert.equal(stream_data.can_post_messages_in_stream(social), false);
 });
 
 test("can_unsubscribe_others", () => {

afedotov89/flexar-zulip

web/src/stream_data.ts
@@ -568,6 +568,10 @@ export function can_unsubscribe_others(sub: StreamSubscription): boolean {
 }
 
 export function can_post_messages_in_stream(stream: StreamSubscription): boolean {
+    if (stream.is_archived) {
+        return false;
+    }
+
     if (page_params.is_spectator) {
         return false;
     }

web/tests/stream_data.test.js
@@ -1116,6 +1116,9 @@ test("can_post_messages_in_stream", ({override}) => {
 
     page_params.is_spectator = true;
     assert.equal(stream_data.can_post_messages_in_stream(social), false);
+
+    social.is_archived = true;
+    assert.equal(stream_data.can_post_messages_in_stream(social), false);
 });
 
 test("can_unsubscribe_others", ({override}) => {