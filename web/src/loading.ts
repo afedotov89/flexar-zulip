zulip/zulip

settings: Extract common logic for hide_spinners.
Move the redundant code for hiding spinners and re-enabling buttons into
a common `hide_spinner` function inside `loading.ts`. This reduces
duplication between `hide_button_spinner` and `hide_dialog_spinner`.

Fixes #26691.

web/src/loading.ts
@@ -108,3 +108,14 @@ export function show_spinner($element: JQuery, $spinner: JQuery): void {
         height: span_height,
     });
 }
+
+export function hide_spinner($element: JQuery, $spinner: JQuery): void {
+    // Re-enable the element (button or dialog button)
+    $element.prop("disabled", false);
+
+    // Show the span
+    $element.find("span").show();
+
+    // Destroy the loading indicator
+    destroy_indicator($spinner);
+}

afedotov89/flexar-zulip

web/src/loading.ts
@@ -106,3 +106,11 @@ export function show_spinner($button_element: JQuery, $spinner: JQuery): void {
         height: span_height,
     });
 }
+
+export function hide_spinner($button_element: JQuery, $spinner: JQuery): void {
+    // Show the span
+    $button_element.find(".submit-button-text").show();
+
+    // Destroy the loading indicator
+    destroy_indicator($spinner);
+}