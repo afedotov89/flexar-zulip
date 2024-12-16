zulip/zulip

compose: Open the compose box with channel picker.
This commit changes the code in such a way that the
"Start new conversation" button open the compose box with the channel
picker open, as we do when no channels is selected.

Fixes #28410.

Co-authored-by: Kunal Sharma <v.shm.kunal@gmail.com>

web/src/compose_actions.ts
@@ -328,9 +328,20 @@ export let start = (raw_opts: ComposeActionsStartOpts): void => {
     if (opts.message_type === "private") {
         compose_state.set_compose_recipient_id(compose_state.DIRECT_MESSAGE_ID);
         compose_recipient.on_compose_select_recipient_update();
-    } else if (opts.stream_id) {
+    } else if (opts.stream_id && opts.topic) {
         compose_state.set_stream_id(opts.stream_id);
         compose_recipient.on_compose_select_recipient_update();
+    } else if (opts.stream_id) {
+        const stream = stream_data.get_sub_by_id(opts.stream_id);
+        if (stream && stream_data.can_post_messages_in_stream(stream)) {
+            compose_state.set_stream_id(opts.stream_id);
+            compose_recipient.on_compose_select_recipient_update();
+        } else {
+            opts.stream_id = undefined;
+            compose_state.set_stream_id("");
+            opts.topic = "";
+            compose_recipient.open_compose_recipient_dropdown();
+        }
     } else {
         // Open stream selection dropdown if no stream is selected.
         compose_state.set_stream_id("");

afedotov89/flexar-zulip

web/src/compose_actions.ts
@@ -328,9 +328,20 @@ export let start = (raw_opts: ComposeActionsStartOpts): void => {
     if (opts.message_type === "private") {
         compose_state.set_compose_recipient_id(compose_state.DIRECT_MESSAGE_ID);
         compose_recipient.on_compose_select_recipient_update();
-    } else if (opts.stream_id) {
+    } else if (opts.stream_id && opts.topic) {
         compose_state.set_stream_id(opts.stream_id);
         compose_recipient.on_compose_select_recipient_update();
+    } else if (opts.stream_id) {
+        const stream = stream_data.get_sub_by_id(opts.stream_id);
+        if (stream && stream_data.can_post_messages_in_stream(stream)) {
+            compose_state.set_stream_id(opts.stream_id);
+            compose_recipient.on_compose_select_recipient_update();
+        } else {
+            opts.stream_id = undefined;
+            compose_state.set_stream_id("");
+            opts.topic = "";
+            compose_recipient.toggle_compose_recipient_dropdown();
+        }
     } else {
         // Open stream selection dropdown if no stream is selected.
         compose_state.set_stream_id("");