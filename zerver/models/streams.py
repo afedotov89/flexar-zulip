zulip/zulip

message: Use new setting for checking stream posting permissions.

zerver/models/streams.py
@@ -261,6 +261,18 @@ def get_stream_by_id_in_realm(stream_id: int, realm: Realm) -> Stream:
     return Stream.objects.select_related("realm", "recipient").get(id=stream_id, realm=realm)
 
 
+def get_stream_for_sending_message(stream_name: str, realm: Realm) -> Stream:
+    return Stream.objects.select_related(
+        "can_send_message_group", "can_send_message_group__named_user_group"
+    ).get(name__iexact=stream_name.strip(), realm_id=realm.id)
+
+
+def get_stream_by_id_for_sending_message(stream_id: int, realm: Realm) -> Stream:
+    return Stream.objects.select_related(
+        "realm", "recipient", "can_send_message_group", "can_send_message_group__named_user_group"
+    ).get(id=stream_id, realm=realm)
+
+
 def bulk_get_streams(realm: Realm, stream_names: set[str]) -> dict[str, Any]:
     def fetch_streams_by_name(stream_names: set[str]) -> QuerySet[Stream]:
         #

afedotov89/flexar-zulip

zerver/models/streams.py
@@ -261,6 +261,18 @@ def get_stream_by_id_in_realm(stream_id: int, realm: Realm) -> Stream:
     return Stream.objects.select_related("realm", "recipient").get(id=stream_id, realm=realm)
 
 
+def get_stream_by_name_for_sending_message(stream_name: str, realm: Realm) -> Stream:
+    return Stream.objects.select_related(
+        "can_send_message_group", "can_send_message_group__named_user_group"
+    ).get(name__iexact=stream_name.strip(), realm_id=realm.id)
+
+
+def get_stream_by_id_for_sending_message(stream_id: int, realm: Realm) -> Stream:
+    return Stream.objects.select_related(
+        "realm", "recipient", "can_send_message_group", "can_send_message_group__named_user_group"
+    ).get(id=stream_id, realm=realm)
+
+
 def bulk_get_streams(realm: Realm, stream_names: set[str]) -> dict[str, Any]:
     def fetch_streams_by_name(stream_names: set[str]) -> QuerySet[Stream]:
         #