zulip/zulip

settings: Update UI for information density settings.
This commit adds new UI for controlling font size and
line height settings in settings panel.

Fixes #33186.

web/src/settings_preferences.ts
@@ -1,7 +1,7 @@
 import $ from "jquery";
 import Cookies from "js-cookie";
 import assert from "minimalistic-assert";
-import type {z} from "zod";
+import {z} from "zod";
 
 import render_dialog_default_language from "../templates/default_language_modal.hbs";
 
@@ -10,12 +10,7 @@ import * as dialog_widget from "./dialog_widget.ts";
 import * as emojisets from "./emojisets.ts";
 import * as hash_parser from "./hash_parser.ts";
 import {$t_html, get_language_list_columns, get_language_name} from "./i18n.ts";
-import {
-    LEGACY_FONT_SIZE_PX,
-    LEGACY_LINE_HEIGHT_PERCENT,
-    NON_COMPACT_MODE_FONT_SIZE_PX,
-    NON_COMPACT_MODE_LINE_HEIGHT_PERCENT,
-} from "./information_density.ts";
+import * as information_density from "./information_density.ts";
 import * as loading from "./loading.ts";
 import * as overlays from "./overlays.ts";
 import {page_params} from "./page_params.ts";
@@ -63,6 +58,7 @@ function change_display_setting(
     data: Record<string, string | boolean | number>,
     $status_el: JQuery,
     success_continuation?: (response_data: unknown) => void,
+    error_continuation?: (response_data: unknown) => void,
     success_msg_html?: string,
     sticky?: boolean,
 ): void {
@@ -77,6 +73,10 @@ function change_display_setting(
 
     if (success_continuation !== undefined) {
         opts.success_continuation = success_continuation;
+    }
+
+    if (error_continuation !== undefined) {
+        opts.error_continuation = error_continuation;
     }
 
     if (sticky && success_msg_html) {
@@ -155,6 +155,7 @@ function user_default_language_modal_post_render(): void {
             change_display_setting(
                 data,
                 $("#settings_content").find(".general-settings-status"),
+                undefined,
                 undefined,
                 $t_html(
                     {
@@ -248,7 +249,7 @@ export function set_up(settings_panel: SettingsPanel): void {
         .find(".setting_web_stream_unreads_count_display_policy")
         .val(settings_object.web_stream_unreads_count_display_policy);
 
-    update_information_density_settings_visibility($container, settings_object, {});
+    information_density.enable_or_disable_control_buttons($container);
 
     if (for_realm_settings) {
         // For the realm-level defaults page, we use the common
@@ -258,52 +259,60 @@ export function set_up(settings_panel: SettingsPanel): void {
 
     // Common handler for sending requests to the server when an input
     // element is changed.
-    $container.on(
-        "change",
-        "input[type=checkbox], .information-density-settings input[type=text], select",
-        function (this: HTMLElement, e) {
-            const $input_elem = $(e.currentTarget);
-            const setting = $input_elem.attr("name");
-            assert(setting !== undefined);
-            const data: Record<string, string | boolean | number> = {};
-            const setting_value = settings_components.get_input_element_value(this)!;
-            assert(typeof setting_value !== "object");
-            data[setting] = setting_value;
+    $container.on("change", "input[type=checkbox], select", function (this: HTMLElement, e) {
+        const $input_elem = $(e.currentTarget);
+        const setting = $input_elem.attr("name");
+        assert(setting !== undefined);
+        const data: Record<string, string | boolean | number> = {};
+        const setting_value = settings_components.get_input_element_value(this)!;
+        assert(typeof setting_value !== "object");
+        data[setting] = setting_value;
 
-            if (setting === "dense_mode") {
-                data.web_font_size_px = setting_value
-                    ? LEGACY_FONT_SIZE_PX
-                    : NON_COMPACT_MODE_FONT_SIZE_PX;
-                data.web_line_height_percent = setting_value
-                    ? LEGACY_LINE_HEIGHT_PERCENT
-                    : NON_COMPACT_MODE_LINE_HEIGHT_PERCENT;
-            }
+        const $status_element = $input_elem
+            .closest(".subsection-parent")
+            .find(".alert-notification");
+        change_display_setting(data, $status_element);
+    });
 
-            if (
-                ((setting === "web_font_size_px" && setting_value !== LEGACY_FONT_SIZE_PX) ||
-                    (setting === "web_line_height_percent" &&
-                        setting_value !== LEGACY_LINE_HEIGHT_PERCENT)) &&
-                user_settings.dense_mode
-            ) {
-                data.dense_mode = false;
-            }
+    $container.find(".info-density-button").on("click", function (this: HTMLElement, e) {
+        e.preventDefault();
+        const changed_property = z
+            .enum(["web_font_size_px", "web_line_height_percent"])
+            .parse($(this).parent().attr("data-property"));
+        const new_value = information_density.get_new_value_for_information_density_settings(
+            $(this),
+            changed_property,
+        );
+        const original_values: {
+            web_font_size_px?: number;
+            web_line_height_percent?: number;
+        } = {};
+        original_values[changed_property] = user_settings[changed_property];
 
-            let success_continuation;
-            if (["dense_mode", "web_font_size_px", "web_line_height_percent"].includes(setting)) {
-                success_continuation = () => {
-                    update_information_density_settings_visibility(
-                        $container,
-                        settings_object,
-                        data,
-                    );
-                };
+        const changed_settings = information_density.update_information_density_settings(
+            $(this),
+            changed_property,
+            new_value,
+        );
+
+        const $status_element = $(this).closest(".subsection-parent").find(".alert-notification");
+        information_density.enable_or_disable_control_buttons($container);
+
+        const error_continuation: () => void = () => {
+            information_density.update_information_density_settings(
+                $(this),
+                changed_property,
+                z.number().parse(original_values[changed_property]),
+            );
+            if (changed_settings.dense_mode !== undefined) {
+                user_settings.dense_mode = true;
+                $("body").toggleClass("less-dense-mode");
+                $("body").toggleClass("more-dense-mode");
             }
-            const $status_element = $input_elem
-                .closest(".subsection-parent")
-                .find(".alert-notification");
-            change_display_setting(data, $status_element, success_continuation);
-        },
-    );
+            information_density.enable_or_disable_control_buttons($container);
+        };
+        change_display_setting(changed_settings, $status_element, undefined, error_continuation);
+    });
 
     $container.find(".setting_color_scheme").on("change", function () {
         const $input_elem = $(this);
@@ -463,37 +472,6 @@ export function update_page(property: UserSettingsProperty): void {
 
     const $input_elem = $container.find(`[name=${CSS.escape(property)}]`);
     settings_components.set_input_element_value($input_elem, value);
-}
-
-export function update_information_density_settings_visibility(
-    $container: JQuery,
-    settings_object: UserSettings | RealmDefaultSettings,
-    request_data: Record<string, boolean | number | string>,
-): void {
-    if (page_params.development_environment) {
-        $container.find(".information-density-settings").show();
-        return;
-    }
-
-    const dense_mode = request_data.dense_mode ?? settings_object.dense_mode;
-    const web_font_size_px = request_data.web_font_size_px ?? settings_object.web_font_size_px;
-    const web_line_height_percent =
-        request_data.web_line_height_percent ?? settings_object.web_line_height_percent;
-
-    if (dense_mode) {
-        $container.find(".information-density-settings").hide();
-        return;
-    }
-
-    if (
-        web_font_size_px === NON_COMPACT_MODE_FONT_SIZE_PX &&
-        web_line_height_percent === NON_COMPACT_MODE_LINE_HEIGHT_PERCENT
-    ) {
-        $container.find(".information-density-settings").hide();
-        return;
-    }
-
-    $container.find(".information-density-settings").show();
 }
 
 export function initialize(): void {

web/src/settings_realm_user_settings_defaults.ts
@@ -1,14 +1,12 @@
 import $ from "jquery";
 import assert from "minimalistic-assert";
+import {z} from "zod";
 
 import * as audible_notifications from "./audible_notifications.ts";
-import {
-    NON_COMPACT_MODE_FONT_SIZE_PX,
-    NON_COMPACT_MODE_LINE_HEIGHT_PERCENT,
-} from "./information_density.ts";
+import * as information_density from "./information_density.ts";
 import * as overlays from "./overlays.ts";
-import {page_params} from "./page_params.ts";
 import {realm_user_settings_defaults} from "./realm_user_settings_defaults.ts";
+import * as settings_components from "./settings_components.ts";
 import * as settings_notifications from "./settings_notifications.ts";
 import * as settings_org from "./settings_org.ts";
 import * as settings_preferences from "./settings_preferences.ts";
@@ -33,6 +31,13 @@ export function maybe_disable_widgets(): void {
         $(".organization-box [data-name='organization-level-user-defaults']")
             .find(".play_notification_sound")
             .addClass("control-label-disabled");
+
+        $(".organization-box [data-name='organization-level-user-defaults']")
+            .find(".info-density-button")
+            .prop("disabled", true);
+        $(".organization-box [data-name='organization-level-user-defaults']")
+            .find(".information-density-settings")
+            .addClass("disabled-setting");
     }
 }
 
@@ -76,24 +81,20 @@ export function set_up(): void {
         });
     });
 
-    if (!page_params.development_environment) {
-        $<HTMLInputElement>("#realm_dense_mode").on("change", function (this: HTMLInputElement) {
-            const val = this.checked;
-            if (val) {
-                $container.find(".information-density-settings").hide();
-                return;
-            }
-
-            if (
-                !realm_user_settings_defaults.dense_mode &&
-                (realm_user_settings_defaults.web_font_size_px !== NON_COMPACT_MODE_FONT_SIZE_PX ||
-                    realm_user_settings_defaults.web_line_height_percent !==
-                        NON_COMPACT_MODE_LINE_HEIGHT_PERCENT)
-            ) {
-                $container.find(".information-density-settings").show();
-            }
-        });
-    }
+    $container.find(".info-density-button").on("click", function (this: HTMLElement, e) {
+        e.preventDefault();
+        const changed_property = z
+            .enum(["web_font_size_px", "web_line_height_percent"])
+            .parse($(this).parent().attr("data-property"));
+        const new_value = information_density.get_new_value_for_information_density_settings(
+            $(this),
+            changed_property,
+        );
+        $(this).closest(".button-group").find(".current-value").text(new_value);
+        information_density.enable_or_disable_control_buttons($container);
+        const $subsection = $(this).closest(".settings-subsection-parent");
+        settings_components.save_discard_default_realm_settings_widget_status_handler($subsection);
+    });
 
     settings_notifications.set_up(realm_default_settings_panel);
 

afedotov89/flexar-zulip

web/src/settings_preferences.ts
@@ -1,7 +1,7 @@
 import $ from "jquery";
 import Cookies from "js-cookie";
 import assert from "minimalistic-assert";
-import type {z} from "zod";
+import {z} from "zod";
 
 import render_dialog_default_language from "../templates/default_language_modal.hbs";
 
@@ -10,12 +10,7 @@ import * as dialog_widget from "./dialog_widget.ts";
 import * as emojisets from "./emojisets.ts";
 import * as hash_parser from "./hash_parser.ts";
 import {$t_html, get_language_list_columns, get_language_name} from "./i18n.ts";
-import {
-    LEGACY_FONT_SIZE_PX,
-    LEGACY_LINE_HEIGHT_PERCENT,
-    NON_COMPACT_MODE_FONT_SIZE_PX,
-    NON_COMPACT_MODE_LINE_HEIGHT_PERCENT,
-} from "./information_density.ts";
+import * as information_density from "./information_density.ts";
 import * as loading from "./loading.ts";
 import * as overlays from "./overlays.ts";
 import {page_params} from "./page_params.ts";
@@ -63,6 +58,7 @@ function change_display_setting(
     data: Record<string, string | boolean | number>,
     $status_el: JQuery,
     success_continuation?: (response_data: unknown) => void,
+    error_continuation?: (response_data: unknown) => void,
     success_msg_html?: string,
     sticky?: boolean,
 ): void {
@@ -77,6 +73,10 @@ function change_display_setting(
 
     if (success_continuation !== undefined) {
         opts.success_continuation = success_continuation;
+    }
+
+    if (error_continuation !== undefined) {
+        opts.error_continuation = error_continuation;
     }
 
     if (sticky && success_msg_html) {
@@ -155,6 +155,7 @@ function user_default_language_modal_post_render(): void {
             change_display_setting(
                 data,
                 $("#settings_content").find(".general-settings-status"),
+                undefined,
                 undefined,
                 $t_html(
                     {
@@ -248,7 +249,7 @@ export function set_up(settings_panel: SettingsPanel): void {
         .find(".setting_web_stream_unreads_count_display_policy")
         .val(settings_object.web_stream_unreads_count_display_policy);
 
-    update_information_density_settings_visibility($container, settings_object, {});
+    information_density.enable_or_disable_control_buttons($container);
 
     if (for_realm_settings) {
         // For the realm-level defaults page, we use the common
@@ -258,52 +259,50 @@ export function set_up(settings_panel: SettingsPanel): void {
 
     // Common handler for sending requests to the server when an input
     // element is changed.
-    $container.on(
-        "change",
-        "input[type=checkbox], .information-density-settings input[type=text], select",
-        function (this: HTMLElement, e) {
-            const $input_elem = $(e.currentTarget);
-            const setting = $input_elem.attr("name");
-            assert(setting !== undefined);
-            const data: Record<string, string | boolean | number> = {};
-            const setting_value = settings_components.get_input_element_value(this)!;
-            assert(typeof setting_value !== "object");
-            data[setting] = setting_value;
+    $container.on("change", "input[type=checkbox], select", function (this: HTMLElement, e) {
+        const $input_elem = $(e.currentTarget);
+        const setting = $input_elem.attr("name");
+        assert(setting !== undefined);
+        const data: Record<string, string | boolean | number> = {};
+        const setting_value = settings_components.get_input_element_value(this)!;
+        assert(typeof setting_value !== "object");
+        data[setting] = setting_value;
 
-            if (setting === "dense_mode") {
-                data.web_font_size_px = setting_value
-                    ? LEGACY_FONT_SIZE_PX
-                    : NON_COMPACT_MODE_FONT_SIZE_PX;
-                data.web_line_height_percent = setting_value
-                    ? LEGACY_LINE_HEIGHT_PERCENT
-                    : NON_COMPACT_MODE_LINE_HEIGHT_PERCENT;
-            }
+        const $status_element = $input_elem
+            .closest(".subsection-parent")
+            .find(".alert-notification");
+        change_display_setting(data, $status_element);
+    });
 
-            if (
-                ((setting === "web_font_size_px" && setting_value !== LEGACY_FONT_SIZE_PX) ||
-                    (setting === "web_line_height_percent" &&
-                        setting_value !== LEGACY_LINE_HEIGHT_PERCENT)) &&
-                user_settings.dense_mode
-            ) {
-                data.dense_mode = false;
-            }
+    $container.find(".info-density-button").on("click", function (this: HTMLElement, e) {
+        e.preventDefault();
+        const changed_property = z
+            .enum(["web_font_size_px", "web_line_height_percent"])
+            .parse($(this).closest(".button-group").attr("data-property"));
+        const original_value = user_settings[changed_property];
 
-            let success_continuation;
-            if (["dense_mode", "web_font_size_px", "web_line_height_percent"].includes(setting)) {
-                success_continuation = () => {
-                    update_information_density_settings_visibility(
-                        $container,
-                        settings_object,
-                        data,
-                    );
-                };
-            }
-            const $status_element = $input_elem
-                .closest(".subsection-parent")
-                .find(".alert-notification");
-            change_display_setting(data, $status_element, success_continuation);
-        },
-    );
+        const new_value = information_density.update_information_density_settings(
+            $(this),
+            changed_property,
+            true,
+        );
+        const data: Record<string, number> = {};
+        data[changed_property] = new_value;
+
+        const $status_element = $(this).closest(".subsection-parent").find(".alert-notification");
+        information_density.enable_or_disable_control_buttons($container);
+
+        const error_continuation: () => void = () => {
+            information_density.update_information_density_settings(
+                $(this),
+                changed_property,
+                true,
+                original_value,
+            );
+            information_density.enable_or_disable_control_buttons($container);
+        };
+        change_display_setting(data, $status_element, undefined, error_continuation);
+    });
 
     $container.find(".setting_color_scheme").on("change", function () {
         const $input_elem = $(this);
@@ -463,37 +462,6 @@ export function update_page(property: UserSettingsProperty): void {
 
     const $input_elem = $container.find(`[name=${CSS.escape(property)}]`);
     settings_components.set_input_element_value($input_elem, value);
-}
-
-export function update_information_density_settings_visibility(
-    $container: JQuery,
-    settings_object: UserSettings | RealmDefaultSettings,
-    request_data: Record<string, boolean | number | string>,
-): void {
-    if (page_params.development_environment) {
-        $container.find(".information-density-settings").show();
-        return;
-    }
-
-    const dense_mode = request_data.dense_mode ?? settings_object.dense_mode;
-    const web_font_size_px = request_data.web_font_size_px ?? settings_object.web_font_size_px;
-    const web_line_height_percent =
-        request_data.web_line_height_percent ?? settings_object.web_line_height_percent;
-
-    if (dense_mode) {
-        $container.find(".information-density-settings").hide();
-        return;
-    }
-
-    if (
-        web_font_size_px === NON_COMPACT_MODE_FONT_SIZE_PX &&
-        web_line_height_percent === NON_COMPACT_MODE_LINE_HEIGHT_PERCENT
-    ) {
-        $container.find(".information-density-settings").hide();
-        return;
-    }
-
-    $container.find(".information-density-settings").show();
 }
 
 export function initialize(): void {

web/src/settings_realm_user_settings_defaults.ts
@@ -2,13 +2,10 @@ import $ from "jquery";
 import assert from "minimalistic-assert";
 
 import * as audible_notifications from "./audible_notifications.ts";
-import {
-    NON_COMPACT_MODE_FONT_SIZE_PX,
-    NON_COMPACT_MODE_LINE_HEIGHT_PERCENT,
-} from "./information_density.ts";
+import * as information_density from "./information_density.ts";
 import * as overlays from "./overlays.ts";
-import {page_params} from "./page_params.ts";
 import {realm_user_settings_defaults} from "./realm_user_settings_defaults.ts";
+import * as settings_components from "./settings_components.ts";
 import * as settings_notifications from "./settings_notifications.ts";
 import * as settings_org from "./settings_org.ts";
 import * as settings_preferences from "./settings_preferences.ts";
@@ -33,6 +30,13 @@ export function maybe_disable_widgets(): void {
         $(".organization-box [data-name='organization-level-user-defaults']")
             .find(".play_notification_sound")
             .addClass("control-label-disabled");
+
+        $(".organization-box [data-name='organization-level-user-defaults']")
+            .find(".info-density-button")
+            .prop("disabled", true);
+        $(".organization-box [data-name='organization-level-user-defaults']")
+            .find(".information-density-settings")
+            .addClass("disabled-setting");
     }
 }
 
@@ -76,24 +80,25 @@ export function set_up(): void {
         });
     });
 
-    if (!page_params.development_environment) {
-        $<HTMLInputElement>("#realm_dense_mode").on("change", function (this: HTMLInputElement) {
-            const val = this.checked;
-            if (val) {
-                $container.find(".information-density-settings").hide();
-                return;
-            }
-
-            if (
-                !realm_user_settings_defaults.dense_mode &&
-                (realm_user_settings_defaults.web_font_size_px !== NON_COMPACT_MODE_FONT_SIZE_PX ||
-                    realm_user_settings_defaults.web_line_height_percent !==
-                        NON_COMPACT_MODE_LINE_HEIGHT_PERCENT)
-            ) {
-                $container.find(".information-density-settings").show();
-            }
-        });
-    }
+    $container.find(".info-density-button").on("click", function (this: HTMLElement, e) {
+        e.preventDefault();
+        const changed_property = information_density.information_density_properties_schema.parse(
+            $(this).closest(".button-group").attr("data-property"),
+        );
+        const new_value = information_density.get_new_value_for_information_density_settings(
+            $(this),
+            changed_property,
+        );
+        $(this).closest(".button-group").find(".current-value").val(new_value);
+        let display_value = new_value.toString();
+        if (changed_property === "web_line_height_percent") {
+            display_value = information_density.get_string_display_value_for_line_height(new_value);
+        }
+        $(this).closest(".button-group").find(".display-value").text(display_value);
+        information_density.enable_or_disable_control_buttons($container);
+        const $subsection = $(this).closest(".settings-subsection-parent");
+        settings_components.save_discard_default_realm_settings_widget_status_handler($subsection);
+    });
 
     settings_notifications.set_up(realm_default_settings_panel);
 