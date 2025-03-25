zulip/zulip

import_data: Make sure converted DMs don't have topic name.
Previously, `build_message` sets a message's topic name to the given
topic name, regardless of whether the message was a direct message (DM)
or a group direct message (GDM).

This change adds the `is_private` parameter to `build_message`. If
`is_private` is `True`, the `topic_name` will be overridden to an empty
string (""). Consequently, this also updates the third-party importers
to pass this parameter when calling `build_message`.

Co-authored-by: Pieter CK <pieterceka123@gmail.com>

zerver/data_import/rocketchat.py
@@ -550,6 +550,7 @@ def process_raw_message_batch(
             has_image=has_image,
             has_link=has_link,
             has_attachment=has_attachment,
+            is_private=is_pm_data,
         )
         zerver_message.append(message)
         build_reactions(

afedotov89/flexar-zulip

zerver/data_import/rocketchat.py
@@ -552,6 +552,7 @@ def process_raw_message_batch(
             has_image=has_image,
             has_link=has_link,
             has_attachment=has_attachment,
+            is_direct_message_type=is_pm_data,
         )
         zerver_message.append(message)
         build_reactions(