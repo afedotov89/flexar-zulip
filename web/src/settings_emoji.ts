zulip/zulip

organization-settings: Redesign edit and delete buttons in settings tables.
Fixes part of #34200.

web/src/settings_emoji.ts
@@ -321,7 +321,9 @@ export function set_up(): void {
         e.preventDefault();
         e.stopPropagation();
         const $button = $(this);
-        const url = "/json/realm/emoji/" + encodeURIComponent($button.attr("data-emoji-name")!);
+        const url =
+            "/json/realm/emoji/" +
+            encodeURIComponent($button.closest("td").attr("data-emoji-name")!);
         const html_body = render_confirm_deactivate_custom_emoji({});
 
         const opts = {

web/src/settings_exports.ts
@@ -426,7 +426,9 @@ export function set_up(): void {
         e.preventDefault();
         e.stopPropagation();
         const $button = $(this);
-        const url = "/json/export/realm/" + encodeURIComponent($button.attr("data-export-id")!);
+        const url =
+            "/json/export/realm/" +
+            encodeURIComponent($button.closest("td").attr("data-export-id")!);
         const html_body = render_confirm_delete_data_export();
 
         confirm_dialog.launch({

web/src/settings_playgrounds.ts
@@ -93,7 +93,8 @@ function build_page(): void {
         e.stopPropagation();
         const $button = $(this);
         const url =
-            "/json/realm/playgrounds/" + encodeURIComponent($button.attr("data-playground-id")!);
+            "/json/realm/playgrounds/" +
+            encodeURIComponent($button.closest("td").attr("data-playground-id")!);
         const html_body = render_confirm_delete_playground();
 
         confirm_dialog.launch({

afedotov89/flexar-zulip

web/src/settings_emoji.ts
@@ -321,7 +321,9 @@ export function set_up(): void {
         e.preventDefault();
         e.stopPropagation();
         const $button = $(this);
-        const url = "/json/realm/emoji/" + encodeURIComponent($button.attr("data-emoji-name")!);
+        const url =
+            "/json/realm/emoji/" +
+            encodeURIComponent($button.closest("tr").attr("data-emoji-name")!);
         const html_body = render_confirm_deactivate_custom_emoji({});
 
         const opts = {

web/src/settings_exports.ts
@@ -426,7 +426,9 @@ export function set_up(): void {
         e.preventDefault();
         e.stopPropagation();
         const $button = $(this);
-        const url = "/json/export/realm/" + encodeURIComponent($button.attr("data-export-id")!);
+        const url =
+            "/json/export/realm/" +
+            encodeURIComponent($button.closest("tr").attr("data-export-id")!);
         const html_body = render_confirm_delete_data_export();
 
         confirm_dialog.launch({

web/src/settings_playgrounds.ts
@@ -93,7 +93,8 @@ function build_page(): void {
         e.stopPropagation();
         const $button = $(this);
         const url =
-            "/json/realm/playgrounds/" + encodeURIComponent($button.attr("data-playground-id")!);
+            "/json/realm/playgrounds/" +
+            encodeURIComponent($button.closest("tr").attr("data-playground-id")!);
         const html_body = render_confirm_delete_playground();
 
         confirm_dialog.launch({