afedotov89/flexar-zulip

commit 403b1e172a7eee6c6ba76617fa26d458658ef711

find_account: Do not place Realm objects into context.

zerver/views/registration.py
@@ -1546,7 +1546,7 @@ def find_account(request: HttpRequest) -> HttpResponse:
                 key = user.delivery_email.lower()
                 context.setdefault(key, {})
                 context[key].setdefault("realms", [])
-                context[key]["realms"].append(user.realm)
+                context[key]["realms"].append([user.realm.name, user.realm.url])
                 # This value will end up being the last user ID among
                 # matching accounts; since it's only used for minor
                 # details like language, that arbitrary choice is OK.

commit 596fec49ac2a4e5cc7d9a805e13e378fa6308081

find_account: Fix HTML version of email.
This was broken in the refactor in 403b1e172a7e.

templates/zerver/emails/find_team.html
@@ -15,8 +15,8 @@
 {% endif %}
 
 <ul>
-    {% for realm in realms %}
-    <li><a href="{{ realm.url }}">{{ realm.name }} ({{ realm.host }})</a></li>
+    {% for realm_data in realms %}
+    <li><a href="{{ realm_data.url }}">{{ realm_data.name }} ({{ realm_data.host }})</a></li>
     {% endfor %}
 </ul>
 

zerver/views/registration.py
@@ -1546,7 +1546,13 @@ def find_account(request: HttpRequest) -> HttpResponse:
                 key = user.delivery_email.lower()
                 context.setdefault(key, {})
                 context[key].setdefault("realms", [])
-                context[key]["realms"].append([user.realm.name, user.realm.url])
+                context[key]["realms"].append(
+                    {
+                        "name": user.realm.name,
+                        "host": user.realm.host,
+                        "url": user.realm.url,
+                    }
+                )
                 # This value will end up being the last user ID among
                 # matching accounts; since it's only used for minor
                 # details like language, that arbitrary choice is OK.