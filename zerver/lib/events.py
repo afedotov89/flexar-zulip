zulip/zulip

navigation_views: Add backend for navigation views in left sidebar.
Fixes part of #32077.

zerver/lib/events.py
@@ -12,6 +12,7 @@ from typing_extensions import NotRequired, TypedDict
 
 from version import API_FEATURE_LEVEL, ZULIP_MERGE_BASE, ZULIP_VERSION
 from zerver.actions.default_streams import default_stream_groups_to_dicts_sorted
+from zerver.actions.navigation_views import do_get_navigation_views
 from zerver.actions.realm_settings import get_realm_authentication_methods_for_page_params_api
 from zerver.actions.saved_snippets import do_get_saved_snippets
 from zerver.actions.users import get_owned_bot_dicts
@@ -213,6 +214,12 @@ def fetch_initial_state_data(
             state["saved_snippets"] = []
         else:
             state["saved_snippets"] = do_get_saved_snippets(user_profile)
+
+    if want("navigation_views"):
+        if user_profile is None:
+            state["navigation_views"] = []
+        else:
+            state["navigation_views"] = do_get_navigation_views(user_profile)
 
     if want("drafts"):
         if user_profile is None:
@@ -646,16 +653,18 @@ def fetch_initial_state_data(
                 "name": integration.name,
                 "display_name": integration.display_name,
                 "all_event_types": get_all_event_types_for_integration(integration),
-                "config_options": [
-                    {
-                        "key": c.name,
-                        "label": c.description,
-                        "validator": c.validator.__name__,
-                    }
-                    for c in integration.config_options
-                ]
-                if integration.config_options
-                else [],
+                "config_options": (
+                    [
+                        {
+                            "key": c.name,
+                            "label": c.description,
+                            "validator": c.validator.__name__,
+                        }
+                        for c in integration.config_options
+                    ]
+                    if integration.config_options
+                    else []
+                ),
             }
             for integration in WEBHOOK_INTEGRATIONS
             if integration.legacy is False
@@ -915,7 +924,19 @@ def apply_event(
                 if saved_snippet["id"] == event["saved_snippet_id"]:
                     del state["saved_snippets"][idx]
                     break
-
+    elif event["type"] == "navigation_views":
+        if event["op"] == "add":
+            state["navigation_views"].append(event["navigation_view"])
+        elif event["op"] == "update":
+            for navigation_view in state["navigation_views"]:
+                if navigation_view["fragment"] == event["navigation_view"]["fragment"]:
+                    navigation_view.update(event["navigation_view"])
+                    break
+        elif event["op"] == "remove":
+            for idx, navigation_view in enumerate(state["navigation_views"]):
+                if navigation_view["fragment"] == event["fragment"]:
+                    del state["navigation_views"][idx]
+                    break
     elif event["type"] == "drafts":
         if event["op"] == "add":
             state["drafts"].extend(event["drafts"])

afedotov89/flexar-zulip

zerver/lib/events.py
@@ -46,6 +46,7 @@ from zerver.lib.message import (
 from zerver.lib.muted_users import get_user_mutes
 from zerver.lib.narrow_helpers import NeverNegatedNarrowTerm, read_stop_words
 from zerver.lib.narrow_predicate import check_narrow_for_events
+from zerver.lib.navigation_views import get_navigation_views_for_user
 from zerver.lib.onboarding_steps import get_next_onboarding_steps
 from zerver.lib.presence import get_presence_for_user, get_presences_for_realm
 from zerver.lib.realm_icon import realm_icon_url
@@ -300,6 +301,12 @@ def fetch_initial_state_data(
             state["saved_snippets"] = []
         else:
             state["saved_snippets"] = do_get_saved_snippets(user_profile)
+
+    if want("navigation_views"):
+        if user_profile is None:
+            state["navigation_views"] = []
+        else:
+            state["navigation_views"] = get_navigation_views_for_user(user_profile)
 
     if want("drafts"):
         if user_profile is None:
@@ -1008,6 +1015,20 @@ def apply_event(
             for idx, saved_snippet in enumerate(state["saved_snippets"]):
                 if saved_snippet["id"] == event["saved_snippet"]["id"]:
                     state["saved_snippets"][idx] = event["saved_snippet"]
+                    break
+
+    elif event["type"] == "navigation_view":
+        if event["op"] == "add":
+            state["navigation_views"].append(event["navigation_view"])
+        elif event["op"] == "update":
+            for navigation_view in state["navigation_views"]:
+                if navigation_view["fragment"] == event["fragment"]:
+                    navigation_view.update(event["data"])
+                    break
+        elif event["op"] == "remove":
+            for idx, navigation_view in enumerate(state["navigation_views"]):
+                if navigation_view["fragment"] == event["fragment"]:
+                    del state["navigation_views"][idx]
                     break
 
     elif event["type"] == "drafts":