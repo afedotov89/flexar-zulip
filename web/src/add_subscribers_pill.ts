zulip/zulip

user_pill: Use user_id to get user info instead of email.

web/src/add_subscribers_pill.ts
@@ -19,7 +19,7 @@ export function create_item_from_text(
     const funcs = [
         stream_pill.create_item_from_stream_name,
         user_group_pill.create_item_from_group_name,
-        user_pill.create_item_from_email,
+        user_pill.create_item_from_user_id,
     ];
     for (const func of funcs) {
         const item = func(text, current_items);
@@ -40,7 +40,7 @@ export function get_text_from_item(item: CombinedPill): string {
             text = user_group_pill.get_group_name_from_item(item);
             break;
         case "user":
-            text = user_pill.get_email_from_item(item);
+            text = user_pill.get_user_id_string_from_item(item);
             break;
     }
     return text;

web/src/compose_pm_pill.ts
@@ -20,8 +20,8 @@ export function initialize_pill(): UserPillWidget {
     const pill = input_pill.create({
         $container,
         pill_config,
-        create_item_from_text: user_pill.create_item_from_email,
-        get_text_from_item: user_pill.get_email_from_item,
+        create_item_from_text: user_pill.create_item_from_user_id,
+        get_text_from_item: user_pill.get_user_id_string_from_item,
         get_display_value_from_item: user_pill.get_display_value_from_item,
         generate_pill_html: (item: UserPill) => user_pill.generate_pill_html(item, true),
     });
@@ -66,7 +66,13 @@ export function set_from_typeahead(person: User): void {
 export function set_from_emails(value: string): void {
     // value is something like "alice@example.com,bob@example.com"
     clear();
-    widget.appendValue(value);
+    if (value === "") {
+        return;
+    }
+    const user_ids_string = people.emails_strings_to_user_ids_string(value);
+    if (user_ids_string) {
+        widget.appendValue(user_ids_string);
+    }
 }
 
 export function set_from_user_ids(value: number[]): void {

web/src/group_setting_pill.ts
@@ -56,7 +56,7 @@ function check_user_allowed_for_setting(
         return true;
     }
 
-    const user = people.get_by_email(user_item.email);
+    const user = people.maybe_get_user_by_id(user_item.user_id, true);
     return user !== undefined && !user.is_guest;
 }
 
@@ -79,7 +79,7 @@ export function create_item_from_text(
         return undefined;
     }
 
-    const user_item = user_pill.create_item_from_email(text, current_items);
+    const user_item = user_pill.create_item_from_user_id(text, current_items);
     if (user_item) {
         if (check_user_allowed_for_setting(user_item, setting_name, setting_type)) {
             return user_item;
@@ -97,7 +97,7 @@ export function get_text_from_item(item: GroupSettingPill): string {
             text = user_group_pill.get_group_name_from_item(item);
             break;
         case "user":
-            text = user_pill.get_email_from_item(item);
+            text = user_pill.get_user_id_string_from_item(item);
             break;
     }
     return text;

afedotov89/flexar-zulip

web/src/add_subscribers_pill.ts
@@ -19,7 +19,7 @@ export function create_item_from_text(
     const funcs = [
         stream_pill.create_item_from_stream_name,
         user_group_pill.create_item_from_group_name,
-        user_pill.create_item_from_email,
+        user_pill.create_item_from_user_id,
     ];
     for (const func of funcs) {
         const item = func(text, current_items);
@@ -40,7 +40,7 @@ export function get_text_from_item(item: CombinedPill): string {
             text = user_group_pill.get_group_name_from_item(item);
             break;
         case "user":
-            text = user_pill.get_email_from_item(item);
+            text = user_pill.get_unique_full_name_from_item(item);
             break;
     }
     return text;

web/src/compose_pm_pill.ts
@@ -20,8 +20,8 @@ export function initialize_pill(): UserPillWidget {
     const pill = input_pill.create({
         $container,
         pill_config,
-        create_item_from_text: user_pill.create_item_from_email,
-        get_text_from_item: user_pill.get_email_from_item,
+        create_item_from_text: user_pill.create_item_from_user_id,
+        get_text_from_item: user_pill.get_unique_full_name_from_item,
         get_display_value_from_item: user_pill.get_display_value_from_item,
         generate_pill_html: (item: UserPill) => user_pill.generate_pill_html(item, true),
     });
@@ -66,7 +66,13 @@ export function set_from_typeahead(person: User): void {
 export function set_from_emails(value: string): void {
     // value is something like "alice@example.com,bob@example.com"
     clear();
-    widget.appendValue(value);
+    if (value === "") {
+        return;
+    }
+    const user_ids_string = people.emails_strings_to_user_ids_string(value);
+    if (user_ids_string) {
+        widget.appendValue(user_ids_string);
+    }
 }
 
 export function set_from_user_ids(value: number[]): void {

web/src/group_setting_pill.ts
@@ -56,7 +56,7 @@ function check_user_allowed_for_setting(
         return true;
     }
 
-    const user = people.get_by_email(user_item.email);
+    const user = people.maybe_get_user_by_id(user_item.user_id, true);
     return user !== undefined && !user.is_guest;
 }
 
@@ -79,7 +79,7 @@ export function create_item_from_text(
         return undefined;
     }
 
-    const user_item = user_pill.create_item_from_email(text, current_items);
+    const user_item = user_pill.create_item_from_user_id(text, current_items);
     if (user_item) {
         if (check_user_allowed_for_setting(user_item, setting_name, setting_type)) {
             return user_item;
@@ -97,7 +97,7 @@ export function get_text_from_item(item: GroupSettingPill): string {
             text = user_group_pill.get_group_name_from_item(item);
             break;
         case "user":
-            text = user_pill.get_email_from_item(item);
+            text = user_pill.get_unique_full_name_from_item(item);
             break;
     }
     return text;