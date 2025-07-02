zulip/zulip

overlays: Add overlay to display scheduled reminders.

web/src/message_reminder.ts
@@ -1,10 +1,17 @@
 import $ from "jquery";
+import type {z} from "zod";
 
 import * as channel from "./channel.ts";
 import * as feedback_widget from "./feedback_widget.ts";
 import {$t} from "./i18n.ts";
+import type {StateData, reminder_schema} from "./state_data.ts";
 import * as timerender from "./timerender.ts";
 import * as ui_report from "./ui_report.ts";
+
+export type Reminder = z.infer<typeof reminder_schema>;
+
+// reminders_data is a map where key=reminder id and value=reminders
+export const reminders_data = new Map<number, Reminder>();
 
 export function set_message_reminder(send_at_time: number, message_id: number): void {
     channel.post({
@@ -38,4 +45,31 @@ export function set_message_reminder(send_at_time: number, message_id: number): 
             ui_report.error($t({defaultMessage: "Failed"}), xhr, $("#home-error"), 2000);
         },
     });
+}
+
+export function add_reminders(reminders: Reminder[]): void {
+    for (const reminder of reminders) {
+        reminders_data.set(reminder.scheduled_message_id, reminder);
+    }
+}
+
+export function initialize(reminders_params: StateData["reminders"]): void {
+    add_reminders(reminders_params.reminders);
+}
+
+export function remove_reminder(reminder_id: number): void {
+    if (reminders_data.has(reminder_id)) {
+        reminders_data.delete(reminder_id);
+    }
+}
+
+export function delete_reminder(reminder_id: number, success?: () => void): void {
+    void channel.del({
+        url: "/json/reminders/" + reminder_id,
+        success,
+    });
+}
+
+export function get_count(): number {
+    return reminders_data.size;
 }

afedotov89/flexar-zulip

web/src/message_reminder.ts
@@ -1,10 +1,16 @@
 import $ from "jquery";
+import type {z} from "zod";
 
 import * as channel from "./channel.ts";
 import * as feedback_widget from "./feedback_widget.ts";
 import {$t} from "./i18n.ts";
+import type {StateData, reminder_schema} from "./state_data.ts";
 import * as timerender from "./timerender.ts";
 import * as ui_report from "./ui_report.ts";
+
+export type Reminder = z.infer<typeof reminder_schema>;
+
+export const reminders_by_id = new Map<number, Reminder>();
 
 export function set_message_reminder(send_at_time: number, message_id: number): void {
     channel.post({
@@ -38,4 +44,31 @@ export function set_message_reminder(send_at_time: number, message_id: number): 
             ui_report.error($t({defaultMessage: "Failed"}), xhr, $("#home-error"), 2000);
         },
     });
+}
+
+export function add_reminders(reminders: Reminder[]): void {
+    for (const reminder of reminders) {
+        reminders_by_id.set(reminder.reminder_id, reminder);
+    }
+}
+
+export function initialize(reminders_params: StateData["reminders"]): void {
+    add_reminders(reminders_params.reminders);
+}
+
+export function remove_reminder(reminder_id: number): void {
+    if (reminders_by_id.has(reminder_id)) {
+        reminders_by_id.delete(reminder_id);
+    }
+}
+
+export function delete_reminder(reminder_id: number, success?: () => void): void {
+    void channel.del({
+        url: "/json/reminders/" + reminder_id,
+        success,
+    });
+}
+
+export function get_count(): number {
+    return reminders_by_id.size;
 }