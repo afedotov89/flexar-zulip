zulip/zulip

lightbox: Fix media title update on change in title.
Previously, when title of media element is updated
the change is not reflected in lightbox view.

This is because the title of the element is cached
from the last time when the media element was opened in
lightbox.

This is fixed by invalidating a message's asset_map cache
whenever the content of message is edited.

Fixes #21311

web/src/lightbox.ts
@@ -212,6 +212,10 @@ export function clear_for_testing(): void {
     asset_map.clear();
 }
 
+export function maybe_invalidate_asset_map_of_message(message_id: number): void {
+    asset_map.delete(message_id);
+}
+
 function set_selected_media_element($media: JQuery<HTMLMediaElement | HTMLImageElement>): void {
     // Clear out any previously selected element
     $(".media-to-select-in-lightbox-list").removeClass("media-to-select-in-lightbox-list");

afedotov89/flexar-zulip

web/src/lightbox.ts
@@ -212,6 +212,10 @@ export function clear_for_testing(): void {
     asset_map.clear();
 }
 
+export function invalidate_asset_map_of_message(message_id: number): void {
+    asset_map.delete(message_id);
+}
+
 function set_selected_media_element($media: JQuery<HTMLMediaElement | HTMLImageElement>): void {
     // Clear out any previously selected element
     $(".media-to-select-in-lightbox-list").removeClass("media-to-select-in-lightbox-list");