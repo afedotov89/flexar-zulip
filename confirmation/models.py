zulip/zulip

confirmation: Replace RealmCreationKey - use Confirmation instead.
Fixes #20028.

There's no reason to have a special `RealmCreationKey` class - the
`Confirmation` system already does this job.

This is somewhat complicated by the need to write a migration for
`RealmCreationKey`->`Confirmation` for pre-existing, valid objects, to
avoid breaking realm creation links that haven't been used yet.

confirmation/models.py
@@ -5,7 +5,7 @@ import secrets
 from base64 import b32encode
 from collections.abc import Mapping
 from datetime import timedelta
-from typing import Optional, TypeAlias, Union, cast
+from typing import TypeAlias, Union, cast
 from urllib.parse import urljoin
 
 from django.conf import settings
@@ -30,6 +30,7 @@ from zerver.models import (
     RealmReactivationStatus,
     UserProfile,
 )
+from zerver.models.prereg_users import RealmCreationStatus
 
 if settings.ZILENCER_ENABLED:
     from zilencer.models import (
@@ -70,6 +71,7 @@ NoZilencerConfirmationObjT: TypeAlias = (
     | EmailChangeStatus
     | UserProfile
     | RealmReactivationStatus
+    | RealmCreationStatus
 )
 ZilencerConfirmationObjT: TypeAlias = Union[
     NoZilencerConfirmationObjT,
@@ -153,7 +155,7 @@ def create_confirmation_object(
         realm = None
     else:
         obj = cast(NoZilencerConfirmationObjT, obj)
-        assert not isinstance(obj, PreregistrationRealm)
+        assert not isinstance(obj, PreregistrationRealm | RealmCreationStatus)
         realm = obj.realm
 
     current_time = timezone_now()
@@ -185,15 +187,17 @@ def create_confirmation_link(
     url_args: Mapping[str, str] = {},
     no_associated_realm_object: bool = False,
 ) -> str:
-    return confirmation_url_for(
-        create_confirmation_object(
-            obj,
-            confirmation_type,
-            validity_in_minutes=validity_in_minutes,
-            no_associated_realm_object=no_associated_realm_object,
-        ),
+    conf = create_confirmation_object(
+        obj,
+        confirmation_type,
+        validity_in_minutes=validity_in_minutes,
+        no_associated_realm_object=no_associated_realm_object,
+    )
+    result = confirmation_url_for(
+        conf,
         url_args=url_args,
     )
+    return result
 
 
 def confirmation_url_for(confirmation_obj: "Confirmation", url_args: Mapping[str, str] = {}) -> str:
@@ -236,6 +240,7 @@ class Confirmation(models.Model):
     REALM_REACTIVATION = 8
     REMOTE_SERVER_BILLING_LEGACY_LOGIN = 9
     REMOTE_REALM_BILLING_LEGACY_LOGIN = 10
+    REALM_CREATION = 11
     type = models.PositiveSmallIntegerField()
 
     class Meta:
@@ -272,6 +277,9 @@ _properties = {
     Confirmation.MULTIUSE_INVITE: ConfirmationType(
         "join", validity_in_days=settings.INVITATION_LINK_VALIDITY_DAYS
     ),
+    Confirmation.REALM_CREATION: ConfirmationType(
+        "create_realm", validity_in_days=settings.REALM_CREATION_LINK_VALIDITY_DAYS
+    ),
     Confirmation.REALM_ACTIVATION: ConfirmationType("get_prereg_key_and_redirect"),
     Confirmation.REALM_REACTIVATION: ConfirmationType("realm_reactivation"),
 }
@@ -294,47 +302,7 @@ def one_click_unsubscribe_link(user_profile: UserProfile, email_type: str) -> st
     )
 
 
-# Functions related to links generated by the generate_realm_creation_link.py
-# management command.
-# Note that being validated here will just allow the user to access the create_realm
-# form, where they will enter their email and go through the regular
-# Confirmation.REALM_ACTIVATION pathway.
-# Arguably RealmCreationKey should just be another ConfirmationObjT and we should
-# add another Confirmation.type for this; it's this way for historical reasons.
-
-
-def validate_key(creation_key: str | None) -> Optional["RealmCreationKey"]:
-    """Get the record for this key, raising InvalidCreationKey if non-None but invalid."""
-    if creation_key is None:
-        return None
-    try:
-        key_record = RealmCreationKey.objects.get(creation_key=creation_key)
-    except RealmCreationKey.DoesNotExist:
-        raise RealmCreationKey.InvalidError
-    time_elapsed = timezone_now() - key_record.date_created
-    if time_elapsed.total_seconds() > settings.REALM_CREATION_LINK_VALIDITY_DAYS * 24 * 3600:
-        raise RealmCreationKey.InvalidError
-    return key_record
-
-
 def generate_realm_creation_url(by_admin: bool = False) -> str:
-    key = generate_key()
-    RealmCreationKey.objects.create(
-        creation_key=key, date_created=timezone_now(), presume_email_valid=by_admin
-    )
-    return urljoin(
-        settings.ROOT_DOMAIN_URI,
-        reverse("create_realm", kwargs={"creation_key": key}),
-    )
+    from zerver.views.registration import prepare_realm_creation_url
 
-
-class RealmCreationKey(models.Model):
-    creation_key = models.CharField("activation key", db_index=True, max_length=40)
-    date_created = models.DateTimeField("created", default=timezone_now)
-
-    # True just if we should presume the email address the user enters
-    # is theirs, and skip sending mail to it to confirm that.
-    presume_email_valid = models.BooleanField(default=False)
-
-    class InvalidError(Exception):
-        pass
+    return prepare_realm_creation_url(presume_email_valid=by_admin)

zerver/tests/test_management_commands.py
@@ -13,7 +13,7 @@ from django.core.management.base import CommandError
 from django.test import override_settings
 from typing_extensions import override
 
-from confirmation.models import RealmCreationKey, generate_realm_creation_url
+from confirmation.models import Confirmation, generate_realm_creation_url
 from zerver.actions.create_user import do_create_user
 from zerver.actions.user_settings import do_change_user_setting
 from zerver.lib.management import ZulipBaseCommand, check_config
@@ -360,11 +360,13 @@ class TestGenerateRealmCreationLink(ZulipTestCase):
     @override_settings(OPEN_REALM_CREATION=False)
     def test_realm_creation_with_expired_link(self) -> None:
         generated_link = generate_realm_creation_url(by_admin=True)
-        key = generated_link[-24:]
-        # Manually expire the link by changing the date of creation
-        obj = RealmCreationKey.objects.get(creation_key=key)
-        obj.date_created -= timedelta(days=settings.REALM_CREATION_LINK_VALIDITY_DAYS + 1)
-        obj.save()
+        key = generated_link.split("/")[-1]
+        # Manually expire the link by changing the date of expiry.
+        confirmation = Confirmation.objects.get(confirmation_key=key)
+        assert confirmation.expiry_date is not None
+
+        confirmation.expiry_date -= timedelta(days=settings.REALM_CREATION_LINK_VALIDITY_DAYS + 1)
+        confirmation.save()
 
         result = self.client_get(generated_link)
         self.assert_in_success_response(["Organization creation link expired or invalid"], result)

zerver/views/registration.py
@@ -1,7 +1,7 @@
 import logging
 from collections.abc import Iterable
 from contextlib import suppress
-from typing import Annotated, Any
+from typing import Annotated, Any, cast
 from urllib.parse import urlencode, urljoin
 
 import orjson
@@ -27,11 +27,9 @@ from confirmation import settings as confirmation_settings
 from confirmation.models import (
     Confirmation,
     ConfirmationKeyError,
-    RealmCreationKey,
     create_confirmation_link,
     get_object_from_key,
     render_confirmation_key_error,
-    validate_key,
 )
 from zerver.actions.create_realm import do_create_realm
 from zerver.actions.create_user import do_activate_mirror_dummy_user, do_create_user
@@ -101,6 +99,7 @@ from zerver.models import (
     UserProfile,
 )
 from zerver.models.constants import MAX_LANGUAGE_ID_LENGTH
+from zerver.models.prereg_users import RealmCreationStatus
 from zerver.models.realm_audit_logs import AuditLogEventType, RealmAuditLog
 from zerver.models.realms import (
     DisposableEmailError,
@@ -1004,6 +1003,19 @@ def prepare_realm_activation_url(
     return activation_url
 
 
+def prepare_realm_creation_url(
+    presume_email_valid: bool = False,
+) -> str:
+    realm_creation_status = RealmCreationStatus.objects.create(
+        presume_email_valid=presume_email_valid
+    )
+    confirmation_url = create_confirmation_link(
+        realm_creation_status, Confirmation.REALM_CREATION, no_associated_realm_object=True
+    )
+
+    return confirmation_url
+
+
 def send_confirm_registration_email(
     email: str,
     activation_url: str,
@@ -1244,15 +1256,9 @@ def realm_import_post_process(
 
 
 @add_google_analytics
-def create_realm(request: HttpRequest, creation_key: str | None = None) -> HttpResponse:
-    try:
-        key_record = validate_key(creation_key)
-    except RealmCreationKey.InvalidError:
-        return TemplateResponse(
-            request,
-            "zerver/portico_error_pages/realm_creation_link_invalid.html",
-        )
-    if key_record is None:
+def create_realm(request: HttpRequest, confirmation_key: str | None = None) -> HttpResponse:
+    if confirmation_key is None:
+        realm_creation_obj: RealmCreationStatus | None = None
         if not settings.OPEN_REALM_CREATION:
             return TemplateResponse(
                 request,
@@ -1262,6 +1268,19 @@ def create_realm(request: HttpRequest, creation_key: str | None = None) -> HttpR
             return TemplateResponse(
                 request,
                 "zerver/portico_error_pages/realm_creation_disabled.html",
+            )
+    else:
+        try:
+            realm_creation_obj = cast(
+                RealmCreationStatus,
+                get_object_from_key(
+                    confirmation_key, [Confirmation.REALM_CREATION], mark_object_used=False
+                ),
+            )
+        except ConfirmationKeyError:
+            return TemplateResponse(
+                request,
+                "zerver/portico_error_pages/realm_creation_link_invalid.html",
             )
 
     # When settings.OPEN_REALM_CREATION is enabled, anyone can create a new realm,
@@ -1298,12 +1317,13 @@ def create_realm(request: HttpRequest, creation_key: str | None = None) -> HttpR
                 realm_default_language,
                 import_from,
             )
-            if key_record is not None and key_record.presume_email_valid:
+            if realm_creation_obj is not None and realm_creation_obj.presume_email_valid:
                 # The user has a token created from the server command line;
                 # skip confirming the email is theirs, taking their word for it.
                 # This is essential on first install if the admin hasn't stopped
                 # to configure outbound email up front, or it isn't working yet.
-                key_record.delete()
+                realm_creation_obj.status = getattr(settings, "STATUS_USED", 1)
+                realm_creation_obj.save(update_fields=["status"])
                 return HttpResponseRedirect(activation_url)
 
             try:
@@ -1320,8 +1340,10 @@ def create_realm(request: HttpRequest, creation_key: str | None = None) -> HttpR
                     return render(request, "500.html", status=500)
                 return config_error(request, "smtp")
 
-            if key_record is not None:
-                key_record.delete()
+            if realm_creation_obj is not None:
+                realm_creation_obj.status = getattr(settings, "STATUS_USED", 1)
+                realm_creation_obj.save(update_fields=["status"])
+
             url = reverse(
                 "new_realm_send_confirm",
                 query={

afedotov89/flexar-zulip

confirmation/models.py
@@ -5,7 +5,7 @@ import secrets
 from base64 import b32encode
 from collections.abc import Mapping
 from datetime import timedelta
-from typing import Optional, TypeAlias, Union, cast
+from typing import TypeAlias, Union, cast
 from urllib.parse import urljoin
 
 from django.conf import settings
@@ -30,6 +30,7 @@ from zerver.models import (
     RealmReactivationStatus,
     UserProfile,
 )
+from zerver.models.prereg_users import RealmCreationStatus
 
 if settings.ZILENCER_ENABLED:
     from zilencer.models import (
@@ -70,6 +71,7 @@ NoZilencerConfirmationObjT: TypeAlias = (
     | EmailChangeStatus
     | UserProfile
     | RealmReactivationStatus
+    | RealmCreationStatus
 )
 ZilencerConfirmationObjT: TypeAlias = Union[
     NoZilencerConfirmationObjT,
@@ -153,7 +155,7 @@ def create_confirmation_object(
         realm = None
     else:
         obj = cast(NoZilencerConfirmationObjT, obj)
-        assert not isinstance(obj, PreregistrationRealm)
+        assert not isinstance(obj, PreregistrationRealm | RealmCreationStatus)
         realm = obj.realm
 
     current_time = timezone_now()
@@ -185,15 +187,17 @@ def create_confirmation_link(
     url_args: Mapping[str, str] = {},
     no_associated_realm_object: bool = False,
 ) -> str:
-    return confirmation_url_for(
-        create_confirmation_object(
-            obj,
-            confirmation_type,
-            validity_in_minutes=validity_in_minutes,
-            no_associated_realm_object=no_associated_realm_object,
-        ),
+    conf = create_confirmation_object(
+        obj,
+        confirmation_type,
+        validity_in_minutes=validity_in_minutes,
+        no_associated_realm_object=no_associated_realm_object,
+    )
+    result = confirmation_url_for(
+        conf,
         url_args=url_args,
     )
+    return result
 
 
 def confirmation_url_for(confirmation_obj: "Confirmation", url_args: Mapping[str, str] = {}) -> str:
@@ -236,6 +240,7 @@ class Confirmation(models.Model):
     REALM_REACTIVATION = 8
     REMOTE_SERVER_BILLING_LEGACY_LOGIN = 9
     REMOTE_REALM_BILLING_LEGACY_LOGIN = 10
+    CAN_CREATE_REALM = 11
     type = models.PositiveSmallIntegerField()
 
     class Meta:
@@ -272,6 +277,9 @@ _properties = {
     Confirmation.MULTIUSE_INVITE: ConfirmationType(
         "join", validity_in_days=settings.INVITATION_LINK_VALIDITY_DAYS
     ),
+    Confirmation.CAN_CREATE_REALM: ConfirmationType(
+        "create_realm", validity_in_days=settings.CAN_CREATE_REALM_LINK_VALIDITY_DAYS
+    ),
     Confirmation.NEW_REALM_USER_REGISTRATION: ConfirmationType("get_prereg_key_and_redirect"),
     Confirmation.REALM_REACTIVATION: ConfirmationType("realm_reactivation_get"),
 }
@@ -294,47 +302,7 @@ def one_click_unsubscribe_link(user_profile: UserProfile, email_type: str) -> st
     )
 
 
-# Functions related to links generated by the generate_realm_creation_link.py
-# management command.
-# Note that being validated here will just allow the user to access the create_realm
-# form, where they will enter their email and go through the regular
-# Confirmation.NEW_REALM_USER_REGISTRATION pathway.
-# Arguably RealmCreationKey should just be another ConfirmationObjT and we should
-# add another Confirmation.type for this; it's this way for historical reasons.
-
-
-def validate_key(creation_key: str | None) -> Optional["RealmCreationKey"]:
-    """Get the record for this key, raising InvalidCreationKey if non-None but invalid."""
-    if creation_key is None:
-        return None
-    try:
-        key_record = RealmCreationKey.objects.get(creation_key=creation_key)
-    except RealmCreationKey.DoesNotExist:
-        raise RealmCreationKey.InvalidError
-    time_elapsed = timezone_now() - key_record.date_created
-    if time_elapsed.total_seconds() > settings.REALM_CREATION_LINK_VALIDITY_DAYS * 24 * 3600:
-        raise RealmCreationKey.InvalidError
-    return key_record
-
-
 def generate_realm_creation_url(by_admin: bool = False) -> str:
-    key = generate_key()
-    RealmCreationKey.objects.create(
-        creation_key=key, date_created=timezone_now(), presume_email_valid=by_admin
-    )
-    return urljoin(
-        settings.ROOT_DOMAIN_URI,
-        reverse("create_realm", kwargs={"creation_key": key}),
-    )
+    from zerver.views.registration import prepare_realm_creation_url
 
-
-class RealmCreationKey(models.Model):
-    creation_key = models.CharField("activation key", db_index=True, max_length=40)
-    date_created = models.DateTimeField("created", default=timezone_now)
-
-    # True just if we should presume the email address the user enters
-    # is theirs, and skip sending mail to it to confirm that.
-    presume_email_valid = models.BooleanField(default=False)
-
-    class InvalidError(Exception):
-        pass
+    return prepare_realm_creation_url(presume_email_valid=by_admin)

zerver/tests/test_management_commands.py
@@ -13,7 +13,7 @@ from django.core.management.base import CommandError
 from django.test import override_settings
 from typing_extensions import override
 
-from confirmation.models import RealmCreationKey, generate_realm_creation_url
+from confirmation.models import Confirmation, generate_realm_creation_url
 from zerver.actions.create_user import do_create_user
 from zerver.actions.user_settings import do_change_user_setting
 from zerver.lib.management import ZulipBaseCommand, check_config
@@ -360,11 +360,13 @@ class TestGenerateRealmCreationLink(ZulipTestCase):
     @override_settings(OPEN_REALM_CREATION=False)
     def test_realm_creation_with_expired_link(self) -> None:
         generated_link = generate_realm_creation_url(by_admin=True)
-        key = generated_link[-24:]
-        # Manually expire the link by changing the date of creation
-        obj = RealmCreationKey.objects.get(creation_key=key)
-        obj.date_created -= timedelta(days=settings.REALM_CREATION_LINK_VALIDITY_DAYS + 1)
-        obj.save()
+        key = generated_link.split("/")[-1]
+        # Manually expire the link by changing the date of expiry.
+        confirmation = Confirmation.objects.get(confirmation_key=key)
+        assert confirmation.expiry_date is not None
+
+        confirmation.expiry_date -= timedelta(days=settings.CAN_CREATE_REALM_LINK_VALIDITY_DAYS + 1)
+        confirmation.save()
 
         result = self.client_get(generated_link)
         self.assert_in_success_response(["Organization creation link expired or invalid"], result)

zerver/views/registration.py
@@ -1,7 +1,7 @@
 import logging
 from collections.abc import Iterable
 from contextlib import suppress
-from typing import Annotated, Any
+from typing import Annotated, Any, cast
 from urllib.parse import urlencode, urljoin
 
 import orjson
@@ -27,11 +27,9 @@ from confirmation import settings as confirmation_settings
 from confirmation.models import (
     Confirmation,
     ConfirmationKeyError,
-    RealmCreationKey,
     create_confirmation_link,
     get_object_from_key,
     render_confirmation_key_error,
-    validate_key,
 )
 from zerver.actions.create_realm import do_create_realm
 from zerver.actions.create_user import do_activate_mirror_dummy_user, do_create_user
@@ -101,6 +99,7 @@ from zerver.models import (
     UserProfile,
 )
 from zerver.models.constants import MAX_LANGUAGE_ID_LENGTH
+from zerver.models.prereg_users import RealmCreationStatus
 from zerver.models.realm_audit_logs import AuditLogEventType, RealmAuditLog
 from zerver.models.realms import (
     DisposableEmailError,
@@ -1006,6 +1005,19 @@ def prepare_realm_activation_url(
     return activation_url
 
 
+def prepare_realm_creation_url(
+    presume_email_valid: bool = False,
+) -> str:
+    realm_creation_status = RealmCreationStatus.objects.create(
+        presume_email_valid=presume_email_valid
+    )
+    confirmation_url = create_confirmation_link(
+        realm_creation_status, Confirmation.CAN_CREATE_REALM, no_associated_realm_object=True
+    )
+
+    return confirmation_url
+
+
 def send_confirm_registration_email(
     email: str,
     activation_url: str,
@@ -1246,15 +1258,9 @@ def realm_import_post_process(
 
 
 @add_google_analytics
-def create_realm(request: HttpRequest, creation_key: str | None = None) -> HttpResponse:
-    try:
-        key_record = validate_key(creation_key)
-    except RealmCreationKey.InvalidError:
-        return TemplateResponse(
-            request,
-            "zerver/portico_error_pages/realm_creation_link_invalid.html",
-        )
-    if key_record is None:
+def create_realm(request: HttpRequest, confirmation_key: str | None = None) -> HttpResponse:
+    if confirmation_key is None:
+        realm_creation_obj: RealmCreationStatus | None = None
         if not settings.OPEN_REALM_CREATION:
             return TemplateResponse(
                 request,
@@ -1264,6 +1270,19 @@ def create_realm(request: HttpRequest, creation_key: str | None = None) -> HttpR
             return TemplateResponse(
                 request,
                 "zerver/portico_error_pages/realm_creation_disabled.html",
+            )
+    else:
+        try:
+            realm_creation_obj = cast(
+                RealmCreationStatus,
+                get_object_from_key(
+                    confirmation_key, [Confirmation.CAN_CREATE_REALM], mark_object_used=False
+                ),
+            )
+        except ConfirmationKeyError:
+            return TemplateResponse(
+                request,
+                "zerver/portico_error_pages/realm_creation_link_invalid.html",
             )
 
     # When settings.OPEN_REALM_CREATION is enabled, anyone can create a new realm,
@@ -1300,12 +1319,13 @@ def create_realm(request: HttpRequest, creation_key: str | None = None) -> HttpR
                 realm_default_language,
                 import_from,
             )
-            if key_record is not None and key_record.presume_email_valid:
+            if realm_creation_obj is not None and realm_creation_obj.presume_email_valid:
                 # The user has a token created from the server command line;
                 # skip confirming the email is theirs, taking their word for it.
                 # This is essential on first install if the admin hasn't stopped
                 # to configure outbound email up front, or it isn't working yet.
-                key_record.delete()
+                realm_creation_obj.status = getattr(settings, "STATUS_USED", 1)
+                realm_creation_obj.save(update_fields=["status"])
                 return HttpResponseRedirect(activation_url)
 
             try:
@@ -1322,8 +1342,10 @@ def create_realm(request: HttpRequest, creation_key: str | None = None) -> HttpR
                     return render(request, "500.html", status=500)
                 return config_error(request, "smtp")
 
-            if key_record is not None:
-                key_record.delete()
+            if realm_creation_obj is not None:
+                realm_creation_obj.status = getattr(settings, "STATUS_USED", 1)
+                realm_creation_obj.save(update_fields=["status"])
+
             url = reverse(
                 "new_realm_send_confirm",
                 query={