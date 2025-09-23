zulip/zulip

streams: Make sure that unused anonymous groups are not created.
Anonymous groups were being created for stream permission
settings when calling the subscriptions endpoint without
any streams data or when calling it only for subscribing
users to streams and not for creating any new streams.

This commit makes sure that no such unused anonymous groups
are created.

zerver/lib/streams.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from collections.abc import Collection, Iterable
 from dataclasses import dataclass
 from datetime import datetime, timedelta
-from typing import TypedDict
+from typing import Any, TypedDict
 
 from django.db import transaction
 from django.db.models import Exists, OuterRef, Q, QuerySet
@@ -30,11 +30,13 @@ from zerver.lib.topic import get_topic_display_name
 from zerver.lib.types import APIStreamDict, UserGroupMembersData
 from zerver.lib.user_groups import (
     UserGroupMembershipDetails,
+    access_user_group_for_setting,
     get_group_setting_value_for_register_api,
     get_members_and_subgroups_of_groups,
     get_recursive_membership_groups,
     get_role_based_system_groups_dict,
     get_root_id_annotated_recursive_subgroups_for_groups,
+    parse_group_setting_value,
     user_has_permission_for_group_setting,
 )
 from zerver.models import (
@@ -1511,13 +1513,57 @@ def filter_stream_authorization_for_adding_subscribers(
     )
 
 
+def access_requested_group_permissions(
+    user_profile: UserProfile,
+    realm: Realm,
+    request_settings_dict: dict[str, Any],
+) -> tuple[dict[str, UserGroup], dict[int, UserGroupMembersData]]:
+    anonymous_group_membership = {}
+    group_settings_map = {}
+    system_groups_name_dict = get_role_based_system_groups_dict(realm)
+    for setting_name, permission_configuration in Stream.stream_permission_group_settings.items():
+        assert setting_name in request_settings_dict
+        if request_settings_dict[setting_name] is not None:
+            setting_request_value = request_settings_dict[setting_name]
+            setting_value = parse_group_setting_value(
+                setting_request_value, system_groups_name_dict[SystemGroups.NOBODY]
+            )
+            group_settings_map[setting_name] = access_user_group_for_setting(
+                setting_value,
+                user_profile,
+                setting_name=setting_name,
+                permission_configuration=permission_configuration,
+            )
+            if (
+                setting_name in ["can_delete_any_message_group", "can_delete_own_message_group"]
+                and group_settings_map[setting_name].id
+                != system_groups_name_dict[SystemGroups.NOBODY].id
+                and not user_profile.can_set_delete_message_policy()
+            ):
+                raise JsonableError(_("Insufficient permission"))
+
+            if not isinstance(setting_value, int):
+                anonymous_group_membership[group_settings_map[setting_name].id] = setting_value
+        else:
+            group_settings_map[setting_name] = get_stream_permission_default_group(
+                setting_name, system_groups_name_dict, creator=user_profile
+            )
+            if permission_configuration.default_group_name == "stream_creator_or_nobody":
+                # Default for some settings like "can_administer_channel_group"
+                # is anonymous group with stream creator.
+                anonymous_group_membership[group_settings_map[setting_name].id] = (
+                    UserGroupMembersData(direct_subgroups=[], direct_members=[user_profile.id])
+                )
+    return group_settings_map, anonymous_group_membership
+
+
 def list_to_streams(
     streams_raw: Collection[StreamDict],
     user_profile: UserProfile,
     autocreate: bool = False,
     unsubscribing_others: bool = False,
     is_default_stream: bool = False,
-    anonymous_group_membership: dict[int, UserGroupMembersData] | None = None,
+    request_settings_dict: dict[str, Any] | None = None,
 ) -> tuple[list[Stream], list[Stream]]:
     """Converts list of dicts to a list of Streams, validating input in the process
 
@@ -1572,6 +1618,14 @@ def list_to_streams(
                     ),
                 )
             )
+
+        assert request_settings_dict is not None
+        group_settings_map, anonymous_group_membership = access_requested_group_permissions(
+            user_profile,
+            user_profile.realm,
+            request_settings_dict,
+        )
+
         # autocreate=True path starts here
         for stream_dict in missing_stream_dicts:
             check_channel_creation_permissions(
@@ -1581,6 +1635,31 @@ def list_to_streams(
                 is_web_public=stream_dict["is_web_public"],
                 message_retention_days=stream_dict.get("message_retention_days", None),
             )
+
+            stream_dict["can_add_subscribers_group"] = group_settings_map[
+                "can_add_subscribers_group"
+            ]
+            stream_dict["can_administer_channel_group"] = group_settings_map[
+                "can_administer_channel_group"
+            ]
+            stream_dict["can_delete_any_message_group"] = group_settings_map[
+                "can_delete_any_message_group"
+            ]
+            stream_dict["can_delete_own_message_group"] = group_settings_map[
+                "can_delete_own_message_group"
+            ]
+            stream_dict["can_move_messages_out_of_channel_group"] = group_settings_map[
+                "can_move_messages_out_of_channel_group"
+            ]
+            stream_dict["can_move_messages_within_channel_group"] = group_settings_map[
+                "can_move_messages_within_channel_group"
+            ]
+            stream_dict["can_send_message_group"] = group_settings_map["can_send_message_group"]
+            stream_dict["can_remove_subscribers_group"] = group_settings_map[
+                "can_remove_subscribers_group"
+            ]
+            stream_dict["can_resolve_topics_group"] = group_settings_map["can_resolve_topics_group"]
+            stream_dict["can_subscribe_group"] = group_settings_map["can_subscribe_group"]
 
         # We already filtered out existing streams, so dup_streams
         # will normally be an empty list below, but we protect against somebody

afedotov89/flexar-zulip

zerver/lib/streams.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from collections.abc import Collection, Iterable
 from dataclasses import dataclass
 from datetime import datetime, timedelta
-from typing import TypedDict
+from typing import Any, TypedDict
 
 from django.db import transaction
 from django.db.models import Exists, OuterRef, Q, QuerySet
@@ -30,11 +30,13 @@ from zerver.lib.topic import get_topic_display_name
 from zerver.lib.types import APIStreamDict, UserGroupMembersData
 from zerver.lib.user_groups import (
     UserGroupMembershipDetails,
+    access_user_group_for_setting,
     get_group_setting_value_for_register_api,
     get_members_and_subgroups_of_groups,
     get_recursive_membership_groups,
     get_role_based_system_groups_dict,
     get_root_id_annotated_recursive_subgroups_for_groups,
+    parse_group_setting_value,
     user_has_permission_for_group_setting,
 )
 from zerver.models import (
@@ -1486,13 +1488,57 @@ def filter_stream_authorization_for_adding_subscribers(
     )
 
 
+def access_requested_group_permissions(
+    user_profile: UserProfile,
+    realm: Realm,
+    request_settings_dict: dict[str, Any],
+) -> tuple[dict[str, UserGroup], dict[int, UserGroupMembersData]]:
+    anonymous_group_membership = {}
+    group_settings_map = {}
+    system_groups_name_dict = get_role_based_system_groups_dict(realm)
+    for setting_name, permission_configuration in Stream.stream_permission_group_settings.items():
+        assert setting_name in request_settings_dict
+        if request_settings_dict[setting_name] is not None:
+            setting_request_value = request_settings_dict[setting_name]
+            setting_value = parse_group_setting_value(
+                setting_request_value, system_groups_name_dict[SystemGroups.NOBODY]
+            )
+            group_settings_map[setting_name] = access_user_group_for_setting(
+                setting_value,
+                user_profile,
+                setting_name=setting_name,
+                permission_configuration=permission_configuration,
+            )
+            if (
+                setting_name in ["can_delete_any_message_group", "can_delete_own_message_group"]
+                and group_settings_map[setting_name].id
+                != system_groups_name_dict[SystemGroups.NOBODY].id
+                and not user_profile.can_set_delete_message_policy()
+            ):
+                raise JsonableError(_("Insufficient permission"))
+
+            if not isinstance(setting_value, int):
+                anonymous_group_membership[group_settings_map[setting_name].id] = setting_value
+        else:
+            group_settings_map[setting_name] = get_stream_permission_default_group(
+                setting_name, system_groups_name_dict, creator=user_profile
+            )
+            if permission_configuration.default_group_name == "channel_creator":
+                # Default for some settings like "can_administer_channel_group"
+                # is anonymous group with stream creator.
+                anonymous_group_membership[group_settings_map[setting_name].id] = (
+                    UserGroupMembersData(direct_subgroups=[], direct_members=[user_profile.id])
+                )
+    return group_settings_map, anonymous_group_membership
+
+
 def list_to_streams(
     streams_raw: Collection[StreamDict],
     user_profile: UserProfile,
     autocreate: bool = False,
     unsubscribing_others: bool = False,
     is_default_stream: bool = False,
-    anonymous_group_membership: dict[int, UserGroupMembersData] | None = None,
+    request_settings_dict: dict[str, Any] | None = None,
 ) -> tuple[list[Stream], list[Stream]]:
     """Converts list of dicts to a list of Streams, validating input in the process
 
@@ -1547,6 +1593,14 @@ def list_to_streams(
                     ),
                 )
             )
+
+        assert request_settings_dict is not None
+        group_settings_map, anonymous_group_membership = access_requested_group_permissions(
+            user_profile,
+            user_profile.realm,
+            request_settings_dict,
+        )
+
         # autocreate=True path starts here
         for stream_dict in missing_stream_dicts:
             check_channel_creation_permissions(
@@ -1556,6 +1610,31 @@ def list_to_streams(
                 is_web_public=stream_dict["is_web_public"],
                 message_retention_days=stream_dict.get("message_retention_days", None),
             )
+
+            stream_dict["can_add_subscribers_group"] = group_settings_map[
+                "can_add_subscribers_group"
+            ]
+            stream_dict["can_administer_channel_group"] = group_settings_map[
+                "can_administer_channel_group"
+            ]
+            stream_dict["can_delete_any_message_group"] = group_settings_map[
+                "can_delete_any_message_group"
+            ]
+            stream_dict["can_delete_own_message_group"] = group_settings_map[
+                "can_delete_own_message_group"
+            ]
+            stream_dict["can_move_messages_out_of_channel_group"] = group_settings_map[
+                "can_move_messages_out_of_channel_group"
+            ]
+            stream_dict["can_move_messages_within_channel_group"] = group_settings_map[
+                "can_move_messages_within_channel_group"
+            ]
+            stream_dict["can_send_message_group"] = group_settings_map["can_send_message_group"]
+            stream_dict["can_remove_subscribers_group"] = group_settings_map[
+                "can_remove_subscribers_group"
+            ]
+            stream_dict["can_resolve_topics_group"] = group_settings_map["can_resolve_topics_group"]
+            stream_dict["can_subscribe_group"] = group_settings_map["can_subscribe_group"]
 
         # We already filtered out existing streams, so dup_streams
         # will normally be an empty list below, but we protect against somebody