kmixco/Zulip

export: Move an extra key for the record into the function.

zerver/lib/export.py
@@ -1584,6 +1584,7 @@ def _get_exported_s3_record(
 ) -> Dict[str, Any]:
     # Helper function for export_files_from_s3
     record: Dict[str, Any] = dict(
+        path=key.key,
         s3_path=key.key,
         bucket=bucket_name,
         size=key.content_length,
@@ -1710,7 +1711,6 @@ def export_files_from_s3(
             bucket_name, s3_obj, processing_emoji, user_id_emails, realm.id
         )
 
-        record["path"] = s3_obj.key
         _save_s3_object_to_file(s3_obj, output_dir, processing_uploads)
 
         records.append(record)

afedotov89/flexar-zulip

zerver/lib/export.py
@@ -2102,6 +2102,7 @@ def _get_exported_s3_record(
 ) -> dict[str, Any]:
     # Helper function for export_files_from_s3
     record: dict[str, Any] = dict(
+        path=s3_obj.key,
         s3_path=s3_obj.key,
         bucket=bucket_name,
         size=s3_obj.content_length,
@@ -2234,7 +2235,6 @@ def export_files_from_s3(
 
             record = _get_exported_s3_record(bucket_name, s3_obj, processing_emoji, realm.id)
 
-            record["path"] = s3_obj.key
             _save_s3_object_to_file(s3_obj, output_dir, processing_uploads)
 
             yield record