zulip/zulip

settings_export: Warn if some accounts will be unusable.
Since it is now possible to generate export with no usable owner
accounts, this adds a couple of warning notes in the "Start export?"
confirmation modal:

  - A note indicating the number of unusable user accounts after export
→ import due to inaccessible user emails.

  - If there are any owner/admin accounts in that list, an additional
note will display their names.

Fixes #34275.

web/src/people.ts
@@ -1957,6 +1957,19 @@ export function fetch_users_from_server(opts: FetchUserDataParams): void {
         },
     });
 }
+export function get_users_full_names_that_matches_role_ids(
+    user_ids: Set<number>,
+    role_ids: Set<number>,
+): string[] {
+    const full_names = new Array<string>();
+    for (const user_id of user_ids) {
+        const person = get_by_user_id(user_id);
+        if (person && role_ids.has(person.role)) {
+            full_names.push(person.full_name);
+        }
+    }
+    return full_names;
+}
 
 export async function fetch_users(user_ids: Set<number>): Promise<UsersFetchResponse["members"]> {
     // Requested users outside the set of known valid user IDs likely

afedotov89/flexar-zulip

web/src/people.ts
@@ -1975,6 +1975,19 @@ export function fetch_users_from_server(opts: FetchUserDataParams): void {
         },
     });
 }
+export function get_users_that_match_role_ids(
+    user_ids: Set<number>,
+    role_ids: Set<number>,
+): User[] {
+    const users = new Array<User>();
+    for (const user_id of user_ids) {
+        const person = get_by_user_id(user_id);
+        if (person && role_ids.has(person.role)) {
+            users.push(person);
+        }
+    }
+    return users;
+}
 
 export async function fetch_users(user_ids: Set<number>): Promise<UsersFetchResponse["members"]> {
     // Requested users outside the set of known valid user IDs likely