evykassirer/zulip

topic_link_util: Rename get_topic_link_content_with_stream_name.
In a future commit we'll be adding `get_topic_link_content_with_stream_id`.

web/src/clipboard_handler.ts
@@ -39,7 +39,7 @@ export async function copy_link_to_clipboard(link: string): Promise<void> {
             } else {
                 const stream = stream_data.get_sub_by_id(stream_topic_details.stream_id);
                 assert(stream !== undefined);
-                const {display_html} = topic_link_util.get_topic_link_content(
+                const {display_html} = topic_link_util.get_topic_link_content_with_stream_name(
                     stream.name,
                     stream_topic_details.topic_name,
                     stream_topic_details.message_id,

web/src/topic_link_util.ts
@@ -54,7 +54,7 @@ export function html_escape_markdown_syntax_characters(text: string): string {
     return text.replaceAll(invalid_stream_topic_regex, escape_invalid_stream_topic_characters);
 }
 
-export function get_topic_link_content(
+export function get_topic_link_content_with_stream_name(
     stream_name: string,
     topic_name?: string,
     message_id?: string,
@@ -103,7 +103,11 @@ export function get_fallback_markdown_link(
     // Generates the vanilla markdown link syntax for a stream/topic/message link, as
     // a fallback for cases where the nicer Zulip link syntax would not
     // render properly due to special characters in the channel or topic name.
-    const {display_html, url} = get_topic_link_content(stream_name, topic_name, message_id);
+    const {display_html, url} = get_topic_link_content_with_stream_name(
+        stream_name,
+        topic_name,
+        message_id,
+    );
     return as_markdown_link_syntax(display_html, url);
 }
 

afedotov89/flexar-zulip

web/src/clipboard_handler.ts
@@ -39,7 +39,7 @@ export async function copy_link_to_clipboard(link: string): Promise<void> {
             } else {
                 const stream = stream_data.get_sub_by_id(stream_topic_details.stream_id);
                 assert(stream !== undefined);
-                const {text} = topic_link_util.get_topic_link_content(
+                const {text} = topic_link_util.get_topic_link_content_with_stream_name(
                     stream.name,
                     stream_topic_details.topic_name,
                     stream_topic_details.message_id,

web/src/topic_link_util.ts
@@ -54,7 +54,7 @@ export function html_escape_markdown_syntax_characters(text: string): string {
     return text.replaceAll(invalid_stream_topic_regex, escape_invalid_stream_topic_characters);
 }
 
-export function get_topic_link_content(
+export function get_topic_link_content_with_stream_name(
     stream_name: string,
     topic_name?: string,
     message_id?: string,
@@ -103,7 +103,11 @@ export function get_fallback_markdown_link(
     // Generates the vanilla markdown link syntax for a stream/topic/message link, as
     // a fallback for cases where the nicer Zulip link syntax would not
     // render properly due to special characters in the channel or topic name.
-    const {text, url} = get_topic_link_content(stream_name, topic_name, message_id);
+    const {text, url} = get_topic_link_content_with_stream_name(
+        stream_name,
+        topic_name,
+        message_id,
+    );
     return as_markdown_link_syntax(text, url);
 }
 