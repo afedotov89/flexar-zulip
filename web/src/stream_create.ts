zulip/zulip

stream-settings: Disable privacy element and topic creation setting.
This commit updates the code in the webapp to use the
`can_create_topic_group` setting to validate the
`history_public_to_subscribers` setting when creating
a new stream or updating a stream's configuration.

web/src/stream_create.ts
@@ -548,6 +548,7 @@ export function show_new_stream_modal(): void {
     stream_ui_updates.update_can_subscribe_group_label($("#stream-creation"));
     stream_ui_updates.update_default_stream_option_state($("#stream-creation"));
     stream_ui_updates.update_private_stream_privacy_option_state($("#stream-creation"));
+    stream_ui_updates.update_can_create_topic_group_setting_state($("#stream-creation"));
     clear_error_display();
 }
 
@@ -565,6 +566,16 @@ function set_up_group_setting_widgets(): void {
             group_setting_widgets[setting_name],
         );
     }
+
+    // Enable or disable protected history stream privacy option when
+    // can_create_topic_group setting is updated.
+    const can_create_topic_group_widget = group_setting_widgets.can_create_topic_group!;
+    can_create_topic_group_widget.onPillCreate(() => {
+        stream_ui_updates.update_private_stream_privacy_option_state($("#stream-creation"));
+    });
+    can_create_topic_group_widget.onPillRemove(() => {
+        stream_ui_updates.update_private_stream_privacy_option_state($("#stream-creation"));
+    });
 }
 
 export function set_up_handlers(): void {
@@ -581,6 +592,7 @@ export function set_up_handlers(): void {
         update_announce_stream_state();
         stream_ui_updates.update_default_stream_option_state($container);
         stream_ui_updates.update_can_subscribe_group_label($container);
+        stream_ui_updates.update_can_create_topic_group_setting_state($container);
     });
 
     $container.on("change", ".default-stream input", () => {

web/tests/stream_events.test.cjs
@@ -50,6 +50,7 @@ mock_esm("../src/user_settings", {
     },
 });
 
+const stream_ui_updates = mock_esm("../src/stream_ui_updates");
 const user_group_edit = mock_esm("../src/user_group_edit");
 const user_profile = mock_esm("../src/user_profile");
 
@@ -312,8 +313,15 @@ test("update_property", ({override}) => {
             can_add_subscribers: false,
             ...sub,
         }));
+        const update_add_subscriptions_elements_stub = make_stub();
+        override(
+            stream_ui_updates,
+            "update_add_subscriptions_elements",
+            update_add_subscriptions_elements_stub.f,
+        );
         stream_events.update_property(stream_id, "can_administer_channel_group", 3);
         assert.equal(stub.num_calls, 1);
+        assert.equal(update_add_subscriptions_elements_stub.num_calls, 1);
         const args = stub.get_args("setting_name", "sub", "val");
         assert.equal(args.setting_name, "can_administer_channel_group");
         assert.equal(args.sub.stream_id, stream_id);
@@ -331,6 +339,21 @@ test("update_property", ({override}) => {
         assert.equal(args.setting_name, "can_resolve_topics_group");
         assert.equal(args.sub.stream_id, stream_id);
         assert.equal(args.val, 3);
+    }
+
+    // Test stream can_create_topic_group change event
+    {
+        const stub = make_stub();
+        override(stream_settings_ui, "update_stream_permission_group_setting", stub.f);
+        const update_private_stream_privacy_option_state_stub = make_stub();
+        override(
+            stream_ui_updates,
+            "update_private_stream_privacy_option_state",
+            update_private_stream_privacy_option_state_stub.f,
+        );
+        stream_events.update_property(stream_id, "can_create_topic_group", 3);
+        assert.equal(stub.num_calls, 1);
+        assert.equal(update_private_stream_privacy_option_state_stub.num_calls, 1);
     }
 
     // Test stream can_subscribe_group change event

afedotov89/flexar-zulip

web/src/stream_create.ts
@@ -537,6 +537,7 @@ export function show_new_stream_modal(): void {
     stream_ui_updates.update_can_subscribe_group_label($("#stream-creation"));
     stream_ui_updates.update_default_stream_option_state($("#stream-creation"));
     stream_ui_updates.update_history_public_to_subscribers_state($("#stream-creation"));
+    stream_ui_updates.update_can_create_topic_group_setting_state($("#stream-creation"));
     clear_error_display();
 }
 
@@ -554,6 +555,16 @@ function set_up_group_setting_widgets(): void {
             group_setting_widgets[setting_name],
         );
     }
+
+    // Enable or disable protected history stream privacy option when
+    // can_create_topic_group setting is updated.
+    const can_create_topic_group_widget = group_setting_widgets["can_create_topic_group"]!;
+    can_create_topic_group_widget.onPillCreate(() => {
+        stream_ui_updates.update_history_public_to_subscribers_state($("#stream-creation"));
+    });
+    can_create_topic_group_widget.onPillRemove(() => {
+        stream_ui_updates.update_history_public_to_subscribers_state($("#stream-creation"));
+    });
 }
 
 export function set_up_handlers(): void {
@@ -637,6 +648,10 @@ export function set_up_handlers(): void {
         if (keydown_util.is_enter_event(e)) {
             e.preventDefault();
         }
+    });
+
+    $container.on("input", "#id_new_history_public_to_subscribers", () => {
+        stream_ui_updates.update_can_create_topic_group_setting_state($("#stream-creation"));
     });
 
     set_up_group_setting_widgets();

web/tests/stream_events.test.cjs
@@ -50,6 +50,7 @@ mock_esm("../src/user_settings", {
     },
 });
 
+const stream_ui_updates = mock_esm("../src/stream_ui_updates");
 const user_group_edit = mock_esm("../src/user_group_edit");
 const user_profile = mock_esm("../src/user_profile");
 
@@ -312,8 +313,15 @@ test("update_property", ({override}) => {
             can_add_subscribers: false,
             ...sub,
         }));
+        const update_add_subscriptions_elements_stub = make_stub();
+        override(
+            stream_ui_updates,
+            "update_add_subscriptions_elements",
+            update_add_subscriptions_elements_stub.f,
+        );
         stream_events.update_property(stream_id, "can_administer_channel_group", 3);
         assert.equal(stub.num_calls, 1);
+        assert.equal(update_add_subscriptions_elements_stub.num_calls, 1);
         const args = stub.get_args("setting_name", "sub", "val");
         assert.equal(args.setting_name, "can_administer_channel_group");
         assert.equal(args.sub.stream_id, stream_id);
@@ -331,6 +339,21 @@ test("update_property", ({override}) => {
         assert.equal(args.setting_name, "can_resolve_topics_group");
         assert.equal(args.sub.stream_id, stream_id);
         assert.equal(args.val, 3);
+    }
+
+    // Test stream can_create_topic_group change event
+    {
+        const stub = make_stub();
+        override(stream_settings_ui, "update_stream_permission_group_setting", stub.f);
+        const update_history_public_to_subscribers_state_stub = make_stub();
+        override(
+            stream_ui_updates,
+            "update_history_public_to_subscribers_state",
+            update_history_public_to_subscribers_state_stub.f,
+        );
+        stream_events.update_property(stream_id, "can_create_topic_group", 3);
+        assert.equal(stub.num_calls, 1);
+        assert.equal(update_history_public_to_subscribers_state_stub.num_calls, 1);
     }
 
     // Test stream can_subscribe_group change event