zulip/zulip

upload-widget: Fix using uppy editor after one failure.
Previously, the "file-editor:complete" handler was
called only once, but this resulted in problem where
user was unable to try uploading it again after
receiving error from server on the first try.

Maybe the handler was configured to run once before
when the implementation used same uppy widget for
different cases, but now we create a new widget
everytime when selecting a file input so should
be fine.

web/src/upload_widget.ts
@@ -238,7 +238,7 @@ function open_uppy_editor(
             const uppy_file = uppy_widget.getFile(uppy_file_id);
             uppy_widget.getPlugin<ImageEditor<Meta, Body>>("ImageEditor")!.selectFile(uppy_file);
 
-            uppy_widget.once("file-editor:complete", (updated_file) => {
+            uppy_widget.on("file-editor:complete", (updated_file) => {
                 void (async () => {
                     assert(updated_file.data instanceof File);
                     const new_dimensions = await get_image_dimensions(updated_file.data);

afedotov89/flexar-zulip

web/src/upload_widget.ts
@@ -243,7 +243,7 @@ function open_uppy_editor(
                 };
             }
 
-            uppy_widget.once("file-editor:complete", (file) => {
+            uppy_widget.on("file-editor:complete", (file) => {
                 assert(file.data instanceof File);
                 let is_night = null;
                 let for_realm_icon = true;