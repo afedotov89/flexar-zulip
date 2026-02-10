zulip/zulip

unread_ops: Change modal text for mark as unread confirmation modal.
This commit changes the modal header and body text for mark messages as
unread confirmation modal.

Fixes: zulip#37593.

web/src/unread_ops.ts
@@ -471,27 +471,27 @@ export function mark_as_unread_from_here(message_id: number): void {
         display_count = `${rounded_count}+`;
     }
 
-    const context = {
-        // If we don't know how many messages will be affected, but
-        // can't prove the number is more than 10, we avoid showing a
-        // count, since it just seems weird to say "3+ messages will
-        // be marked as read".
-        //
-        // It's not obvious this case is worth having special strings
-        // for, given how unlikely it is. A sample scenario is that
-        // we're be that we're near the fetched bottom of a /near/1
-        // search view where can_apply_locally is false, which will
-        // have triggered a request for the next batch of messages
-        // from the server, but that request has not returned. But it
-        // may happen more offline if the client is intermittantly
-        // offline.
-        show_message_count: locally_available_message_count >= MIN_MARK_AS_UNREAD_COUNT_LOWER_BOUND,
-        count: display_count,
-    };
+    // If we don't know how many messages will be affected, but
+    // can't prove the number is more than 10, we avoid showing a
+    // count, since it just seems weird to say "3+ messages will
+    // be marked as read".
+    //
+    // It's not obvious this case is worth having special strings
+    // for, given how unlikely it is. A sample scenario is that
+    // we're be that we're near the fetched bottom of a /near/1
+    // search view where can_apply_locally is false, which will
+    // have triggered a request for the next batch of messages
+    // from the server, but that request has not returned. But it
+    // may happen more offline if the client is intermittantly
+    // offline.
+    const show_message_count =
+        locally_available_message_count >= MIN_MARK_AS_UNREAD_COUNT_LOWER_BOUND;
 
     confirm_dialog.launch({
-        html_heading: $t_html({defaultMessage: "Mark messages as unread?"}),
-        html_body: render_confirm_mark_as_unread_from_here(context),
+        html_heading: show_message_count
+            ? $t_html({defaultMessage: "Mark {display_count} messages as unread?"}, {display_count})
+            : $t_html({defaultMessage: "Mark messages as unread?"}),
+        html_body: render_confirm_mark_as_unread_from_here(),
         on_click() {
             if (prefer_local_ids) {
                 do_mark_unread(locally_available_matching_message_ids);

afedotov89/flexar-zulip

web/src/unread_ops.ts
@@ -4,7 +4,6 @@ import assert from "minimalistic-assert";
 import * as z from "zod/mini";
 
 import render_confirm_mark_messages_as_read from "../templates/confirm_dialog/confirm_mark_all_as_read.hbs";
-import render_confirm_mark_as_unread_from_here from "../templates/confirm_dialog/confirm_mark_as_unread_from_here.hbs";
 import render_skipped_marking_unread from "../templates/skipped_marking_unread.hbs";
 
 import * as blueslip from "./blueslip.ts";
@@ -449,27 +448,29 @@ export function mark_as_unread_from_here(message_id: number): void {
         display_count = `${rounded_count}+`;
     }
 
-    const context = {
-        // If we don't know how many messages will be affected, but
-        // can't prove the number is more than 10, we avoid showing a
-        // count, since it just seems weird to say "3+ messages will
-        // be marked as read".
-        //
-        // It's not obvious this case is worth having special strings
-        // for, given how unlikely it is. A sample scenario is that
-        // we're be that we're near the fetched bottom of a /near/1
-        // search view where can_apply_locally is false, which will
-        // have triggered a request for the next batch of messages
-        // from the server, but that request has not returned. But it
-        // may happen more offline if the client is intermittantly
-        // offline.
-        show_message_count: locally_available_message_count >= MIN_MARK_AS_UNREAD_COUNT_LOWER_BOUND,
-        count: display_count,
-    };
+    // If we don't know how many messages will be affected, but
+    // can't prove the number is more than 10, we avoid showing a
+    // count, since it just seems weird to say "3+ messages will
+    // be marked as read".
+    //
+    // It's not obvious this case is worth having special strings
+    // for, given how unlikely it is. A sample scenario is that
+    // we're be that we're near the fetched bottom of a /near/1
+    // search view where can_apply_locally is false, which will
+    // have triggered a request for the next batch of messages
+    // from the server, but that request has not returned. But it
+    // may happen more offline if the client is intermittantly
+    // offline.
+    const show_message_count =
+        locally_available_message_count >= MIN_MARK_AS_UNREAD_COUNT_LOWER_BOUND;
 
     confirm_dialog.launch({
-        modal_title_html: $t_html({defaultMessage: "Mark messages as unread?"}),
-        modal_content_html: render_confirm_mark_as_unread_from_here(context),
+        modal_title_html: show_message_count
+            ? $t_html({defaultMessage: "Mark {display_count} messages as unread?"}, {display_count})
+            : $t_html({defaultMessage: "Mark messages as unread?"}),
+        modal_content_html: $t_html({
+            defaultMessage: "Messages in multiple conversations may be affected.",
+        }),
         is_compact: true,
         on_click() {
             if (prefer_local_ids) {