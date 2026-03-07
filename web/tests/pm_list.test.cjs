evykassirer/zulip

left_sidebar: Modal styling for more DM conversations.
Part of #34471.

web/tests/pm_list.test.cjs
@@ -15,6 +15,9 @@ run_test("update_dom_with_unread_counts", () => {
     const $private_li = $("#direct-messages-section-header");
     $private_li.set_find_results(".unread_count", $total_count);
 
+    const $private_modal_header = $("#direct-messages-modal-section-header");
+    $private_modal_header.set_find_results(".unread_count", $total_count);
+
     counts = {
         direct_message_count: 10,
     };

afedotov89/flexar-zulip

web/tests/pm_list.test.cjs
@@ -15,6 +15,9 @@ run_test("update_dom_with_unread_counts", () => {
     const $dm_li = $("#direct-messages-section-header");
     $dm_li.set_find_results(".unread_count", $total_count);
 
+    const $dm_modal_header = $("#direct-messages-modal-section-header");
+    $dm_modal_header.set_find_results(".unread_count", $total_count);
+
     counts = {
         direct_message_count: 10,
     };