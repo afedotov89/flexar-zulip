Rocker810/zulip

onboarding: Replace dict[str, Any] with dataclass.

zerver/tests/test_onboarding_steps.py
@@ -32,14 +32,14 @@ class TestGetNextOnboardingSteps(ZulipTestCase):
         do_mark_onboarding_step_as_read(self.user, "intro_inbox_view_modal")
         onboarding_steps = get_next_onboarding_steps(self.user)
         self.assert_length(onboarding_steps, 8)
-        self.assertEqual(onboarding_steps[0]["name"], "intro_recent_view_modal")
-        self.assertEqual(onboarding_steps[1]["name"], "first_stream_created_banner")
-        self.assertEqual(onboarding_steps[2]["name"], "jump_to_conversation_banner")
-        self.assertEqual(onboarding_steps[3]["name"], "non_interleaved_view_messages_fading")
-        self.assertEqual(onboarding_steps[4]["name"], "interleaved_view_messages_fading")
-        self.assertEqual(onboarding_steps[5]["name"], "intro_resolve_topic")
-        self.assertEqual(onboarding_steps[6]["name"], "navigation_tour_video")
-        self.assertEqual(onboarding_steps[7]["name"], "narrow_to_dm_with_welcome_bot_new_user")
+        self.assertEqual(onboarding_steps[0].name, "intro_recent_view_modal")
+        self.assertEqual(onboarding_steps[1].name, "first_stream_created_banner")
+        self.assertEqual(onboarding_steps[2].name, "jump_to_conversation_banner")
+        self.assertEqual(onboarding_steps[3].name, "non_interleaved_view_messages_fading")
+        self.assertEqual(onboarding_steps[4].name, "interleaved_view_messages_fading")
+        self.assertEqual(onboarding_steps[5].name, "intro_resolve_topic")
+        self.assertEqual(onboarding_steps[6].name, "navigation_tour_video")
+        self.assertEqual(onboarding_steps[7].name, "narrow_to_dm_with_welcome_bot_new_user")
 
         with self.settings(TUTORIAL_ENABLED=False):
             onboarding_steps = get_next_onboarding_steps(self.user)
@@ -47,7 +47,7 @@ class TestGetNextOnboardingSteps(ZulipTestCase):
 
         with self.settings(NAVIGATION_TOUR_VIDEO_URL=None):
             onboarding_steps = get_next_onboarding_steps(self.user)
-        self.assertNotIn("navigation_tour_video", onboarding_steps)
+        self.assertTrue(all(s.name != "navigation_tour_video" for s in onboarding_steps))
 
     def test_all_onboarding_steps_done(self) -> None:
         self.assertNotEqual(get_next_onboarding_steps(self.user), [])

afedotov89/flexar-zulip

zerver/tests/test_onboarding_steps.py
@@ -32,15 +32,15 @@ class TestGetNextOnboardingSteps(ZulipTestCase):
         do_mark_onboarding_step_as_read(self.user, "intro_inbox_view_modal")
         onboarding_steps = get_next_onboarding_steps(self.user)
         self.assert_length(onboarding_steps, 9)
-        self.assertEqual(onboarding_steps[0]["name"], "intro_recent_view_modal")
-        self.assertEqual(onboarding_steps[1]["name"], "first_stream_created_banner")
-        self.assertEqual(onboarding_steps[2]["name"], "jump_to_conversation_banner")
-        self.assertEqual(onboarding_steps[3]["name"], "non_interleaved_view_messages_fading")
-        self.assertEqual(onboarding_steps[4]["name"], "interleaved_view_messages_fading")
-        self.assertEqual(onboarding_steps[5]["name"], "intro_resolve_topic")
-        self.assertEqual(onboarding_steps[6]["name"], "navigation_tour_video")
-        self.assertEqual(onboarding_steps[7]["name"], "intro_go_to_conversation_tooltip")
-        self.assertEqual(onboarding_steps[8]["name"], "narrow_to_dm_with_welcome_bot_new_user")
+        self.assertEqual(onboarding_steps[0].name, "intro_recent_view_modal")
+        self.assertEqual(onboarding_steps[1].name, "first_stream_created_banner")
+        self.assertEqual(onboarding_steps[2].name, "jump_to_conversation_banner")
+        self.assertEqual(onboarding_steps[3].name, "non_interleaved_view_messages_fading")
+        self.assertEqual(onboarding_steps[4].name, "interleaved_view_messages_fading")
+        self.assertEqual(onboarding_steps[5].name, "intro_resolve_topic")
+        self.assertEqual(onboarding_steps[6].name, "navigation_tour_video")
+        self.assertEqual(onboarding_steps[7].name, "intro_go_to_conversation_tooltip")
+        self.assertEqual(onboarding_steps[8].name, "narrow_to_dm_with_welcome_bot_new_user")
 
         with self.settings(TUTORIAL_ENABLED=False):
             onboarding_steps = get_next_onboarding_steps(self.user)
@@ -48,7 +48,7 @@ class TestGetNextOnboardingSteps(ZulipTestCase):
 
         with self.settings(NAVIGATION_TOUR_VIDEO_URL=None):
             onboarding_steps = get_next_onboarding_steps(self.user)
-        self.assertNotIn("navigation_tour_video", onboarding_steps)
+        self.assertTrue(all(step.name != "navigation_tour_video" for step in onboarding_steps))
 
     def test_all_onboarding_steps_done(self) -> None:
         self.assertNotEqual(get_next_onboarding_steps(self.user), [])