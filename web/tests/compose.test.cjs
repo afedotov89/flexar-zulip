zulip/zulip

compose: Poll for thumbnail availability in preview.
Previously, if the user toggles the preview mode in the compose box before
an image/GIF in the drafted message has been uploaded and thumbnailed,
then the loading indicator shown in the preview of the message will never update.

This commit implements a polling mechanism in the compose preview.
When the preview is rendered, we now scan for thumbnail placeholders and
periodically check their thumbnail status. Once a thumbnail becomes
available, the preview is automatically re-rendered to display the image.

fixes #32961.

web/tests/compose.test.cjs
@@ -715,6 +715,11 @@ test_ui("on_events", ({override, override_rewire}) => {
     })();
 
     (function test_markdown_preview_compose_clicked() {
+        $("#compose .preview_content").set_find_results(
+            ".image-loading-placeholder",
+            $.create("no-images", {children: []}),
+        );
+
         function setup_mock_markdown_contains_backend_only_syntax(msg_content, return_val) {
             override(markdown, "contains_backend_only_syntax", (msg) => {
                 assert.equal(msg, msg_content);

afedotov89/flexar-zulip

web/tests/compose.test.cjs
@@ -685,6 +685,11 @@ test_ui("on_events", ({override, override_rewire}) => {
     })();
 
     (function test_markdown_preview_compose_clicked() {
+        $("#compose .preview_content").set_find_results(
+            ".image-loading-placeholder",
+            $.create("no-images", {elements: []}),
+        );
+
         function setup_mock_markdown_contains_backend_only_syntax(msg_content, return_val) {
             override(markdown, "contains_backend_only_syntax", (msg) => {
                 assert.equal(msg, msg_content);