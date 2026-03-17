zulip/zulip

saml: Create ExternalAuthID for new users registered via SAML.
Thread the `external_auth_id_dict_for_registration` from the social
auth pipeline through to `do_create_user` so that new users registered
via SAML get an ExternalAuthID record created at registration time.

Rename `external_auth_id_dict` to `external_auth_id_dict_for_registration`
throughout the pipeline to clarify that this dict is only used during
user registration (not login).

Store the external auth ID on `PreregistrationUser` (new fields:
`external_auth_method_name`, `external_auth_id`) instead of in the
session via `set_expirable_session_var`. This is more robust and
consistent with how other PreregistrationUser data is handled.

The data flows through: social_auth_finish → ExternalAuthDataDict →
register_remote_user → maybe_send_to_registration (PreregistrationUser) →
registration_helper → do_create_user.

Fixes part of #33096.

Co-Authored-By: Claude Opus 4.6 <noreply@anthropic.com>

zerver/actions/create_user.py
@@ -535,7 +535,7 @@ def do_create_user(
     enable_marketing_emails: bool = True,
     email_address_visibility: int | None = None,
     add_initial_stream_subscriptions: bool = True,
-    external_auth_id_dict: dict[str, str] | None = None,
+    external_auth_id_dict_for_registration: dict[str, str] | None = None,
 ) -> UserProfile:
     if settings.BILLING_ENABLED:
         from corporate.lib.stripe import RealmBillingSession
@@ -630,8 +630,11 @@ def do_create_user(
             "add_members", full_members_system_group, [user_profile.id]
         )
 
-    if external_auth_id_dict:
-        for external_auth_method_name, external_auth_id in external_auth_id_dict.items():
+    if external_auth_id_dict_for_registration:
+        for (
+            external_auth_method_name,
+            external_auth_id,
+        ) in external_auth_id_dict_for_registration.items():
             ExternalAuthID.objects.create(
                 user=user_profile,
                 realm=user_profile.realm,

afedotov89/flexar-zulip

zerver/actions/create_user.py
@@ -622,7 +622,10 @@ def do_create_user(
         )
 
     if external_auth_id_dict:
-        for external_auth_method_name, external_auth_id in external_auth_id_dict.items():
+        for (
+            external_auth_method_name,
+            external_auth_id,
+        ) in external_auth_id_dict.items():
             ExternalAuthID.objects.create(
                 user=user_profile,
                 realm=user_profile.realm,