afedotov89/flexar-zulip

commit f84308370c6121145018eea30c37a591a6133cb2

compose_call: Abandon all callbacks only on closing textarea.
The previous model only aborted the most recent
XHR on closing the textarea and did not handle
the n-1 XHRs that might have been associated with
the same textarea.

We also clear any OAuth token callbacks associated
with a message textarea when it is closed.

We do not require clearing token callbacks associated
with a message textarea when creating a new call;
those can still be executed once the OAuth flow
is complete.

Signed-off-by: apoorvapendse <apoorvavpendse@gmail.com>

web/src/compose_call_ui.ts
@@ -66,8 +66,6 @@ export function generate_and_insert_audio_or_video_call_link(
     const key = edit_message_id ?? "";
 
     if (provider_is_zoom || provider_is_zoom_server_to_server) {
-        compose_call.abort_video_callbacks(edit_message_id);
-
         const request = {
             is_video_call: !is_audio_call,
         };
@@ -81,7 +79,6 @@ export function generate_and_insert_audio_or_video_call_link(
                         return;
                     }
                     const data = call_response_schema.parse(res);
-                    compose_call.call_xhrs.delete(key);
                     if (is_audio_call) {
                         insert_audio_call_url(data.url, $target_textarea);
                     } else {
@@ -92,7 +89,6 @@ export function generate_and_insert_audio_or_video_call_link(
                     if (xhr && compose_call.ignored_call_xhrs.has(xhr)) {
                         return;
                     }
-                    compose_call.call_xhrs.delete(key);
                     const parsed = z.object({code: z.string()}).safeParse(_xhr.responseJSON);
                     if (
                         status === "error" &&
@@ -114,9 +110,6 @@ export function generate_and_insert_audio_or_video_call_link(
                     }
                 },
             });
-            if (xhr !== undefined) {
-                compose_call.call_xhrs.set(key, xhr);
-            }
         };
 
         if (current_user.has_zoom_token || provider_is_zoom_server_to_server) {
@@ -153,8 +146,6 @@ export function generate_and_insert_audio_or_video_call_link(
                 break;
             }
             case available_providers.constructor_groups?.id: {
-                compose_call.abort_video_callbacks(edit_message_id);
-
                 xhr = channel.post({
                     url: "/json/calls/constructorgroups/create",
                     data: {},
@@ -163,14 +154,12 @@ export function generate_and_insert_audio_or_video_call_link(
                             return;
                         }
                         const data = call_response_schema.parse(response);
-                        compose_call.call_xhrs.delete(key);
                         insert_video_call_url(data.url, $target_textarea);
                     },
                     error(_xhr, status) {
                         if (xhr && compose_call.ignored_call_xhrs.has(xhr)) {
                             return;
                         }
-                        compose_call.call_xhrs.delete(key);
                         if (status !== "abort") {
                             ui_report.generic_embed_error(
                                 $t_html({defaultMessage: "Failed to create video call."}),
@@ -179,11 +168,6 @@ export function generate_and_insert_audio_or_video_call_link(
                         }
                     },
                 });
-
-                if (xhr !== undefined) {
-                    compose_call.call_xhrs.set(key, xhr);
-                }
-
                 break;
             }
             case available_providers.nextcloud_talk?.id: {
@@ -245,5 +229,8 @@ export function generate_and_insert_audio_or_video_call_link(
                 );
             }
         }
+    }
+    if (xhr !== undefined) {
+        compose_call.track_xhr_for_key(key, xhr);
     }
 }

web/src/compose_call.ts
@@ -1,7 +1,7 @@
 import {realm} from "./state_data.ts";
 
 export const zoom_token_callbacks = new Map();
-export const call_xhrs = new Map<string, JQuery.jqXHR<unknown>>();
+export const call_xhrs = new Map<string, JQuery.jqXHR<unknown>[]>();
 export const ignored_call_xhrs = new Set<JQuery.jqXHR>();
 
 export function get_jitsi_server_url(video_call_id?: string): URL | null {
@@ -16,16 +16,34 @@ export function get_jitsi_server_url(video_call_id?: string): URL | null {
     return url;
 }
 
-export function abort_video_callbacks(edit_message_id = ""): void {
-    zoom_token_callbacks.delete(edit_message_id);
-    const xhr = call_xhrs.get(edit_message_id);
-    if (xhr !== undefined) {
+export function track_xhr_for_key(key: string, xhr: JQuery.jqXHR): void {
+    const existing_xhrs = call_xhrs.get(key);
+    if (existing_xhrs) {
+        existing_xhrs.push(xhr);
+    } else {
+        call_xhrs.set(key, [xhr]);
+    }
+}
+
+// This abandons any Zoom OAuth completion callbacks as well as XHR related callbacks
+// by "aborting" the XHRs associated with a message textarea identified by the id.
+export function abandon_all_callbacks_for_key(key: string): void {
+    zoom_token_callbacks.delete(key);
+    abandon_pending_xhrs_for_key(key);
+}
+
+function abandon_pending_xhrs_for_key(key: string): void {
+    const existing_xhrs = call_xhrs.get(key);
+    if (existing_xhrs === undefined) {
+        return;
+    }
+    for (const xhr of existing_xhrs) {
         // TODO: Use xhr.abort(), if XHR methods are available
         // after https://github.com/getsentry/sentry-javascript/issues/19242
         // gets resolved.
         ignored_call_xhrs.add(xhr);
-        call_xhrs.delete(edit_message_id);
     }
+    call_xhrs.delete(key);
 }
 
 export function compute_show_video_chat_button(): boolean {

web/src/compose_setup.ts
@@ -55,7 +55,9 @@ function setup_compose_actions_hooks(): void {
     compose_actions.register_compose_box_clear_hook(compose.clear_preview_area);
 
     compose_actions.register_compose_cancel_hook(abort_xhr);
-    compose_actions.register_compose_cancel_hook(compose_call.abort_video_callbacks);
+    compose_actions.register_compose_cancel_hook(() => {
+        compose_call.abandon_all_callbacks_for_key("");
+    });
 }
 
 export function initialize(): void {

web/src/message_edit.ts
@@ -1138,7 +1138,7 @@ export function end_message_row_edit($row: JQuery): void {
         currently_editing_messages.delete(message.id);
         resized_edit_box_height.delete(message.id);
         message_lists.current.hide_edit_message($row);
-        compose_call.abort_video_callbacks(message.id.toString());
+        compose_call.abandon_all_callbacks_for_key(message.id.toString());
     }
     if ($row.find(".could-be-condensed").length > 0) {
         if ($row.find(".condensed").length > 0) {

web/tests/compose_video.test.cjs
@@ -9,6 +9,7 @@ const {run_test} = require("./lib/test.cjs");
 const $ = require("./lib/zjquery.cjs");
 const {page_params} = require("./lib/zpage_params.cjs");
 
+const compose_call = zrequire("compose_call");
 const channel = mock_esm("../src/channel");
 const compose_closed_ui = mock_esm("../src/compose_closed_ui");
 const compose_ui = mock_esm("../src/compose_ui");
@@ -410,4 +411,33 @@ test("test_constructor_groups_video_chat_button_toggle enabled", ({override}) =>
     override(window, "to_$", () => $("window-stub"));
     compose_setup.initialize();
     assert.equal($(".compose-control-buttons-container .video_link").visible(), true);
+});
+
+test("abandon_all_callbacks_for_key", ({override}) => {
+    override(realm, "realm_video_chat_provider", realm_available_video_chat_providers.zoom.id);
+    compose_call.track_xhr_for_key("1", {});
+    compose_call.track_xhr_for_key("1", {});
+    compose_call.track_xhr_for_key("1", {});
+
+    const token_callback = () => {};
+    compose_call.zoom_token_callbacks.set("1", token_callback);
+
+    assert.equal(compose_call.ignored_call_xhrs.size, 0);
+    assert.equal(compose_call.zoom_token_callbacks.get("1"), token_callback);
+    compose_call.abandon_all_callbacks_for_key("1");
+    assert.equal(compose_call.ignored_call_xhrs.size, 3);
+    assert.equal(compose_call.zoom_token_callbacks.get("1"), undefined);
+
+    // Callback abandon mechanism should not interfere with XHRs/token
+    // callbacks of other textareas.
+    compose_call.ignored_call_xhrs.clear();
+    assert.equal(compose_call.ignored_call_xhrs.size, 0);
+    compose_call.track_xhr_for_key("1", {});
+    compose_call.track_xhr_for_key("1", {});
+    compose_call.track_xhr_for_key("1", {});
+    compose_call.zoom_token_callbacks.set("1", token_callback);
+
+    compose_call.abandon_all_callbacks_for_key("2");
+    assert.equal(compose_call.ignored_call_xhrs.size, 0);
+    assert.equal(compose_call.zoom_token_callbacks.get("1"), token_callback);
 });

commit cc844b335c2d018492b18c44fe687dd98cdaee78

compose_call_ui: Call track_xhr_for_key in token callbacks
This was a minor bug introduced in f84308370c6121145018eea30c37a591a6133cb2
where we didn't track XHRs generated when running
make_oauth_call as a token callback.

So they also weren't added to the ignored_call_xhrs
set when compose_call.abandon_pending_xhrs_for_key
was called on closing some textarea.

Signed-off-by: apoorvapendse <apoorvavpendse@gmail.com>

web/src/compose_call_ui.ts
@@ -70,7 +70,7 @@ export function generate_and_insert_audio_or_video_call_link(
             is_video_call: !is_audio_call,
         };
 
-        const make_oauth_call = (): void => {
+        const make_oauth_call = (from_token_callback?: boolean): void => {
             xhr = channel.post({
                 url: `/json/calls/${oauth_call_provider}/create`,
                 data: request,
@@ -114,6 +114,9 @@ export function generate_and_insert_audio_or_video_call_link(
                     }
                 },
             });
+            if (xhr && from_token_callback) {
+                compose_call.track_xhr_for_key(key, xhr);
+            }
         };
 
         if (
@@ -123,11 +126,9 @@ export function generate_and_insert_audio_or_video_call_link(
         ) {
             make_oauth_call();
         } else {
-            compose_call.update_oauth_provider_callback_for_key(
-                oauth_call_provider,
-                key,
-                make_oauth_call,
-            );
+            compose_call.update_oauth_provider_callback_for_key(oauth_call_provider, key, () => {
+                make_oauth_call(true);
+            });
             window.open(
                 window.location.protocol +
                     "//" +